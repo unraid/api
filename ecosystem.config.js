ae944baec178dd1b56078387a6d474f811b7a7e5
Alexis Tyler <xo@wvvw.me>
2019-06-14 11:11:01 +0930

    rename app to graphql-api and add safe-mode env
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,23 @@
 /* eslint-disable camelcase */
 
 module.exports = {
-    apps: [
-        {
-            name: 'my_servers-client',
-            script: './index.js',
-            watch: false,
-            wait_ready: true,
-            listen_timeout: 3000,
-            env: {
-                PORT: 5000,
-                NODE_ENV: 'development'
-            },
-            env_production: {
-                PORT: '/var/run/my_servers-client.sock',
-                NODE_ENV: 'production'
-            }
+    apps: [{
+        name: 'graphql-api',
+        script: './index.js',
+        watch: false,
+        wait_ready: true,
+        listen_timeout: 3000,
+        env: {
+            PORT: 5000,
+            NODE_ENV: 'development'
+        },
+        'env_safe-mode': {
+            PORT: '/var/run/graphql-api.sock',
+            NODE_ENV: 'safe-mode'
+        },
+        env_production: {
+            PORT: '/var/run/graphql-api.sock',
+            NODE_ENV: 'production'
         }
-    ]
+    }]
 };
