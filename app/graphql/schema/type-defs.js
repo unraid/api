5ebb57aeb45589a4cf93b61d93a32f5ab40c5360
Alexis Tyler <xo@wvvw.me>
2019-08-09 15:20:26 +0930

    update deps
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

--- app/graphql/schema/type-defs.js
+++ app/graphql/schema/type-defs.js
@@ -13,8 +13,9 @@
 module.exports = function (path, mergeGraphqlSchemas) {
     const { join } = path;
     const { fileLoader, mergeTypes } = mergeGraphqlSchemas;
+    const files = fileLoader(join(__dirname, './types/**/*.graphql'));
 
-    return mergeTypes(fileLoader(join(__dirname, './types/**/*.graphql')), {
+    return mergeTypes(files, {
         all: true
     });
 }
