2339a5d4d3c030aee4a87891ef25b3059e16ac46
Alexis Tyler <xo@wvvw.me>
2019-08-11 17:14:09 +0930

    swap copyright headers
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /*
- * Copyright 2005-2019, Lime Technology
- * Copyright 2018-2019, Alexis Tyler
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Copyright 2019 Lime Technology Inc.  All rights reserved.
+ * Written by: Alexis Tyler
  */
 
 // Boot app
