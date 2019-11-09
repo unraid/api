62114e1edc9136d2041a432482337570f4e33d29
Alexis Tyler <xo@wvvw.me>
2019-11-09 12:14:36 +1030

    style(module:metrics): add new line to end of file

--- app/metrics.js
+++ app/metrics.js
@@ -7,24 +7,24 @@
  * The Graphql pm2 metrics reporters.
  */
 module.exports = function ($injector) {
-    const websocketClients = {
-        name: 'Websocket clients',
-        value: () => $injector.resolve('ws-clients').size
-    };
+	const websocketClients = {
+		name: 'Websocket clients',
+		value: () => $injector.resolve('ws-clients').size
+	};
 
-    const memoryCaches = {
-        name: 'Memory caches',
-        value: () => {
-            const caches = $injector.resolve('caches');
-            const keys = caches.keys();
+	const memoryCaches = {
+		name: 'Memory caches',
+		value: () => {
+			const caches = $injector.resolve('caches');
+			const keys = caches.keys();
 
-            // Return amount of caches that exist
-            return keys.length;
-        }
-    };
+			// Return amount of caches that exist
+			return keys.length;
+		}
+	};
 
-    return {
-        websocketClients,
-        memoryCaches
-    };
-}
+	return {
+		websocketClients,
+		memoryCaches
+	};
+};
