6ee6a570d6e7d28ef55fe0f48c441a771d6c18ba
Alexis Tyler <xo@wvvw.me>
2020-02-02 15:28:51 +1030

    refactor: misc changes

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const path = require('path');
 const cluster = require('cluster');
 
+// Show real stack trace in development
+if (process.env.NODE_ENV !== 'production') {
+    require('source-map-support').install({
+        handleUncaughtExceptions: false
+    });
+}
+
 const RESTART_ATTEMPTS = 10;
 let currentRestartAttempt = 0;
 
 if (cluster.isMaster) {
     cluster.fork();
 
-    cluster.on('exit', (worker, code, signal) => {
+    cluster.on('exit', (_, code) => {
         if (code !== 7) {
             process.exit(code);
         }
 
-        if (currentRestartAttempt < RESTART_ATTEMPTS) {
-            currentRestartAttempt++;
-            cluster.fork();
+        if (currentRestartAttempt >= RESTART_ATTEMPTS) {
+            console.debug(`No restart attempts left. Exiting.`);
+            process.exit(1);
         }
 
-        console.debug(`No restart attempts left. Exiting.`);
-        process.exit(1);
+        currentRestartAttempt++;
+        cluster.fork();
     });
 }
 
 if (cluster.isWorker) {
     // @ts-ignore
     const package = require('./package.json');
-    const { main, name } = package;
+    const { main } = package;
 
     if (!main) {
         console.error('Missing main field in package.json');
