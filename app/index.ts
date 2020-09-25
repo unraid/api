7164e5810df8db20c7808a032a44510e609b1e8c
Alexis Tyler <xo@wvvw.me>
2020-09-25 10:49:43 +0930

    fix: move sentry into correct file

--- app/index.ts
+++ app/index.ts
@@ -3,21 +3,9 @@
  * Written by: Alexis Tyler
  */
 
-import os from 'os';
 import am from 'am';
-import * as Sentry from '@sentry/node';
 import { core, loadServer } from '@unraid/core';
 import { server } from './server';
-
-// Send errors to server if enabled
-Sentry.init({
-	dsn: process.env.SENTRY_DSN,
-	tracesSampleRate: 1.0,
-	release: require('../package.json').short(),
-	environment: process.env.NODE_ENV,
-	serverName: os.hostname(),
-	enabled: Boolean(process.env.SENTRY_DSN)
-});
 
 // Boot app
 am(async () => {
@@ -28,9 +16,6 @@ am(async () => {
 	await loadServer('graphql-api', server);
 }, (error: NodeJS.ErrnoException) => {
 	// We should only end here if core has an issue loading
-
-	// Send last exception to Sentry
-	Sentry.captureException(error);
 
 	// Log last error
 	console.error(error.message);
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // @ts-check
+const os = require('os');
 const path = require('path');
+const Sentry = require('@sentry/node');
 const package = require('./package.json');
 const { main } = package;
+
+// Send errors to server if enabled
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	tracesSampleRate: 1.0,
+	release: require('./package.json').version,
+	environment: process.env.NODE_ENV,
+	serverName: os.hostname(),
+	enabled: Boolean(process.env.SENTRY_DSN)
+});
 
 if (!main) {
     throw new Error('Missing main field in package.json');
@@ -18,6 +30,11 @@ if (process.env.NODE_ENV === 'development') {
     }
 }
 
-
-const mainPath = path.resolve(__dirname, main);
-require(mainPath);
+try {
+    const mainPath = path.resolve(__dirname, main);
+    require(mainPath);
+} catch (error) {
+    Sentry.captureException(error);
+    console.error(error.message);
+    process.exit(1);
+}
