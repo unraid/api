9625a573769b9eb20eb2f84686e3b72440164137
Alexis Tyler <xo@wvvw.me>
2020-11-20 11:10:27 +1030

    refactor: ensure we always log level changes

--- app/core/log.ts
+++ app/core/log.ts
@@ -13,7 +13,7 @@ const levels = ['error', 'warn', 'info', 'debug', 'trace'] as const;
 const transports = ['console', 'syslog'] as const;
 
 class Logger {
-    public level = 'trace' as typeof levels[number];
+    public level = 'info' as typeof levels[number];
     public levels = levels;
     public transport = (process.env.DEBUG ? 'console' : 'syslog') as typeof transports[number];
     public transports = transports;
@@ -74,7 +74,7 @@ class Logger {
                 this.level = this.levels[index + 1];
             }
         
-            this.info('Log level updated to %s.', this.level);
+            this._log('debug', 'Log level updated to %s.', [this.level]);
         });
 
     }
@@ -82,12 +82,16 @@ class Logger {
     log(level: typeof levels[number], message: string, args: any[]) {
         // Only enable logging when `this.level >= level`.
         if (this.levels.indexOf(this.level) >= this.levels.indexOf(level)) {
-            if (this.transport === 'console') {
-                console[level].call(console, `[${this.addColourToString(this.colour(level), level)}] ${this.prefix}${message}`, ...this.redact.map(args));
-            }
-            if (this.transport === 'syslog') {
-                this.syslog[level](format(message, ...args.map(arg => this.redact.map(arg))));
-            }
+            this._log(level, message, args);
+        }
+    }
+    
+    private _log(level: typeof levels[number], message: string, args: any[]) {
+        if (this.transport === 'console') {
+            console[level].call(console, `[${this.addColourToString(this.colour(level), level)}] ${this.prefix}${message}`, ...this.redact.map(args));
+        }
+        if (this.transport === 'syslog') {
+            this.syslog[level](format(message, ...args.map(arg => this.redact.map(arg))));
         }
     }
 
