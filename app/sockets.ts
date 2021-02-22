e5ad51bb1c2e4f6230a185dfa936fec85077649c
Alexis Tyler <xo@wvvw.me>
2021-02-22 13:18:22 +1030

    fix: use const for socket names

--- app/sockets.ts
+++ app/sockets.ts
@@ -1,4 +1,4 @@
 import { InternalGraphql } from './mothership/sockets/internal-graphql';
 import { MothershipSocket } from './mothership/sockets/mothership';
 
-export const sockets = new Map<string, MothershipSocket | InternalGraphql>();
+export const sockets = new Map<'relay' | 'internalGraphql', MothershipSocket | InternalGraphql>();
