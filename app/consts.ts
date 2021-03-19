0090589d49d5aefd8ac77af47c797bf43edf4d3c
Alexis Tyler <xo@wvvw.me>
2021-03-19 15:22:23 +1030

    fix: ensure we allow staging env to override consts

--- app/consts.ts
+++ app/consts.ts
@@ -22,14 +22,14 @@ export const ONE_MINUTE = 60 * ONE_SECOND;
 /**
  * Relay ws link.
  */
-export const MOTHERSHIP_RELAY_WS_LINK = process.env.MOTHERSHIP_RELAY_WS_LINK ? process.env.MOTHERSHIP_RELAY_WS_LINK : 'wss://mothership.unraid.net/relay';
+export const MOTHERSHIP_RELAY_WS_LINK = process.env.MOTHERSHIP_RELAY_WS_LINK ?? (process.env.ENVIRONMENT === 'staging' ? 'wss://staging.mothership.unraid.net/relay' : 'wss://mothership.unraid.net/relay');
 
 /**
  * Graphql link.
  */
-export const MOTHERSHIP_GRAPHQL_LINK = process.env.MOTHERSHIP_GRAPHQL_LINK ? process.env.MOTHERSHIP_GRAPHQL_LINK : 'https://mothership.unraid.net/graphql';
+export const MOTHERSHIP_GRAPHQL_LINK = process.env.MOTHERSHIP_GRAPHQL_LINK ?? (process.env.ENVIRONMENT === 'staging' ? 'https://staging.mothership.unraid.net/graphql' : 'https://mothership.unraid.net/graphql');
 
 /**
  * Internal ws link.
  */
-export const INTERNAL_WS_LINK = process.env.INTERNAL_WS_LINK ? process.env.INTERNAL_WS_LINK : internalWsAddress();
+export const INTERNAL_WS_LINK = process.env.INTERNAL_WS_LINK ?? internalWsAddress();
