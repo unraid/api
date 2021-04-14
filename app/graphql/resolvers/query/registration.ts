07207b87f3b494bbe5872f16f85f65fe4fbf8524
Alexis Tyler <xo@wvvw.me>
2021-04-15 08:54:55 +0930

    fix: replace - with = in keyfile

--- app/graphql/resolvers/query/registration.ts
+++ app/graphql/resolvers/query/registration.ts
@@ -17,7 +17,7 @@ export default async (_: unknown, __: unknown, context: Context) => {
 
 	const keyFile = varState.data.regFile ? await promises.readFile(varState.data.regFile, 'utf-8').then(file => {
 		const fileAsBase64 = Buffer.from(file).toString('base64');
-		return fileAsBase64.trim().replace(/\+/g, '-').replace(/\//g, '_').replace(/-/g, '');
+		return fileAsBase64.trim().replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 	}) : '';
 
 	return {
