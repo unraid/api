6e993d5cf2c88ba38d166712f3f0c7c64db6c807
Alexis Tyler <xo@wvvw.me>
2021-04-14 21:53:25 +0930

    fix: add flash and registration query resolvers

--- app/graphql/resolvers/query/index.ts
+++ app/graphql/resolvers/query/index.ts
@@ -3,18 +3,22 @@
  * Written by: Alexis Tyler
  */
 import display from './display';
+import flash from './flash';
 import info from './info';
 import online from './online';
 import owner from './owner';
+import registration from './registration';
 import server from './server';
 import servers from './servers';
 import vms from './vms';
 
 export const Query = {
 	display,
+	flash,
 	info,
 	online,
 	owner,
+	registration,
 	vms,
 	server,
 	servers
