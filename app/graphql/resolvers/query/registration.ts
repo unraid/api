eb5272c998a18e11b1f4b9ba619a2bf8b1b7d233
Alexis Tyler <xo@wvvw.me>
2021-04-14 21:52:20 +0930

    feat: registration query endpoint

---
+++ app/graphql/resolvers/query/registration.ts
@@ -0,0 +1,31 @@
+/*!
+ * Copyright 2021 Lime Technology Inc. All rights reserved.
+ * Written by: Alexis Tyler
+ */
+
+import { promises } from 'fs';
+import { varState } from '../../../core/states';
+import { ensurePermission } from '../../../core/utils';
+import { Context } from '../../schema/utils';
+
+export default async (_: unknown, __: unknown, context: Context) => {
+	ensurePermission(context.user, {
+		resource: 'registration',
+		action: 'read',
+		possession: 'any'
+	});
+
+	const keyFile = varState.data.regFile ? await promises.readFile(varState.data.regFile, 'utf-8').then(file => {
+		const fileAsBase64 = Buffer.from(file).toString('base64');
+		return fileAsBase64.trim().replace('+', '-').replace('/', '_').replace('=', '');
+	}) : '';
+
+	return {
+		guid: varState.data.regGuid,
+		type: varState.data.regTy,
+		keyFile: {
+			location: varState.data.regFile,
+			contents: keyFile
+		}
+	};
+};
