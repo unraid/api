26e5e51539d82fb23ca84e561e3c6159c6f4339f
Alexis Tyler <xo@wvvw.me>
2021-07-01 00:03:43 +0930

    fix: sending notification

--- app/graphql/resolvers/mutation/notifications.ts
+++ app/graphql/resolvers/mutation/notifications.ts
@@ -7,7 +7,7 @@ import { ensurePermission } from '../../../core/utils';
 import { mothership } from '../../../mothership/subscribe-to-servers';
 import { Context } from '../../schema/utils';
 
-export const sendNotification = async (_: unknown, __: unknown, context: Context) => {
+export const sendNotification = async (_: unknown, args: { notification: Notification }, context: Context) => {
 	const { user } = context;
 
 	// Check permissions
@@ -17,15 +17,11 @@ export const sendNotification = async (_: unknown, __: unknown, context: Context
 		possession: 'own'
 	});
 
-	console.log('sendNotification mutation', { _, __, context });
-
-	const notification = {};
-
 	// Prepare query
 	const query = mothership.request({
 		query: 'mutation($notification:NotificationInput!){sendNotification(notification:$notification){title subject description importance link status}}',
 		variables: {
-			notification
+			notification: args.notification
 		}
 	});
 
