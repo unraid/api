90f04eb5a0b0c489b8d70bcdaaa116426c9675b4
Alexis <xo@wvvw.me>
2021-08-30 13:54:04 +0930

    fix: remove loading external permissions

--- app/core/permissions.ts
+++ app/core/permissions.ts
@@ -1,47 +1,9 @@
-import fs from 'fs';
-import path from 'path';
-import { paths } from './paths';
 import { coreLogger } from './log';
 import { permissions as defaultPermissions } from './default-permissions';
 import { AccessControl } from 'accesscontrol';
 
-interface Permission {
-	role?: string;
-	resource: string;
-	action: string;
-	attributes: string;
-}
-
-type Permissions = Record<string, {
-	extends?: string | string[];
-	permissions?: Permission[];
-}>;
-
-const loadPermissionsFile = (filePath: string) => {
-	// Create file if it's missing
-	if (!fs.existsSync(filePath)) {
-		const directoryPath = filePath.slice(0, filePath.lastIndexOf('/') + 1);
-		fs.mkdir(directoryPath, { recursive: true }, () => {
-			fs.writeFileSync(filePath, JSON.stringify(defaultPermissions, null, 2));
-		});
-
-		return defaultPermissions;
-	}
-
-	// Load newly created permissions file
-	return JSON.parse(fs.readFileSync(filePath).toString('utf8')) as Partial<Permissions>;
-};
-
 const getPermissions = () => {
-	try {
-		const unraidDataDir = paths.get('unraid-data')!;
-		const permissionsConfigPath = path.join(unraidDataDir, 'permissions.json');
-
-		// Load permissions file
-		return loadPermissionsFile(permissionsConfigPath);
-	} catch {}
-
-	// Fallback to built in permissions
+	// Use built in permissions
 	return defaultPermissions;
 };
 
