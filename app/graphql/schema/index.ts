0af7a565fdd7fe7f0a6b61a44ee6a7c313dea43f
Alexis <xo@wvvw.me>
2021-08-30 13:58:43 +0930

    feat: add vms count to graphql

--- app/graphql/schema/index.ts
+++ app/graphql/schema/index.ts
@@ -27,6 +27,7 @@ const files = [
 	'./dist/types/graphql/schema/types/info/os.graphql',
 	'./dist/types/graphql/schema/types/info/system.graphql',
 	'./dist/types/graphql/schema/types/info/versions.graphql',
+	'./dist/types/graphql/schema/types/info/vms.graphql',
 	'./dist/types/graphql/schema/types/notifications/notifications.graphql',
 	'./dist/types/graphql/schema/types/owner/owner.graphql',
 	'./dist/types/graphql/schema/types/plugins/plugin.graphql',
