47e687b88632e3ca290a9e1cb33b494558429682
Alexis <xo@wvvw.me>
2021-12-21 09:44:21 +1030

    fix: typo

--- app/common/get-certs.ts
+++ app/common/get-certs.ts
@@ -4,7 +4,7 @@ import { attemptReadFileSync } from '../core/utils/misc/attempt-read-file-sync';
 
 export const getCerts = () => {
 	const nonWildcardCert = attemptReadFileSync(paths.get('non-wildcard-ssl-certificate')!);
-	const wildcardCert = attemptReadFileSync(paths.get('non-wildcard-ssl-certificate')!);
+	const wildcardCert = attemptReadFileSync(paths.get('wildcard-ssl-certificate')!);
 	return {
 		nonWildcard: nonWildcardCert ? pki.certificateFromPem(nonWildcardCert)?.subject?.attributes?.[0]?.value as string : undefined,
 		wildcard: wildcardCert ? (pki.certificateFromPem(wildcardCert)?.subject?.attributes?.[0]?.value as string).replace('*.', '') : undefined
