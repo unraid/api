828db87b73b110859c644e963b76a94b14602d94
Alexis <xo@wvvw.me>
2022-01-03 13:09:11 +1030

    feat: add expiration to registration endpoint

--- app/graphql/resolvers/query/registration.ts
+++ app/graphql/resolvers/query/registration.ts
@@ -14,10 +14,15 @@ export default async (_: unknown, __: unknown, context: Context) => {
 		possession: 'any'
 	});
 
+	const isTrial = varState.data.regTy.toLowerCase() === 'trial';
+	const isExpired = varState.data.regTy.includes('expired');
+
 	return {
 		guid: varState.data.regGuid,
 		type: varState.data.regTy,
 		state: varState.data.regState,
+		// Based on https://github.com/unraid/dynamix.unraid.net/blob/c565217fa8b2acf23943dc5c22a12d526cdf70a1/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/state.php#L64
+		expiration: 1_000 * ((isTrial || isExpired) ? Number(varState.data.regTm2) : 0),
 		keyFile: {
 			location: varState.data.regFile,
 			contents: await getKeyFile()
