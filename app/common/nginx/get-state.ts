27bd4a3bf38fd0c7dfa3ae2fbe0becc6c7dc7e63
Alexis <xo@wvvw.me>
2022-01-20 07:07:12 +1030

    fix: use correct path for nginx state file

--- app/common/nginx/get-state.ts
+++ app/common/nginx/get-state.ts
@@ -3,7 +3,7 @@ import { loadState } from '../../core/utils/misc/load-state';
 import { NginxState } from '../../types/nginx';
 
 export const getNginxState = () => {
-	const filePath = '/var/local/nginx/nginx.ini';
+	const filePath = '/var/local/nginx/state.ini';
 	if (!existsSync(filePath)) return {};
 	return loadState<Partial<NginxState>>(filePath);
 };
