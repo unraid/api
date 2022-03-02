d4614a37498544305a615de02185eb500f996eac
Alexis Tyler <xo@wvvw.me>
2022-03-02 14:33:08 +1030

    fix: use flashGuid for owners object

--- app/graphql/resolvers/query/owner.ts
+++ app/graphql/resolvers/query/owner.ts
@@ -5,6 +5,7 @@
 
 import { logger } from '../../../core';
 import { apiManager } from '../../../core/api-manager';
+import { varState } from '../../../core/states';
 import { ensurePermission } from '../../../core/utils';
 import { Context, getServers } from '../../schema/utils';
 
@@ -18,33 +19,27 @@ export default async (_: unknown, __: unknown, context: Context) => {
 		possession: 'any'
 	});
 
-	// This should always return the server with a matching "my servers" key as this is the owner of the server the API is running on
-	const apiKey = apiManager.getValidKeys().find(key => key.name === 'my_servers')?.key.toString()!;
-
-	// Check if the user's key exists
-	if (!apiKey) {
-		logger.trace('While resolving "owner" we failed fetching servers as no "my_servers" API key was found.');
-		return null;
-	}
+	// This should always return the server with a matching guid as this is this server
+	const flashGuid = varState.data.flashGuid;
 
 	// Check the user has servers in their account
-	const servers = apiKey ? await getServers() : [];
+	const servers = flashGuid ? await getServers() : [];
 	if (servers.length === 0) {
 		logger.trace('While resolving "owner" we found no servers.');
 		return null;
 	}
 
 	// Check if we got a server with a matching API key
-	const server = apiKey ? servers?.find(server => server.apikey === apiKey) : null;
+	const server = flashGuid ? servers?.find(server => server.guid === flashGuid) : null;
 	if (!server) {
-		logger.trace('While resolving "owner" we found no server matching our "my_servers" API key.');
+		logger.trace('While resolving "owner" we found no server response.');
 		return null;
 	}
 
 	// Check if the server we found had an owner object
 	const owner = server?.owner;
 	if (!owner) {
-		logger.trace('While resolving "owner" we found no owner matching our "my_servers" API key.');
+		logger.trace('While resolving "owner" we found no owner matching our flashGuid.');
 		return null;
 	}
 
