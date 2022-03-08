799aefa62b39421876eb98964e7b6cbd805e4cd1
Alexis Tyler <xo@wvvw.me>
2022-03-08 16:56:48 +1030

    fix: change imports around

--- app/graphql/resolvers/index.ts
+++ app/graphql/resolvers/index.ts
@@ -7,19 +7,13 @@ import GraphQLJSON from 'graphql-type-json';
 import GraphQLLong from 'graphql-type-long';
 import GraphQLUUID from 'graphql-type-uuid';
 import { GraphQLDateTime } from 'graphql-iso-date';
-import { Query } from './query';
-import { Mutation } from './mutation';
-import { Subscription } from './subscription';
-import { UserAccount } from './user-account';
+
+export { Query } from './query';
+export { Mutation } from './mutation';
+export { Subscription } from './subscription';
+export { UserAccount } from './user-account';
 
 export const JSON = GraphQLJSON;
 export const Long = GraphQLLong;
 export const UUID = GraphQLUUID;
 export const DateTime = GraphQLDateTime;
-
-export {
-	Query,
-	Mutation,
-	Subscription,
-	UserAccount
-};
