17030e788f66007326f10ae22a154147b3b8c31b
Alexis Tyler <xo@wvvw.me>
2022-03-08 17:21:09 +1030

    chore: log imports for graphql

--- app/graphql/resolvers/index.ts
+++ app/graphql/resolvers/index.ts
@@ -6,14 +6,33 @@
 import GraphQLJSON from 'graphql-type-json';
 import GraphQLLong from 'graphql-type-long';
 import GraphQLUUID from 'graphql-type-uuid';
-import { GraphQLDateTime } from 'graphql-iso-date/dist/dateTime/index.js';
-
-export { Query } from './query';
-export { Mutation } from './mutation';
-export { Subscription } from './subscription';
-export { UserAccount } from './user-account';
+import { GraphQLDateTime } from 'graphql-iso-date';
+import { Query } from './query';
+import { Mutation } from './mutation';
+import { Subscription } from './subscription';
+import { UserAccount } from './user-account';
 
 export const JSON = GraphQLJSON;
 export const Long = GraphQLLong;
 export const UUID = GraphQLUUID;
 export const DateTime = GraphQLDateTime;
+
+if (process.env.PLEASE_LOG_IMPORTS) {
+	console.log({
+		JSON,
+		Long,
+		UUID,
+		DateTime,
+		Query,
+		Mutation,
+		Subscription,
+		UserAccount
+	});
+}
+
+export {
+	Query,
+	Mutation,
+	Subscription,
+	UserAccount
+};
