1f7c1a6739930cb0c9694ca0b06fc0250a7630cd
Alexis Tyler <xo@wvvw.me>
2022-05-18 13:32:25 +0930

    fix: ensure reconnection timeout is ms not new time

--- app/mothership/get-relay-connection-status.ts
+++ app/mothership/get-relay-connection-status.ts
@@ -2,5 +2,5 @@ import { store } from './store';
 
 export const readyStates = ['CONNECTING' as const, 'OPEN' as const, 'CLOSING' as const, 'CLOSED' as const, 'RECONNECTING' as const];
 export const getRelayConnectionStatus = () => readyStates[store.relay?._ws?.readyState ?? 3];
-export const getRelayReconnectingTimeout = () => store.timeout;
+export const getRelayReconnectingTimeout = () => store.timeout ? (store.timeout - Date.now()) : 0;
 export const getRelayDisconnectionReason = () => store.reason;
