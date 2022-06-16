568860eb17154f2648a4ab3fcb0eac5435350d6a
Alexis Tyler <xo@wvvw.me>
2022-06-16 10:16:50 +0930

    chore: CRLF

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
-    
-    extends: [
-        '@unraid'
-    ],
-    rules: {
-        curly: ["error", "multi-line"],
-        "quotes": [2, "single", { "avoidEscape": true }]
-    },
-    overrides: [{
-        files: ['*.ts', '*.tsx'],
-        parser: '@typescript-eslint/parser',
-        rules: {
-            "@typescript-eslint/member-ordering": ["off"],
-        }
-    }]
-}
+	root: true,
+
+	extends: [
+		'@unraid'
+	],
+	rules: {
+		curly: ['error', 'multi-line'],
+		quotes: [2, 'single', { avoidEscape: true }]
+	},
+	overrides: [{
+		files: ['*.ts', '*.tsx'],
+		parser: '@typescript-eslint/parser',
+		rules: {
+			'@typescript-eslint/member-ordering': ['off']
+		}
+	}]
+};
