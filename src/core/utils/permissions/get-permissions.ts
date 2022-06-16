bb42417395655c9063da5849af21e83c93dc8fdb
Alexis Tyler <xo@wvvw.me>
2022-06-16 20:31:04 +0930

    fix: types of getPermisions

--- src/core/utils/permissions/get-permissions.ts
+++ src/core/utils/permissions/get-permissions.ts
@@ -6,11 +6,11 @@
 import { ac } from '@app/core/permissions';
 
 /**
- * Get permissions from an {@link https://onury.io/accesscontrol/?api=ac#AccessControl | AccessControl} role.
- * @param role The {@link https://onury.io/accesscontrol/?api=ac#AccessControl | AccessControl} role to be looked up.
+ * Get permissions from an {@link https://onury.io/accesscontrol/?api=ac#AccessControl AccessControl} role.
+ * @param role The {@link https://onury.io/accesscontrol/?api=ac#AccessControl AccessControl} role to be looked up.
  */
-export const getPermissions = (role: string): Record<string, unknown> => {
-	const grants: Record<string, unknown> = ac.getGrants();
+export const getPermissions = (role: string): Record<string, Record<string, string[]>> => {
+	const grants: Record<string, Record<string, string[]>> = ac.getGrants();
 	const { $extend, ...roles } = grants[role] ?? {};
 	const inheritedRoles = Array.isArray($extend) ? $extend.map(role => getPermissions(role))[0] : {};
 	return Object.assign({}, roles, inheritedRoles);
