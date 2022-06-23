ef608ea4c71a646c3d46ea5c00c468a0fb57097a
Alexis Tyler <xo@wvvw.me>
2022-06-23 12:14:19 +0930

    fix: only import command as it's used

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,25 +3,8 @@ import segfaultHandler from 'segfault-handler';
 import { parse } from 'ts-command-line-args';
 import dotEnv from 'dotenv';
 import { cliLogger } from '@app/core/log';
-import { start } from '@app/cli/commands/start';
-import { stop } from '@app/cli/commands/stop';
 import { Flags, mainOptions, options, args } from '@app/cli/options';
-import { report } from '@app/cli/commands/report';
 import { setEnv } from '@app/cli/set-env';
-import { restart } from '@app/cli/commands/restart';
-import { switchEnv } from '@app/cli/commands/switch-env';
-import { version } from '@app/cli/commands/version';
-import { status } from '@app/cli/commands/status';
-
-const commands = {
-	start,
-	stop,
-	restart,
-	'switch-env': switchEnv,
-	version,
-	status,
-	report
-};
 
 const command = mainOptions.command as unknown as string;
 
@@ -50,6 +33,17 @@ export const main = async (...argv: string[]) => {
 		// Run help command
 		parse<Flags>(args, { ...options, partial: true, stopAtFirstUnknown: true, argv: ['-h'] });
 	}
+
+	// Only import the command we need when we use it
+	const commands = {
+		start: import('@app/cli/commands/start').then(pkg => pkg.start),
+		stop: import('@app/cli/commands/stop').then(pkg => pkg.stop),
+		restart: import('@app/cli/commands/restart').then(pkg => pkg.restart),
+		'switch-env': import('@app/cli/commands/switch-env').then(pkg => pkg.switchEnv),
+		version: import('@app/cli/commands/version').then(pkg => pkg.version),
+		status: import('@app/cli/commands/status').then(pkg => pkg.status),
+		report: import('@app/cli/commands/report').then(pkg => pkg.report)
+	};
 
 	// Unknown command
 	if (!Object.keys(commands).includes(command)) {
