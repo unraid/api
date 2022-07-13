158e78dd84454a9bb96c3a138ad776786d86367d
Alexis Tyler <xo@wvvw.me>
2022-07-13 17:25:31 +0930

    test: add query/cloud/check-relay

---
+++ src/__test__/graphql/resolvers/query/cloud/check-relay.test.ts
@@ -0,0 +1,28 @@
+import { checkRelay } from '@app/graphql/resolvers/query/cloud/check-relay';
+import { expect, test, vi } from 'vitest';
+
+test('Defaults', async () => {
+	const { relayStore } = await import('@app/mothership/store');
+	relayStore.code = undefined;
+	relayStore.reason = undefined;
+	relayStore.relay = undefined;
+	relayStore.timeout = undefined;
+	expect(checkRelay()).toStrictEqual({
+		error: '',
+		status: 'closed',
+		timeout: 0
+	});
+});
+
+test('Reconnecting', async () => {
+	const { relayStore } = await import('@app/mothership/store');
+	relayStore.code = 12;
+	relayStore.reason = 'SERVICE_RESTART';
+	relayStore.relay = undefined;
+	relayStore.timeout = Date.now() + 60_000;
+	expect(checkRelay()).toStrictEqual({
+		error: 'SERVICE_RESTART',
+		status: 'closed',
+		timeout: 60_000
+	});
+});
