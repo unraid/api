a296f7d63df4c3066e84e58fecdf99431668472e
Eli Bosley <ekbosley@gmail.com>
2022-08-26 10:00:59 -0700

    rebase fixes

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,7 +2,6 @@
 
 rm -f /app/deploy/**/**/*
 
-/app/scripts/copy-full-version.sh
 npm run build
 npm run clean
 /app/scripts/copy-full-version.sh
@@ -11,7 +10,6 @@ mkdir -p /app/deploy/pre-pack/
 cp /app/dist/api /app/deploy/pre-pack/unraid-api
 cp /app/.env.production /app/deploy/pre-pack/.env.production
 cp /app/.env.staging /app/deploy/pre-pack/.env.staging
-cp /app/package-deployment.json /app/deploy/pre-pack/package.json
 cp /app/README.md /app/deploy/pre-pack/
 cd /app/deploy/pre-pack
 npm pack
