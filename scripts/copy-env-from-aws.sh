86e0fb4a9709d44912b3c5db3da473594385a9a4
Eli Bosley <ekbosley@gmail.com>
2022-08-26 08:22:09 -0400

    add copy-env script

--- scripts/copy-env-from-aws.sh
+++ scripts/copy-env-from-aws.sh
@@ -1,18 +1,10 @@
 
 # Pull Staging Env File
-if [ -n "$STAGING_SECRET_ID" ]; then
-aws secretsmanager get-secret-value --secret-id $STAGING_SECRET_ID --region=us-east-1 | \
+aws secretsmanager get-secret-value --secret-id api.env.staging --region=us-west-2 | \
 jq -r '.SecretString' | \
 jq -r "to_entries|map(\"\(.key)=\\\"\(.value|tostring)\\\"\")|.[]" > .env.staging
-else 
-echo "To copy staging secrets, please set the STAGING_SECRET_ID variable";
-fi
 
 # Pull Production Env File
-if [ -n "$PRODUCTION_SECRET_ID" ]; then
-aws secretsmanager get-secret-value --secret-id $PRODUCTION_SECRET_ID --region=us-east-1 | \
+aws secretsmanager get-secret-value --secret-id api.env.production --region=us-west-2 | \
 jq -r '.SecretString' | \
 jq -r "to_entries|map(\"\(.key)=\\\"\(.value|tostring)\\\"\")|.[]" > .env.production
-else
-echo "To copy production secrets, please set the PRODUCTION_SECRET_ID variable";
-fi
