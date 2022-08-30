e23c0c533fb7aaaeb4c568232cd11b238473090b
Eli Bosley <ekbosley@gmail.com>
2022-08-30 08:17:44 -0700

    fix: load env properly in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { config } from 'dotenv';
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		coverage: {
-			all: true,
-			include: ['src/**/*'],
-			reporter: ['text', 'json', 'html'],
+export default defineConfig(() => {
+	config({ path: './.env.test' });
+	// Manually set NODE_ENV to make sure we always run tests in test mode
+	process.env.NODE_ENV = 'test';
+	return {
+
+		plugins: [tsconfigPaths()],
+		test: {
+			coverage: {
+				all: true,
+				include: ['src/**/*'],
+				reporter: ['text', 'json', 'html'],
+			},
+			clearMocks: true,
 		},
-		clearMocks: true,
-		setupFiles: ['dotenv/config'],
-	},
+	};
 });
+
