951ec91218f18a5926c5b43a178df0f27472db15
Alexis Tyler <xo@wvvw.me>
2022-09-07 14:40:20 +0930

    fix: start:dev script

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,6 @@ segfaultHandler.registerHandler('/var/log/unraid-api/crash.log', (signal, addres
 import { am } from 'am';
 import { main } from '@app/cli/index';
 import { internalLogger } from '@app/core/log';
-import '@app/store';
 
 void am(main, (error: unknown) => {
 	internalLogger.fatal((error as Error).message);
