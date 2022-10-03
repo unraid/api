acccf901bc67cde23c2ef3774d7c7809d354b77e
Eli Bosley <ekbosley@gmail.com>
2022-10-03 11:58:25 -0400

    fix: better testing for error conditions

--- api/src/__test__/core/utils/misc/send-form-to-keyserver.test.ts
+++ api/src/__test__/core/utils/misc/send-form-to-keyserver.test.ts
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest';
 
 import { sendFormToKeyServer } from '@app/core/utils/misc/send-form-to-keyserver';
 
-test('when server is unreachable, is handled', async () => {
+test('when server is unreachable, rejects and throws error', async () => {
 	await expect(sendFormToKeyServer('INVLALID_URL', { myData: 'is_invalid' })).rejects.toThrowErrorMatchingInlineSnapshot('"Invalid URL"');
 
-	await expect(sendFormToKeyServer('http://thisisaninvalidwebsiteaddress12345', { myData: 'is_invalid' })).rejects.toThrowErrorMatchingInlineSnapshot('"getaddrinfo ENOTFOUND thisisaninvalidwebsiteaddress12345"');
+	await expect(sendFormToKeyServer('http://thisisaninvalidwebsiteaddress12345', { myData: 'is_invalid' })).rejects.toThrowError();
 });
