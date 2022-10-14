3b9f52121b4b266d2339e76efd5328a90bff02cf
Alexis Tyler <xo@wvvw.me>
2022-10-14 15:51:33 +1030

    fix: log on error writing config

--- api/src/store/sync/config-disk-sync.ts
+++ api/src/store/sync/config-disk-sync.ts
@@ -8,26 +8,29 @@ import isEqual from 'lodash/isEqual';
 import { getDiff } from 'json-difference';
 
 export const syncConfigToDisk: StoreSubscriptionHandler = async lastState => {
-	const { config, paths } = store.getState();
-	const configPath = paths['myservers-config'];
-	if (config.status !== FileLoadStatus.LOADED) return;
+	try {
+		const { config, paths } = store.getState();
+		const configPath = paths['myservers-config'];
+		if (config.status !== FileLoadStatus.LOADED) return;
 
-	// Create new state
-	const newConfig = getWriteableConfig(config);
-	const oldConfig = lastState?.config ? getWriteableConfig(lastState.config) : null;
+		// Create new state
+		const newConfig = getWriteableConfig(config);
+		const oldConfig = lastState?.config ? getWriteableConfig(lastState.config) : null;
 
-	// We already wrote this to the file
-	if (isEqual(newConfig, oldConfig)) {
-		return;
+		// We already wrote this to the file
+		if (isEqual(newConfig, oldConfig)) return;
+
+		logger.addContext('diff', getDiff(oldConfig ?? {}, newConfig, true));
+		logger.trace('Dumping MyServers config back to file');
+		logger.removeContext('diff');
+
+		// Stringify state
+		const stringifiedData = safelySerializeObjectToIni(newConfig);
+
+		// Update config file
+		await writeFile(configPath, stringifiedData);
+	} catch (error: unknown) {
+		if (!(error instanceof Error)) throw new Error(`Failed writing config to disk with unknown error "${String(error)}"`);
+		logger.error('Failed writing config to disk with "%s"', error.message);
 	}
-
-	logger.addContext('diff', getDiff(oldConfig ?? {}, newConfig, true));
-	logger.trace('Dumping MyServers config back to file');
-	logger.removeContext('diff');
-
-	// Stringify state
-	const stringifiedData = safelySerializeObjectToIni(newConfig);
-
-	// Update config file
-	await writeFile(configPath, stringifiedData);
 };
