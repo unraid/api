d6db1a01d9ee66104ccb0341270c0e4c4b08d7b0
Eli Bosley <ekbosley@gmail.com>
2022-10-15 11:58:17 -0700

    fix: add addl token information, waiting on UPC

--- api/src/cli/commands/validate-token.ts
+++ api/src/cli/commands/validate-token.ts
@@ -1,25 +1,32 @@
 import { JWKS_LOCAL_PAYLOAD, JWKS_REMOTE_LINK } from '@app/consts';
 import { cliLogger } from '@app/core';
-import { createLocalJWKSet, createRemoteJWKSet, jwtVerify } from 'jose';
+import { createLocalJWKSet, createRemoteJWKSet, JWTPayload, jwtVerify } from 'jose';
+import { setEnv } from '@app/cli/set-env';
 
 const JWKSOffline = createLocalJWKSet(JWKS_LOCAL_PAYLOAD);
 const JWKSOnline = createRemoteJWKSet(new URL(JWKS_REMOTE_LINK));
 
-export const validateToken = async (...argv: string[]): Promise<boolean> => {
-	if (argv.length > 1) {
-		return false;
+export const validateToken = async (...argv: string[]): Promise<void> => {
+	// @TODO Please add ability to read the users ID from myservers.cfg
+	// and positively match them to determine whether token is valid for that user
+	setEnv('LOG_TYPE', 'raw');
+
+	if (argv.length > 2) {
+		cliLogger.error('Too many args, please pass just the token');
+		return;
 	}
 
-	const token = argv[0];
+	const token = argv[1];
 
 	let caughtError: null | unknown = null;
+	let tokenPayload: null | JWTPayload = null;
 	try {
 		cliLogger.trace('Attempting to validate token with local key');
-		await jwtVerify(token, JWKSOffline);
+		tokenPayload = (await jwtVerify(token, JWKSOffline)).payload;
 	} catch (error: unknown) {
 		try {
 			cliLogger.trace('Local validation failed for key, trying remote validation');
-			await jwtVerify(token, JWKSOnline);
+			tokenPayload = (await jwtVerify(token, JWKSOnline)).payload;
 		} catch (error: unknown) {
 			caughtError = error;
 		}
@@ -27,8 +34,7 @@ export const validateToken = async (...argv: string[]): Promise<boolean> => {
 
 	if (caughtError) {
 		cliLogger.error('Caught error validating jwt token', caughtError);
-		return false;
 	}
 
-	return true;
+	cliLogger.info('%o', tokenPayload);
 };
