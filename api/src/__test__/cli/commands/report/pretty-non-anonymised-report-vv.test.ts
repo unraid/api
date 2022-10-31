421a9939fa3afd7a47e0eb9c9ce1eeccf652871d
Eli Bosley <ekbosley@gmail.com>
2022-10-31 13:44:27 -0400

    fix: update report to be maintainable

--- api/src/__test__/cli/commands/report/pretty-non-anonymised-report-vv.test.ts
+++ api/src/__test__/cli/commands/report/pretty-non-anonymised-report-vv.test.ts
@@ -63,10 +63,8 @@ vi.mock('process');
 
 test('Returns a pretty non-anonymised report with -vv', async () => {
 	const { writeStub, closeStub } = await import('readline') as unknown as { writeStub: SpyInstance; closeStub: SpyInstance };
-	const { cliLogger } = await import('@app/core/log');
+
 	const { stdout } = await import('process');
-	const cliDebugLoggerSpy = vi.spyOn(cliLogger, 'debug');
-	const cliTraceLoggerSpy = vi.spyOn(cliLogger, 'trace');
 
 	// The args we'll pass to the report function
 	const args = ['-vv'];
@@ -78,49 +76,22 @@ test('Returns a pretty non-anonymised report with -vv', async () => {
 	// This should be run in interactive mode
 	expect(vi.mocked(readline).createInterface.mock.calls.length).toBe(1);
 
-	// Should have logged report to console
-	expect(cliDebugLoggerSpy.mock.calls.length).toBe(1);
-	expect(cliDebugLoggerSpy.mock.calls[0]).toEqual(['Setting process.env[LOG_TYPE] = raw']);
-	expect(cliTraceLoggerSpy.mock.calls.length).toBe(3);
-	expect(cliTraceLoggerSpy.mock.calls[0]).toEqual(['Got unraid OS version "%s"', 'unknown']);
-	expect(cliTraceLoggerSpy.mock.calls[1][0]).toEqual('Cloud response %s');
-	expect(JSON.parse(cliTraceLoggerSpy.mock.calls[1][1])).toMatchInlineSnapshot(`
-		{
-		  "allowedOrigins": [],
-		  "apiKey": {
-		    "error": null,
-		    "valid": true,
-		  },
-		  "cloud": {
-		    "error": null,
-		    "ip": "52.40.54.163",
-		    "status": "ok",
-		  },
-		  "error": null,
-		  "minigraphql": {
-		    "status": "connected",
-		  },
-		  "relay": {
-		    "error": null,
-		    "status": "connected",
-		    "timeout": null,
-		  },
-		}
-	`);
-	expect(cliTraceLoggerSpy.mock.calls[2]).toEqual(['Skipped checking for servers as local graphql is offline']);
-
 	expect(vi.mocked(stdout).write.mock.calls.length).toBe(1);
 	expect(vi.mocked(stdout).write.mock.calls[0][0]).toMatchInlineSnapshot(`
 		"<-----UNRAID-API-REPORT----->
 		SERVER_NAME: Tower
 		ENVIRONMENT: THIS_WILL_BE_REPLACED_WHEN_BUILT
 		UNRAID_VERSION: unknown
-		UNRAID_API_VERSION: THIS_WILL_BE_REPLACED_WHEN_BUILT (stopped)
-		NODE_VERSION: ${process.version}
+		UNRAID_API_VERSION: THIS_WILL_BE_REPLACED_WHEN_BUILT
+		UNRAID_API_STATUS: stopped
+		NODE_VERSION: v18.5.0
 		API_KEY: valid
 		MY_SERVERS: signed out
-		CLOUD: ok [IP=52.40.54.163]
-		RELAY: connected
+		CLOUD: 
+			STATUS: [ok] 
+			IP: [52.40.54.163] 
+		RELAY: 
+			STATUS: [connected]  
 		MINI-GRAPH: connected
 		SERVERS: API is offline
 		ALLOWED_ORIGINS:
