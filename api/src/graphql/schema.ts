01d85d861f6359dd69fccc15b40f48f0d2db7687
Eli Bosley <ekbosley@gmail.com>
2022-11-02 07:08:38 -0700

    fix: remove func directive to fix build

--- api/src/graphql/schema.ts
+++ api/src/graphql/schema.ts
@@ -1,12 +1,8 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { FuncDirective } from '@app/graphql/func-directive';
 import { types as typeDefs } from '@app/graphql/types';
 import * as resolvers from '@app/graphql/resolvers';
 
 export const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers,
-	schemaDirectives: {
-		func: FuncDirective,
-	},
 });
