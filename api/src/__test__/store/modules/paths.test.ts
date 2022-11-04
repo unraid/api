4f659752ff5931bc6cd71d48cf11d74911b60373
Alexis Tyler <xo@wvvw.me>
2022-11-04 15:28:35 +1030

    fix: paths test

--- api/src/__test__/store/modules/paths.test.ts
+++ api/src/__test__/store/modules/paths.test.ts
@@ -3,24 +3,24 @@ import { store } from '@app/store';
 
 test('Returns paths', async () => {
 	const { paths } = store.getState();
-	expect(paths).toMatchInlineSnapshot(`
-		{
-		  "core": "/app/src/store/modules",
-		  "docker-autostart": "/var/lib/docker/unraid-autostart",
-		  "docker-socket": "/var/run/docker.sock",
-		  "dynamix-base": "/app/dev/dynamix",
-		  "dynamix-config": "/boot/config/plugins/dynamix/dynamix.cfg",
-		  "emhttpd-socket": "/var/run/emhttpd.socket",
-		  "htpasswd": "/etc/nginx/htpasswd",
-		  "myservers-base": "/boot/config/plugins/dynamix.my.servers/",
-		  "myservers-config": "/app/dev/Unraid.net/myservers.cfg",
-		  "myservers-config-states": "/app/dev/states/myservers.cfg",
-		  "myservers-env": "/boot/config/plugins/dynamix.my.servers/env",
-		  "parity-checks": "/boot/config/parity-checks.log",
-		  "states": "/app/dev/states",
-		  "unraid-api-base": "/usr/local/bin/unraid-api/",
-		  "unraid-data": "/app/dev/data",
-		  "unraid-version": "/etc/unraid-version",
-		}
+	expect(Object.keys(paths)).toMatchInlineSnapshot(`
+		[
+		  "core",
+		  "unraid-api-base",
+		  "unraid-version",
+		  "unraid-data",
+		  "docker-autostart",
+		  "docker-socket",
+		  "parity-checks",
+		  "htpasswd",
+		  "emhttpd-socket",
+		  "states",
+		  "dynamix-base",
+		  "dynamix-config",
+		  "myservers-base",
+		  "myservers-config",
+		  "myservers-config-states",
+		  "myservers-env",
+		]
 	`);
 });
