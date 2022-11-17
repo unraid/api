81c7d0c27bb8e5cb35aa976a5b24c3e5a9f5ea71
Eli Bosley <ekbosley@gmail.com>
2022-11-17 09:28:27 -0500

    fix: be more specific with the bin for unraid-api (#423)

--- api/src/cli/get-unraid-api-pid.ts
+++ api/src/cli/get-unraid-api-pid.ts
@@ -1,13 +1,15 @@
+import { UNRAID_API_BIN } from '@app/consts';
 import findProcess from 'find-process';
 
 export const getAllUnraidApiPids = async (): Promise<number[]> => {
 	const pids = await findProcess('name', 'unraid-api', true);
-	return pids.filter(allProcess => allProcess.pid !== process.pid)
-		.map(found => found.pid);
+	return pids.filter(allProcess => allProcess.pid !== process.pid && allProcess.cmd.includes(UNRAID_API_BIN))
+		.map(apiProcess => apiProcess.pid);
 };
 
-export const getUnraidApiPid = async () => {
+export const getUnraidApiPid = async (): Promise<number | undefined> => {
 	// Find all processes called "unraid-api" which aren't this process
 	const pids = await findProcess('name', 'unraid-api', true);
-	return pids.find(_ => _.pid !== process.pid)?.pid;
+
+	return pids.find(_ => _.pid !== process.pid && _.cmd.includes(UNRAID_API_BIN))?.pid;
 };
