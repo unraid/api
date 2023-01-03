abd753fa53d1c11d4d9114870b902e80354ea2b1
Eli Bosley <ekbosley@gmail.com>
2023-01-03 15:51:57 -0500

    fix: origin checks now throw 403s instead of timing out (#468)

---
+++ api/src/originMiddleware.ts
@@ -0,0 +1,49 @@
+import { type NextFunction, type Request, type Response } from 'express';
+import { logger, config } from '@app/core';
+import { getAllowedOrigins } from '@app/common/allowed-origins';
+
+const getOriginGraphqlError = () => ({
+	data: null,
+	errors: [{ message: 'The CORS policy for this site does not allow access from the specified Origin.' }],
+});
+
+/**
+ * Middleware to check a users origin and send a GraphQL error if they are not using a valid one
+ * @param req Express Request
+ * @param res Express Response
+ * @param next Express NextFunction
+ * @returns void
+ */
+export const originMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+	// Dev Mode Bypass
+	if (process.env.NODE_ENV === 'development' || config.debug) {
+		logger.warn('BYPASSING_CORS_CHECK: Dev Mode or Debug Mode Enabled');
+
+		next();
+		return;
+	}
+
+	const origin = req.get('Origin');
+	const allowedOrigins = getAllowedOrigins();
+	logger.trace(`Allowed origins: ${allowedOrigins.join(', ')}`);
+
+	// Disallow requests with no origin
+	// (like mobile apps, curl requests or viewing /graphql directly)
+	if (!origin) {
+		logger.debug('No origin provided, denying CORS!');
+		res.status(403).send(getOriginGraphqlError());
+		return;
+	}
+
+	logger.trace(`📒 Checking "${origin.toLowerCase()}" for CORS access.`);
+
+	// Only allow known origins
+	if (!allowedOrigins.includes(origin.toLowerCase())) {
+		logger.error('❌ %s is not in the allowed origins list, denying CORS!', origin.toLowerCase());
+		res.status(403).send(getOriginGraphqlError());
+		return;
+	}
+
+	logger.trace('✔️ Origin check passed, granting CORS!');
+	next();
+};
