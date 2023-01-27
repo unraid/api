2837fbb63ade90f0acbeb58eb196f16b762e60fa
Eli Bosley <ekbosley@gmail.com>
2023-01-27 12:10:22 -0800

    feat: timeout in report

--- api/src/graphql/resolvers/query/cloud/check-minigraphql.ts
+++ api/src/graphql/resolvers/query/cloud/check-minigraphql.ts
@@ -6,6 +6,9 @@ import { type MinigraphqlResponse } from '@app/graphql/generated/api/types';
 export const checkMinigraphql = (): MinigraphqlResponse => {
 	logger.trace('Cloud endpoint: Checking mini-graphql');
 	// Do we have a connection to mothership?
-	const { status, error } = getters.minigraph();
-	return { status, error };
+	const { status, error, timeout, timeoutStart } = getters.minigraph();
+
+	const timeoutRemaining = timeout && timeoutStart ? timeout - (Date.now() - timeoutStart) : null;
+
+	return { status, error, timeout: timeoutRemaining };
 };
