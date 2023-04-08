4f003d81db944b9558dbb0bebc3dc96716f46df9
Eli Bosley <ekbosley@gmail.com>
2023-04-08 10:24:43 -0400

    feat: allow hot swap between graphql-ws and graphql-subscriptions
    - this will enable us to update the UPC after our other clients

--- api/src/graphql/client/api/get-api-client.ts
+++ api/src/graphql/client/api/get-api-client.ts
@@ -1,4 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client/core';
+import {
+    ApolloClient,
+    HttpLink,
+    InMemoryCache,
+    split,
+} from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
 import { INTERNAL_HTTP_LINK, INTERNAL_WS_LINK } from '@app/consts';
 import { WebSocketLink } from '@apollo/client/link/ws';
@@ -7,6 +12,8 @@ import WebSocket from 'ws';
 import { fetch } from 'cross-fetch';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { graphqlLogger } from '@app/core/log';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 
 class WebsocketWithOriginHeader extends WebSocket {
     constructor(address, protocols) {
@@ -19,53 +26,60 @@ class WebsocketWithOriginHeader extends WebSocket {
     }
 }
 
-
 export const getApiApolloClient = ({ upcApiKey }: { upcApiKey: string }) => {
-	const httpLink = new HttpLink({
-		uri: INTERNAL_HTTP_LINK,
-		fetch,
-		headers: {
-			Origin: '/var/run/unraid-cli.sock',
-			'x-api-key': upcApiKey,
-			'Content-Type': 'application/json',
-		},
-	})
-	const wsLink = new WebSocketLink(new SubscriptionClient(INTERNAL_WS_LINK, {
-		reconnect: true,
-		connectionParams: {
-			'x-api-key': upcApiKey
-		},
-		
-	}, WebsocketWithOriginHeader))
+    const httpLink = new HttpLink({
+        uri: INTERNAL_HTTP_LINK,
+        fetch,
+        headers: {
+            Origin: '/var/run/unraid-cli.sock',
+            'x-api-key': upcApiKey,
+            'Content-Type': 'application/json',
+        },
+    });
 
-	const splitLink = split(
-		({ query }) => {
-			const definition = getMainDefinition(query);
-			return (
-				definition.kind === 'OperationDefinition' &&
-				definition.operation === 'subscription'
-			);
-		},
-		wsLink,
-		httpLink
-	);
 
-	const errorLink = onError(({ networkError }) => {
-		if (networkError) {
-			graphqlLogger.warn('[GRAPHQL-CLIENT] NETWORK ERROR ENCOUNTERED %o', networkError);
-		}
-	})
+    // Create the subscription websocket link
+    const wsLink = new GraphQLWsLink(
+        createClient({
+            webSocketImpl: WebsocketWithOriginHeader,
+            url: INTERNAL_WS_LINK,
+            connectionParams: () => {
+                return { 'x-api-key': upcApiKey };
+            },
+        })
+    );
 
-	return new ApolloClient({
-	defaultOptions: {
-		query: {
-			fetchPolicy: 'no-cache'
-		},
-		mutate: {
-			fetchPolicy: 'no-cache'
-		}
-	},
-	cache: new InMemoryCache(),
-	link: errorLink.concat(splitLink)
-	});
-}
+    const splitLink = split(
+        ({ query }) => {
+            const definition = getMainDefinition(query);
+            return (
+                definition.kind === 'OperationDefinition' &&
+                definition.operation === 'subscription'
+            );
+        },
+        wsLink,
+        httpLink
+    );
+
+    const errorLink = onError(({ networkError }) => {
+        if (networkError) {
+            graphqlLogger.warn(
+                '[GRAPHQL-CLIENT] NETWORK ERROR ENCOUNTERED %o',
+                networkError
+            );
+        }
+    });
+
+    return new ApolloClient({
+        defaultOptions: {
+            query: {
+                fetchPolicy: 'no-cache',
+            },
+            mutate: {
+                fetchPolicy: 'no-cache',
+            },
+        },
+        cache: new InMemoryCache(),
+        link: errorLink.concat(splitLink),
+    });
+};
--- api/src/server.ts
+++ api/src/server.ts
@@ -9,9 +9,9 @@ import { watch } from 'chokidar';
 import express, { json, type Request, type Response } from 'express';
 import http from 'http';
 import {
-	ApolloServerPluginLandingPageGraphQLPlayground,
-	ApolloServerPluginDrainHttpServer,
-	ApolloServerPluginLandingPageDisabled,
+    ApolloServerPluginLandingPageGraphQLPlayground,
+    ApolloServerPluginDrainHttpServer,
+    ApolloServerPluginLandingPageDisabled,
 } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-express';
 import { logger, config, pubsub, graphqlLogger } from '@app/core';
@@ -20,7 +20,7 @@ import display from '@app/graphql/resolvers/query/display';
 import { getters } from '@app/store';
 import { schema } from '@app/graphql/schema';
 import { execute, subscribe } from 'graphql';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { GRAPHQL_WS, SubscriptionServer } from 'subscriptions-transport-ws';
 import { wsHasConnected, wsHasDisconnected } from '@app/ws';
 import { apiKeyToUser } from '@app/graphql';
 import { randomUUID } from 'crypto';
@@ -28,15 +28,27 @@ import { getServerAddress } from '@app/common/get-server-address';
 import { apolloConfig } from '@app/graphql/config';
 import { originMiddleware } from '@app/originMiddleware';
 import { API_VERSION } from '@app/environment';
-import { getBannerPathIfPresent, getCasePathIfPresent } from '@app/core/utils/images/image-file-helpers';
+import {
+    getBannerPathIfPresent,
+    getCasePathIfPresent,
+} from '@app/core/utils/images/image-file-helpers';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws';
 
-const configFilePath = path.join(getters.paths()['dynamix-base'], 'case-model.cfg');
-const customImageFilePath = path.join(getters.paths()['dynamix-base'], 'case-model.png');
+const configFilePath = path.join(
+    getters.paths()['dynamix-base'],
+    'case-model.cfg'
+);
+const customImageFilePath = path.join(
+    getters.paths()['dynamix-base'],
+    'case-model.png'
+);
 
 const updatePubsub = async () => {
-	await pubsub.publish('display', {
-		display: await display(),
-	});
+    await pubsub.publish('display', {
+        display: await display(),
+    });
 };
 
 // Update pub/sub when config/image file is added/updated/removed
@@ -73,168 +85,240 @@ export const httpServer = http.createServer(app);
 
 // Log only if the server actually binds to the port
 httpServer.on('listening', () => {
-	logger.info('Server is up! %s', getServerAddress(httpServer));
+    logger.info('Server is up! %s', getServerAddress(httpServer));
 });
 
-// This needs to be a let as it's referenced before it's used
-// eslint-disable-next-line prefer-const
-let subscriptionServer: SubscriptionServer;
+// graphql-ws
+const graphqlWs = new WebSocketServer({ noServer: true });
+
+// subscriptions-transport-ws
+const subTransWs = new WebSocketServer({
+    noServer: true,
+});
+
+// graphql-ws setup
+const graphqlWsServer = useServer<{ 'x-api-key': string }, { context: { user: any, websocketId: string } }>(
+    {
+        schema,
+        onError(ctx, message, errors) {
+            logger.debug('%o %o %o', ctx, message, errors);
+        },
+        async onConnect(ctx) {
+            logger.debug(
+                'Connecting new client with params: %o',
+                ctx.connectionParams
+            );
+            const params: unknown = ctx.connectionParams?.['x-api-key'];
+            if (params && typeof params === 'string') {
+                const apiKey = params;
+                const user = await apiKeyToUser(apiKey);
+                const websocketId = randomUUID();
+                logger.debug('User is %o', user);
+				ctx.extra.context = { user, websocketId } 
+				return true;
+            }
+            return {};
+        },
+        context: (ctx) => {
+			return ctx.extra.context;
+		}
+    },
+    graphqlWs
+);
+
+// subscriptions-transport-ws setup
+const subscriptionsTransportServer = SubscriptionServer.create(
+    {
+        // This is the `schema` we just created.
+        schema,
+        // These are imported from `graphql`.
+        execute,
+        subscribe,
+        // Ensure keep-alive packets are sent
+        keepAlive: 10_000,
+        // Providing `onConnect` is the `SubscriptionServer` equivalent to the
+        // `context` function in `ApolloServer`. Please [see the docs](https://github.com/apollographql/subscriptions-transport-ws#constructoroptions-socketoptions--socketserver)
+        // for more information on this hook.
+        async onConnect(connectionParams: { 'x-api-key': string }) {
+            const apiKey = connectionParams['x-api-key'];
+            const user = await apiKeyToUser(apiKey);
+            const websocketId = randomUUID();
+
+            graphqlLogger.addContext('websocketId', websocketId);
+            graphqlLogger.debug('%s connected', user.name);
+            graphqlLogger.removeContext('websocketId');
+
+            // Update ws connection count and other needed values
+            wsHasConnected(websocketId);
+
+            return {
+                user,
+                websocketId,
+            };
+        },
+        async onDisconnect(
+            _,
+            websocketContext: {
+                initPromise: Promise<
+                    | boolean
+                    | {
+                          user: {
+                              name: string;
+                          };
+                          websocketId: string;
+                      }
+                >;
+            }
+        ) {
+            const context = await websocketContext.initPromise;
+
+            // The websocket has disconnected before init event has resolved
+            // @see: https://github.com/apollographql/subscriptions-transport-ws/issues/349
+            if (context === true || context === false) {
+                // This seems to also happen if a tab is left open and then a server starts up
+                // The tab hits the server over and over again without sending init
+                graphqlLogger.debug('unknown disconnected');
+                return;
+            }
+
+            const { user, websocketId } = context;
+
+            graphqlLogger.addContext('websocketId', websocketId);
+            graphqlLogger.debug('%s disconnected.', user.name);
+            graphqlLogger.removeContext('websocketId');
+
+            // Update ws connection count and other needed values
+            wsHasDisconnected(websocketId);
+        },
+    },
+    subTransWs
+);
 
 const apolloServerPluginOnExit = {
-	async serverWillStart() {
-		return {
-			/**
-			 * When the app exits this will be run.
-			 */
-			async drainServer() {
-				// Close all connections to subscriptions server
-				subscriptionServer.close();
-			},
-		};
-	},
+    async serverWillStart() {
+        return {
+            /**
+             * When the app exits this will be run.
+             */
+            async drainServer() {
+                // Close all connections to subscriptions server
+                subscriptionsTransportServer.close();
+                graphqlWsServer.dispose();
+            },
+        };
+    },
 };
 
 // Create graphql instance
 export const server = new ApolloServer({
-	...apolloConfig,
-	plugins: [
-		apolloServerPluginOnExit,
-		// eslint-disable-next-line new-cap
-		(process.env.PLAYGROUND ?? config.debug) ? ApolloServerPluginLandingPageGraphQLPlayground() : ApolloServerPluginLandingPageDisabled(),
-		// Close all connections to http server when the app closes
-		// eslint-disable-next-line new-cap
-		ApolloServerPluginDrainHttpServer({ httpServer }),
-	],
+    ...apolloConfig,
+    plugins: [
+        apolloServerPluginOnExit,
+        // eslint-disable-next-line new-cap
+        process.env.PLAYGROUND ?? config.debug
+            ? ApolloServerPluginLandingPageGraphQLPlayground()
+            : ApolloServerPluginLandingPageDisabled(),
+        // Close all connections to http server when the app closes
+        // eslint-disable-next-line new-cap
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+    ],
 });
 
-subscriptionServer = SubscriptionServer.create({
-	// This is the `schema` we just created.
-	schema,
-	// These are imported from `graphql`.
-	execute,
-	subscribe,
-	// Ensure keep-alive packets are sent
-	keepAlive: 10_000,
-	// This `server` is the instance returned from `new ApolloServer`.
-	// Ensures the same graphql validation rules are applied to both the Subscription Server and the ApolloServer
-	validationRules: server.requestOptions.validationRules,
-	// Providing `onConnect` is the `SubscriptionServer` equivalent to the
-	// `context` function in `ApolloServer`. Please [see the docs](https://github.com/apollographql/subscriptions-transport-ws#constructoroptions-socketoptions--socketserver)
-	// for more information on this hook.
-	async onConnect(
-		connectionParams: {'x-api-key': string },
-	) {
-		const apiKey = connectionParams['x-api-key'];
-		const user = await apiKeyToUser(apiKey);
-		const websocketId = randomUUID();
+httpServer.on('upgrade', (req, socket, head) => {
+    // extract websocket subprotocol from header
+    const protocol = req.headers['sec-websocket-protocol'];
+    const protocols = Array.isArray(protocol)
+        ? protocol
+        : protocol?.split(',').map((p) => p.trim());
 
-		graphqlLogger.addContext('websocketId', websocketId);
-		graphqlLogger.debug('%s connected', user.name);
-		graphqlLogger.removeContext('websocketId');
-
-		// Update ws connection count and other needed values
-		wsHasConnected(websocketId);
-
-		return {
-			user,
-			websocketId,
-		};
-	},
-	async onDisconnect(_, websocketContext: {
-		initPromise: Promise<boolean | {
-			user: {
-				name: string;
-			};
-			websocketId: string;
-		}>;
-	}) {
-		const context = await websocketContext.initPromise;
-
-		// The websocket has disconnected before init event has resolved
-		// @see: https://github.com/apollographql/subscriptions-transport-ws/issues/349
-		if (context === true || context === false) {
-			// This seems to also happen if a tab is left open and then a server starts up
-			// The tab hits the server over and over again without sending init
-			graphqlLogger.debug('unknown disconnected');
-			return;
-		}
-
-		const { user, websocketId } = context;
-
-		graphqlLogger.addContext('websocketId', websocketId);
-		graphqlLogger.debug('%s disconnected.', user.name);
-		graphqlLogger.removeContext('websocketId');
-
-		// Update ws connection count and other needed values
-		wsHasDisconnected(websocketId);
-	},
-}, {
-	// This is the `httpServer` we created in a previous step.
-	server: httpServer,
-	// This `server` is the instance returned from `new ApolloServer`.
-	path: server.graphqlPath,
+    // decide which websocket server to use
+    const wss =
+        protocols?.includes(GRAPHQL_WS) && // subscriptions-transport-ws subprotocol
+        !protocols.includes(GRAPHQL_TRANSPORT_WS_PROTOCOL) // graphql-ws subprotocol
+            ? subTransWs
+            : // graphql-ws will welcome its own subprotocol and
+              // gracefully reject invalid ones. if the client supports
+              // both transports, graphql-ws will prevail
+              graphqlWs;
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit('connection', ws, req);
+    });
 });
 
 // List all endpoints at start of server
 app.get('/', (_, res: Response) => res.status(200).send('OK'));
 
 app.post('/verify', async (req, res) => {
-	try {
-		// Check two-factor token is valid
-		verifyTwoFactorToken(req.body?.username, req.body?.token);
+    try {
+        // Check two-factor token is valid
+        verifyTwoFactorToken(req.body?.username, req.body?.token);
 
-		// Success
-		logger.debug('2FA token valid, allowing login.');
+        // Success
+        logger.debug('2FA token valid, allowing login.');
 
-		// Allow the user to pass
-		res.sendStatus(204);
-		return;
-	} catch (error: unknown) {
-		logger.addContext('error', error);
-		logger.error('Failed validating 2FA token.');
-		logger.removeContext('error');
+        // Allow the user to pass
+        res.sendStatus(204);
+        return;
+    } catch (error: unknown) {
+        logger.addContext('error', error);
+        logger.error('Failed validating 2FA token.');
+        logger.removeContext('error');
 
-		// User failed verification
-		res.status(401);
-		res.send((error as Error).message);
-	}
+        // User failed verification
+        res.status(401);
+        res.send((error as Error).message);
+    }
 });
 
-app.get('/graphql/api/customizations/:type', async (req: Request, res: Response) => {
-	// @TODO - Clean up this function
-	const apiKey = req.headers['x-api-key'];
-	if (apiKey && typeof apiKey === 'string' && (await apiKeyToUser(apiKey)).role !== 'guest') {
-		if (req.params.type === 'banner') {
-			const path = await getBannerPathIfPresent();
-			if (path) {
-				res.sendFile(path);
-				return;
-			}
-		} else if (req.params.type === 'case') {
-			const path = await getCasePathIfPresent();
-			if (path) {
-				res.sendFile(path);
-				return;
-			}
-		}
+app.get(
+    '/graphql/api/customizations/:type',
+    async (req: Request, res: Response) => {
+        // @TODO - Clean up this function
+        const apiKey = req.headers['x-api-key'];
+        if (
+            apiKey &&
+            typeof apiKey === 'string' &&
+            (await apiKeyToUser(apiKey)).role !== 'guest'
+        ) {
+            if (req.params.type === 'banner') {
+                const path = await getBannerPathIfPresent();
+                if (path) {
+                    res.sendFile(path);
+                    return;
+                }
+            } else if (req.params.type === 'case') {
+                const path = await getCasePathIfPresent();
+                if (path) {
+                    res.sendFile(path);
+                    return;
+                }
+            }
 
-		return res.status(404).send('no customization of this type found');
-	}
+            return res.status(404).send('no customization of this type found');
+        }
 
-	return res.status(403).send('unauthorized');
-});
+        return res.status(403).send('unauthorized');
+    }
+);
 
 // Handle errors by logging them and returning a 500.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: Error & { stackTrace?: string; status?: number }, _, res: Response, __) => {
-	// Don't log CORS errors
-	if (error.message.includes('CORS')) return;
+app.use(
+    (
+        error: Error & { stackTrace?: string; status?: number },
+        _,
+        res: Response,
+        __
+    ) => {
+        // Don't log CORS errors
+        if (error.message.includes('CORS')) return;
 
-	logger.error(error);
+        logger.error(error);
 
-	if (error.stack) {
-		error.stackTrace = error.stack;
-	}
+        if (error.stack) {
+            error.stackTrace = error.stack;
+        }
 
-	res.status(error.status ?? 500).send(error);
-});
+        res.status(error.status ?? 500).send(error);
+    }
+);
