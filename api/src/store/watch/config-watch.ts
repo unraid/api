2c8a6ee19ee82ff4d363bcf2d73ee251b0bd6e89
Eli Bosley <ekbosley@gmail.com>
2023-04-18 12:51:26 -0400

    fix: create config if not exists in chokidar watch

--- api/src/store/watch/config-watch.ts
+++ api/src/store/watch/config-watch.ts
@@ -2,11 +2,15 @@ import { getters, store } from '@app/store';
 import { watch } from 'chokidar';
 import { loadConfigFile } from '@app/store/modules/config';
 import { logger } from '@app/core/log';
+import { existsSync, writeFileSync } from 'fs';
 
 export const setupConfigPathWatch = () => {
 	const myServersConfigPath = getters.paths()?.['myservers-config'];
 	if (myServersConfigPath) {
 		logger.info('Watch Setup on Config Path: %s', myServersConfigPath);
+		if (!existsSync(myServersConfigPath)) {
+			writeFileSync(myServersConfigPath, '', 'utf-8');
+		}
 		watch(myServersConfigPath, {
 			persistent: true,
 			ignoreInitial: false,
