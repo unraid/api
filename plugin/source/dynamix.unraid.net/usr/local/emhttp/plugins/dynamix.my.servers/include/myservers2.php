2f6d1b01cf2c9bd07279333dc923233623805842
Zack Spear <hi@zackspear.com>
2023-04-26 15:01:10 -0700

    feat: add web component to settings for api logs download

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/myservers2.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/myservers2.php
@@ -539,6 +539,7 @@ $upc_translations = [
       'heading' => _('Connect plugin install failed'),
       'message' => _('The Connect plugin install is incomplete').'. '._('Please uninstall and reinstall the Connect plugin').'. '._('Be sure to let the install complete before you close the window').'.',
     ],
+    "downloadUnraidApiLogs" => "Download unraid-api Logs",
   ],
 ];
 // note: $myservers variable defined in myservers1.php, by parsing myservers.cfg
