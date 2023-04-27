69c634f9e47df4cdd1d5635d6ebaee820eabf6cf
Eli Bosley <ekbosley@gmail.com>
2023-04-26 21:15:44 -0400

    feat: add report to zip file (#628)

--- api/src/graphql/express/get-logs.ts
+++ api/src/graphql/express/get-logs.ts
@@ -1,15 +1,35 @@
-import { apiKeyToUser } from "@app/graphql/index";
-import { getters } from "@app/store/index";
-import { execa } from "execa";
-import { type Response, type Request} from 'express';
-import { stat } from "fs/promises";
-import path from "path";
+import { report } from '@app/cli/commands/report';
+import { logger } from '@app/core/log';
+import { apiKeyToUser } from '@app/graphql/index';
+import { getters } from '@app/store/index';
+import { execa } from 'execa';
+import { type Response, type Request } from 'express';
+import { stat, writeFile } from 'fs/promises';
+import { join } from 'path';
+
+const saveApiReport = async (pathToReport: string) => {
+    try {
+        const apiReport = await report('-vv', '--json');
+        logger.debug('Report object %o', apiReport);
+        await writeFile(
+            pathToReport,
+            JSON.stringify(apiReport, null, 2),
+            'utf-8'
+        );
+    } catch (error) {
+        logger.warn('Could not generate report for zip with error %o', error);
+    }
+};
 
 export const getLogs = async (req: Request, res: Response) => {
-    // @TODO - Clean up this function
     const apiKey = req.headers['x-api-key'];
-    const logPath = getters.paths()["log-base"];
-    const zipToWrite = path.join(logPath, '../unraid-api.tar.gz');
+    const logPath = getters.paths()['log-base'];
+    try {
+        await saveApiReport(join(logPath, 'report.json'));
+    } catch (error) {
+        logger.warn('Could not generate report for zip with error %o', error);
+    }
+    const zipToWrite = join(logPath, '../unraid-api.tar.gz');
     if (
         apiKey &&
         typeof apiKey === 'string' &&
@@ -18,11 +38,7 @@ export const getLogs = async (req: Request, res: Response) => {
         const exists = Boolean(await stat(logPath).catch(() => null));
         if (exists) {
             try {
-                await execa('tar', [
-                    '-czf',
-                    zipToWrite,
-                    logPath
-                ]);
+                await execa('tar', ['-czf', zipToWrite, logPath]);
                 return res.status(200).sendFile(zipToWrite);
             } catch (error) {
                 return res.status(503).send(`Failed: ${error}`);
