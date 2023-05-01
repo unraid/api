4c4d8d0554a7b23f04d9d663b6a1f51100322b75
Eli Bosley <ekbosley@gmail.com>
2023-05-01 13:01:32 -0400

    feat: support default image (#630)

---
+++ api/src/__test__/core/utils/images/image-file-helpers.test.ts
@@ -0,0 +1,23 @@
+import { getBannerPathIfPresent, getCasePathIfPresent } from "@app/core/utils/images/image-file-helpers";
+import { store } from "@app/store/index";
+import { loadDynamixConfigFile } from "@app/store/modules/dynamix";
+
+import { expect, test } from "vitest";
+
+test('get case path returns expected result', () => {
+    expect(getCasePathIfPresent()).resolves.toContain('/dev/dynamix/case-model.png')
+})
+
+test('get banner path returns null (state unloaded)', () => {
+    expect(getBannerPathIfPresent()).resolves.toMatchInlineSnapshot('null')
+})
+
+test('get banner path returns the banner (state loaded)', async() => {
+	await store.dispatch(loadDynamixConfigFile()).unwrap();
+    expect(getBannerPathIfPresent()).resolves.toContain('/dev/dynamix/banner.png');
+})
+
+test('get banner path returns null when no banner (state loaded)', async () => {
+    await store.dispatch(loadDynamixConfigFile()).unwrap();
+    expect(getBannerPathIfPresent('notabanner.png')).resolves.toMatchInlineSnapshot('null');
+});
