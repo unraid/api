f1130576a0bd8b8e7d7c0e4e78f47c2de1735772
Zack Spear <hi@zackspear.com>
2023-05-31 14:15:49 -0700

    feat: user profile dropdown components

---
+++ components/UserProfile/DropdownButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineEmits(['click'])
+</script>
+
+<template>
+  <div class="relative flex items-center justify-end h-full">
+    <button @click="$emit('click')">Open</button>
+  </div>
+</template>
