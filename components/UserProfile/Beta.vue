42f94550ae31b7bc13212cb90018a06766d65617
Zack Spear <hi@zackspear.com>
2023-05-31 14:14:00 -0700

    feat: create beta component

---
+++ components/UserProfile/Beta.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+export interface Props {
+  colorClasses?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  colorClasses: 'text-grey-mid border-grey-mid',
+});
+</script>
+
+<template>
+  <span
+    class="text-10px uppercase py-4px px-6px border-2 rounded-full"
+    :class="colorClasses"
+  >
+    {{ 'Beta' }}
+  </span>
+</template>
