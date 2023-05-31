3ed140ffac158c15b087fc46eb430ce4e52406ad
Zack Spear <hi@zackspear.com>
2023-05-31 14:12:49 -0700

    feat: url helpers

---
+++ helpers/urls.ts
@@ -0,0 +1,16 @@
+/**
+ * @todo setup .env
+ */
+const ACCOUNT = 'https://account.unraid.net';
+const CONNECT_DOCS = 'https://docs.unraid.net/category/unraid-connect';
+const CONNECT_DASHBOARD = 'https://connect.myunraid.net';
+const PURCHASE = 'https://unraid.net/preflight';
+const PLUGIN_SETTINGS = '/Settings/ManagementAccess#UnraidNetSettings';
+
+export {
+  ACCOUNT,
+  CONNECT_DASHBOARD,
+  CONNECT_DOCS,
+  PURCHASE,
+  PLUGIN_SETTINGS,
+};
