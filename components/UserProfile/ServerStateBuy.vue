7ad55473281d4e8e300386f9113a444d1b55b912
Zack Spear <hi@zackspear.com>
2023-06-01 00:27:07 -0700

    fix: server state buy component

---
+++ components/UserProfile/ServerStateBuy.vue
@@ -0,0 +1,6 @@
+
+<template>
+  <button class="text-sm font-semibold transition-colors duration-150 ease-in-out border-t-0 border-l-0 border-r-0 border-b-2 border-transparent hover:border-orange-dark focus:border-orange-dark focus:outline-none">
+    <slot />
+  </button>
+</template>
