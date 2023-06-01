44ccb7abf0a2eb02e82b730a9093cc24829e1ad7
Zack Spear <hi@zackspear.com>
2023-06-01 00:11:26 -0700

    feat(upc): avatar & brand components

---
+++ components/UserProfile/TriangleDown.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+export interface Props {
+  open?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  open: false,
+});
+</script>
+<template>
+  <span
+    class="w-0 h-0 border-t-[6px] border-l-[6px] border-r-[6px] border-t-solid border-t-black border-l-solid border-l-transparent border-r-solid border-r-transparent transition-transform"
+    :class="{
+      '-rotate-180': open,
+      'rotate-0': !open,
+    }"
+  />
+</template>
