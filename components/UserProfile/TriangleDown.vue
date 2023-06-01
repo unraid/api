834691f12bc57097d8e60627a0c525fbb4007c48
Zack Spear <hi@zackspear.com>
2023-06-01 00:38:22 -0700

    refactor(upc): trigger arrow size

--- components/UserProfile/TriangleDown.vue
+++ components/UserProfile/TriangleDown.vue
@@ -8,7 +8,7 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 <template>
   <span
-    class="w-0 h-0 border-t-[6px] border-l-[6px] border-r-[6px] border-t-solid border-t-black border-l-solid border-l-transparent border-r-solid border-r-transparent transition-transform"
+    class="w-0 h-0 border-t-[4px] border-l-[4px] border-r-[4px] border-t-solid border-t-black border-l-solid border-l-transparent border-r-solid border-r-transparent transition-transform"
     :class="{
       '-rotate-180': open,
       'rotate-0': !open,
