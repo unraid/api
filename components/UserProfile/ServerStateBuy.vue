17ca9adc79af388170a052fc7e82d993a9cc20f6
Zack Spear <hi@zackspear.com>
2023-06-05 19:11:01 -0700

    refactor: callback progress

--- components/UserProfile/ServerStateBuy.vue
+++ components/UserProfile/ServerStateBuy.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <button class="text-sm font-semibold transition-colors duration-150 ease-in-out border-t-0 border-l-0 border-r-0 border-b-2 border-transparent hover:border-orange-dark focus:border-orange-dark focus:outline-none">
+  <button class="text-12px font-semibold transition-colors duration-150 ease-in-out border-t-0 border-l-0 border-r-0 border-b-2 border-transparent hover:border-orange-dark focus:border-orange-dark focus:outline-none">
     <slot />
   </button>
 </template>
--- store/callback.ts
+++ store/callback.ts
@@ -1,7 +1,11 @@
 import AES from 'crypto-js/aes';
 import Utf8 from 'crypto-js/enc-utf8';
 import { defineStore, createPinia, setActivePinia } from "pinia";
-import { useServerStore } from './server';
+import type { CallbackSendPayload } from '~/types/callback';
+import type {
+  ServerAccountCallbackSendPayload,
+  ServerPurchaseCallbackSendPayload,
+} from '~/types/server';
 /**
  * @see https://stackoverflow.com/questions/73476371/using-pinia-with-vue-js-web-components
  * @see https://github.com/vuejs/pinia/discussions/1085
@@ -9,22 +13,19 @@ import { useServerStore } from './server';
 setActivePinia(createPinia());
 
 export const useCallbackStore = defineStore('callback', () => {
-  const serverStore = useServerStore();
   // store helpers
   // const config = useRuntimeConfig(); // results in a nuxt error after web components are built
   // const encryptKey = config.public.callbackKey;
   const encryptKey = 'Uyv2o8e*FiQe8VeLekTqyX6Z*8XonB';
-  console.debug('[useCallbackStore]', { encryptKey });
   // state
-  const encryptedMessage = ref('');
+  const callbackFeedbackVisible = ref<boolean>(false);
   const decryptedData = ref();
-  // getters
-
+  const encryptedMessage = ref('');
   // actions
-  const send = (url: string = 'https://unraid.ddev.site/init-purchase') => {
+  const send = (url: string = 'https://unraid.ddev.site/init-purchase', payload: CallbackSendPayload) => {
     console.debug('[send]');
     const stringifiedData = JSON.stringify({
-      ...serverStore.server,
+      ...payload,
       sender: window.location.href,
     });
     // @todo don't save to store
@@ -42,14 +43,16 @@ export const useCallbackStore = defineStore('callback', () => {
     console.debug('[watcher]', currentUrl);
     const callbackValue = currentUrl.searchParams.get('data');
     if (!callbackValue) {
-      console.debug('[watcher] no callback to handle');
-      return;
+      return console.debug('[watcher] no callback to handle');
     }
     const decryptedMessage = AES.decrypt(callbackValue, encryptKey);
     decryptedData.value = JSON.parse(decryptedMessage.toString(Utf8));
     console.debug('[watcher]', decryptedMessage, decryptedData.value);
     if (!decryptedData.value) return console.error('Callback Watcher: Data not present');
     if (!decryptedData.value.action) return console.error('Callback Watcher: Required "action" not present');
+    // Display the feedback modal
+    show();
+    // Parse the data and perform actions
     switch (decryptedData.value.action) {
       case 'install':
         console.debug(`Installing key ${decryptedData.value.keyUrl}\n\nOEM: ${decryptedData.value.oem}\n\nSender: ${decryptedData.value.sender}`);
@@ -63,5 +66,26 @@ export const useCallbackStore = defineStore('callback', () => {
     }
   };
 
-  return { send, watcher, decryptedData };
+  const hide = () => callbackFeedbackVisible.value = false;
+  const show = () => callbackFeedbackVisible.value = true;
+  const toggle = useToggle(callbackFeedbackVisible);
+
+  /**
+   * @todo consider removing query string once actions are done
+   */
+  watch(callbackFeedbackVisible, (newVal, _oldVal) => {
+    console.debug('[callbackFeedbackVisible]', newVal, _oldVal);
+  });
+
+  return {
+    // state
+    decryptedData,
+    callbackFeedbackVisible,
+    // actions
+    send,
+    watcher,
+    hide,
+    show,
+    toggle,
+  };
 });
