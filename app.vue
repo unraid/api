a9c859f022922f6c5c34e9178f5aebf78d6daa5b
Zack Spear <hi@zackspear.com>
2023-06-06 18:41:19 -0700

    fix: web component modals

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import serverState from './_data/serverState';
-
 const nuxtApp = useNuxtApp();
 onBeforeMount(() => {
   nuxtApp.$customElements.registerEntry('ConnectComponents');
@@ -8,56 +6,7 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="max-w-5xl mx-auto bg-gray-200">
-    <client-only>
-      <div class="flex flex-col gap-6 p-6">
-        <h2>Vue Components</h2>
-        <h3>UserProfileCe</h3>
-        <UserProfileCe :server="serverState" />
-        <hr />
-        <h3>DownloadApiLogsCe</h3>
-        <DownloadApiLogsCe />
-        <hr />
-        <h3>AuthCe</h3>
-        <AuthCe />
-        <hr />
-        <h3>KeyActionsCe</h3>
-        <KeyActionsCe />
-        <hr />
-        <h3>WanIpCheckCe</h3>
-        <WanIpCheckCe />
-      </div>
-      <div class="flex flex-col gap-6 p-6">
-        <h2>Web Components</h2>
-        <h3>UserProfileCe</h3>
-        <connect-user-profile :server="JSON.stringify(serverState)"></connect-user-profile>
-        <hr />
-        <h3>DownloadApiLogsCe</h3>
-        <connect-download-api-logs></connect-download-api-logs>
-        <hr />
-        <h3>AuthCe</h3>
-        <connect-auth></connect-auth>
-        <hr />
-        <h3>KeyActionsCe</h3>
-        <connect-key-actions></connect-key-actions>
-        <hr />
-        <h3>WanIpCheckCe</h3>
-        <connect-wan-ip-check></connect-wan-ip-check>
-      </div>
-    </client-only>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style lang="postcss" scoped>
-h2 {
-  @apply text-xl font-semibold font-mono;
-}
-
-h3 {
-  @apply text-lg font-semibold font-mono;
-}
-
-hr {
-  @apply border-black;
-}
-</style>
---
+++ components/Modals.ce.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import 'tailwindcss/tailwind.css';
+import '~/assets/main.css';
+
+import { useCallbackStore } from '~/store/callback';
+import { usePromoStore } from '~/store/promo';
+
+const callbackStore = useCallbackStore();
+const promoStore = usePromoStore();
+const { callbackFeedbackVisible } = storeToRefs(callbackStore);
+const { promoVisible } = storeToRefs(promoStore);
+</script>
+
+<template>
+  <div class="relative z-[99999]">
+    <UpcCallbackFeedback v-if="callbackFeedbackVisible" :open="callbackFeedbackVisible" />
+    <UpcPromo v-if="promoVisible" :open="promoVisible" />
+  </div>
+</template>
+
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
