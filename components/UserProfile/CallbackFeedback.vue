1a239b69143a48580156947caa2e98afb155213a
Zack Spear <hi@zackspear.com>
2023-06-07 17:30:31 -0700

    feat: install key and account config webgui requests

--- components/UserProfile/CallbackFeedback.vue
+++ components/UserProfile/CallbackFeedback.vue
@@ -3,6 +3,7 @@ import { storeToRefs } from 'pinia';
 import 'tailwindcss/tailwind.css';
 import '~/assets/main.css';
 import { useCallbackStore } from '~/store/callback';
+import { useInstallKeyStore } from '~/store/installKey';
 
 export interface Props {
   open?: boolean;
@@ -13,28 +14,33 @@ withDefaults(defineProps<Props>(), {
 });
 
 const callbackStore = useCallbackStore();
-const { decryptedData } = storeToRefs(callbackStore);
+const { callbackLoading, decryptedData } = storeToRefs(callbackStore);
+
+const close = () => {
+  if (callbackLoading.value) return console.debug('[close] not allowed');
+  callbackStore.hide();
+};
 </script>
 
 <template>
   <Modal
     :open="open"
-    @close="callbackStore.hide()"
+    @close="close"
     max-width="max-w-800px"
+    :show-close-x="!callbackLoading"
   >
     <div class="text-center relative w-full flex flex-col gap-y-16px">
       <header>
         <h1 class="text-24px font-semibold flex flex-wrap justify-center gap-x-1">Callback Feedback</h1>
       </header>
 
-      <BrandLoading class="w-90px mx-auto" />
+      <BrandLoading v-if="callbackLoading" class="w-90px mx-auto" />
       <pre class="text-left text-black p-8px w-full overflow-scroll bg-gray-400">{{ JSON.stringify(decryptedData, null, 2) }}</pre>
 
-      <div class="w-full max-w-xs flex flex-col gap-y-16px mx-auto">
+      <div v-if="!callbackLoading" class="w-full max-w-xs flex flex-col gap-y-16px mx-auto">
         <button
-          @click="callbackStore.hide()"
+          @click="close"
           class="text-12px tracking-wide inline-block mx-8px opacity-60 hover:opacity-100 focus:opacity-100 underline transition"
-          :title="'Close Promo'"
         >
           {{ 'Close' }}
         </button>
