4079c811282effc83e2d04d821b5275a9787f93e
Zack Spear <hi@zackspear.com>
2023-06-26 15:47:21 -0700

    fix: expired state

--- components/UserProfile/DropdownError.vue
+++ components/UserProfile/DropdownError.vue
@@ -9,28 +9,28 @@ import type { UserProfileLink } from '~/types/userProfile';
 const accountStore = useAccountStore();
 const { stateData } = storeToRefs(useServerStore());
 const links = ref<UserProfileLink[]>([
-  {
-    click: () => accountStore.troubleshoot(),
-    external: true,
-    icon: InformationCircleIcon,
-    text: 'Placeholder Button',
-  },
-  {
-    external: true,
-    href: CONNECT_FORUMS,
-    icon: InformationCircleIcon,
-    text: 'Connect Support Forum',
-  },
+  // {
+  //   click: () => accountStore.troubleshoot(),
+  //   external: true,
+  //   icon: InformationCircleIcon,
+  //   text: 'Placeholder Button',
+  // },
+  // {
+  //   external: true,
+  //   href: CONNECT_FORUMS,
+  //   icon: InformationCircleIcon,
+  //   text: 'Connect Support Forum',
+  // },
 ]);
 </script>
 
 <template>
-  <ul v-if="stateData.error" class="list-reset flex flex-col gap-y-4px -mx-8px mb-4px py-12px px-16px bg-unraid-red/60 rounded">
+  <ul v-if="stateData.error" class="font-semibold list-reset flex flex-col gap-y-4px -mx-8px mb-4px py-12px px-16px text-white bg-unraid-red/90 rounded">
     <h3 class="text-18px">{{ stateData.heading }}</h3>
-    <p class="text-14px opacity-85">{{ stateData.message }}</p>
+    <p class="text-14px">{{ stateData.message }}</p>
     <template v-if="links">
       <li v-for="(link, index) in links" :key="`link_${index}`" class="-mx-8px">
-        <UpcDropdownItem :item="link" />
+        <UpcDropdownItem :item="link" class="text-white" />
       </li>
     </template>
   </ul>
