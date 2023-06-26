7dca976e42a0b1f1bbc9aa2d534b2305cc4e9b73
Zack Spear <hi@zackspear.com>
2023-06-26 16:03:54 -0700

    refactor: date format

--- helpers/time/dateFormat.ts
+++ helpers/time/dateFormat.ts
@@ -1,11 +1,9 @@
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 
+/** @see https://day.js.org/docs/en/display/format#localized-formats */
 dayjs.extend(localizedFormat);
 
-const formatDate = (date: number): string => {
-  console.debug('[formatDate]', date, dayjs(date));
-  return dayjs(date).format('llll');
-}
+const formatDate = (date: number): string => dayjs(date).format('llll');
 
 export default formatDate;
