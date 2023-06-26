079c54ece425fb9b643f420592363cc492fe7a51
Zack Spear <hi@zackspear.com>
2023-06-26 15:46:58 -0700

    fix: format time

--- helpers/time/dateFormat.ts
+++ helpers/time/dateFormat.ts
@@ -1,5 +1,11 @@
 import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 
-const formatDate = (date: string): string => dayjs(parseInt(date, 10)).format('llll');
+dayjs.extend(localizedFormat);
+
+const formatDate = (date: number): string => {
+  console.debug('[formatDate]', date, dayjs(date));
+  return dayjs(date).format('llll');
+}
 
 export default formatDate;
