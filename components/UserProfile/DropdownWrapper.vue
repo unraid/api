fa83b238754471cf269f788be55716cdebc10a72
Zack Spear <hi@zackspear.com>
2023-06-30 21:54:32 -0700

    style: dropdown wrapper default shadow color

--- components/UserProfile/DropdownWrapper.vue
+++ components/UserProfile/DropdownWrapper.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <nav class="flex flex-col gap-y-8px p-8px bg-alpha rounded-lg shadow-xl">
+  <nav class="flex flex-col gap-y-8px p-8px bg-alpha rounded-lg shadow-xl shadow-orange/10">
     <slot/>
   </nav>
 </template>
