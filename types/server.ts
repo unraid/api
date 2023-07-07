a6e1e5ba268bfdb1cf4134d824fce025cc12bcdb
Zack Spear <hi@zackspear.com>
2023-07-07 15:40:55 -0700

    refactor: ServerState type

--- types/server.ts
+++ types/server.ts
@@ -2,24 +2,30 @@ import { KeyIcon } from '@heroicons/vue/24/solid';
 import { Theme } from '~/store/theme';
 import { UserProfileLink } from '~/types/userProfile';
 
-export enum ServerState {
-  BASIC = 'BASIC',
-  PLUS = 'PLUS',
-  PRO = 'PRO',
-  TRIAL = 'TRIAL',
-  EEXPIRED = 'EEXPIRED',
-  ENOKEYFILE = 'ENOKEYFILE',
-  EGUID = 'EGUID',
-  EGUID1 = 'EGUID1',
-  ETRIAL = 'ETRIAL',
-  ENOKEYFILE2 = 'ENOKEYFILE2',
-  ENOKEYFILE1 = 'ENOKEYFILE1',
-  ENOFLASH = 'ENOFLASH',
-  EBLACKLISTED = 'EBLACKLISTED',
-  EBLACKLISTED1 = 'EBLACKLISTED1',
-  EBLACKLISTED2 = 'EBLACKLISTED2',
-  ENOCONN = 'ENOCONN',
-}
+export type ServerState = 'BASIC'
+  | 'PLUS'
+  | 'PRO'
+  | 'TRIAL'
+  | 'EEXPIRED'
+  | 'ENOKEYFILE'
+  | 'EGUID'
+  | 'EGUID1'
+  | 'ETRIAL'
+  | 'ENOKEYFILE2'
+  | 'ENOKEYFILE1'
+  | 'ENOFLASH'
+  | 'ENOFLASH1'
+  | 'ENOFLASH2'
+  | 'ENOFLASH3'
+  | 'ENOFLASH4'
+  | 'ENOFLASH5'
+  | 'ENOFLASH6'
+  | 'ENOFLASH7'
+  | 'EBLACKLISTED'
+  | 'EBLACKLISTED1'
+  | 'EBLACKLISTED2'
+  | 'ENOCONN'
+  | undefined;
 export interface Server {
   apiKey?: string;
   avatar?: string;
@@ -42,8 +48,7 @@ export interface Server {
   regGen?: number;
   regGuid?: string;
   site?: string;
-  // state?: ServerState;
-  state: string;
+  state?: ServerState;
   theme: Theme;
   uptime?: number;
   username?: string;
@@ -67,7 +72,7 @@ export interface ServerAccountCallbackSendPayload {
   regGen?: number;
   regGuid?: string;
   site?: string;
-  state: string;
+  state: ServerState;
   wanFQDN?: string;
 }
 
@@ -81,7 +86,7 @@ export interface ServerPurchaseCallbackSendPayload {
   keyTypeForPurchase: ServerKeyTypeForPurchase;
   locale: string;
   registered: boolean;
-  state: string;
+  state: ServerState;
   site: string;
 }
 
