410cb5b9eeca4823e3b125da932cacaaf37bf1e8
Zack Spear <hi@zackspear.com>
2023-07-07 16:51:09 -0700

    refactor: reorder content in upc dropdown

--- components/UserProfile/DropdownConnectStatus.vue
+++ components/UserProfile/DropdownConnectStatus.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
+import { ExclamationTriangleIcon, CheckCircleIcon } from '@heroicons/vue/24/solid';
 // import { storeToRefs } from 'pinia';
 // import { useServerStore } from '~/store/server';
 // const { stateData } = storeToRefs(useServerStore());
@@ -10,14 +10,19 @@ const apiLoading = ref(false);
 </script>
 
 <template>
-  <li class="mb-8px px-8px flex flex-col items-center">
+  <li class="px-8px flex flex-col items-center">
     <template v-if="apiLoading">
       <BrandLoading class="w-36px mx-auto" :height="21" />
       <span class="text-12px italic opacity-80">{{ 'Loading Connect status…' }}</span>
     </template>
-    <span v-else class="w-full flex flex-row justify-start items-center gap-x-8px">
-      <ExclamationTriangleIcon v-if="onlineStatus !== 'online'" class="text-unraid-red w-16px h-16px" />
-      <span v-else class="block w-12px h-12px bg-green-600 rounded-full"></span>
+    <span
+      v-else
+      class="w-full flex flex-row justify-start items-center gap-x-8px"
+      :title="onlineStatus !== 'online' ? 'Connect is not connected to Unraid cloud services' : 'Connect is connected to Unraid cloud services'"
+    >
+      <!-- <span class="block w-12px h-12px bg-green-600 rounded-full"></span> -->
+      <CheckCircleIcon v-if="onlineStatus === 'online'" class="text-green-500 w-16px h-16px" />
+      <ExclamationTriangleIcon v-else class="text-red-500 w-16px h-16px" />
       <span>{{ onlineStatus !== 'online' ? 'Disconnected' : 'Connected' }}</span>
     </span>
   </li>
