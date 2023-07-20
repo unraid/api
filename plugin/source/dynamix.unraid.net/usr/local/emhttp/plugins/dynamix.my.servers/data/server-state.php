47a238b140631b483c9c004b28ee171c58629cb5
Zack Spear <hi@zackspear.com>
2023-07-20 14:12:01 -0700

    refactor(plg): upc server state data simplify

---
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/data/server-state.php
@@ -0,0 +1,11 @@
+<?php
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+
+$var = (array)parse_ini_file('state/var.ini');
+
+require_once "$docroot/webGui/include/Helpers.php";
+require_once "$docroot/plugins/dynamix.my.servers/include/state.php";
+
+header('Content-type: application/json');
+
+echo json_encode($serverState);
