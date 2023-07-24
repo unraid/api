c1d5da9cd7a106e4c76cbdb48202ea44836f0f2b
Zack Spear <hi@zackspear.com>
2023-07-24 14:30:52 -0700

    chore: clean up

--- composables/gql/gql.ts
+++ composables/gql/gql.ts
@@ -13,13 +13,13 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n": types.CloudStatusDocument,
     "\n  fragment FragmentCloud on Cloud {\n    error\n    apiKey {\n      valid\n      error\n    }\n    cloud {\n      status\n      error\n    }\n    minigraphql {\n      status\n      error\n    }\n    relay {\n      status\n      error\n    }\n  }\n": types.FragmentCloudFragmentDoc,
     "\n  fragment FragmentConfig on Config {\n    error\n    valid\n  }\n": types.FragmentConfigFragmentDoc,
     "\n  fragment FragmentOwner on Owner {\n    avatar\n    username\n  }\n": types.FragmentOwnerFragmentDoc,
     "\n  fragment FragmentRegistration on Registration {\n    state\n    expiration\n    keyFile {\n      contents\n    }\n  }\n": types.FragmentRegistrationFragmentDoc,
     "\n  fragment FragmentVars on Vars {\n    regGen\n    regState\n    configError\n    configValid\n  }\n": types.FragmentVarsFragmentDoc,
     "\n  query serverState {\n    cloud {\n      ...FragmentCloud\n    }\n    config {\n      ...FragmentConfig\n    }\n    info {\n      os {\n        hostname\n      }\n    }\n    owner {\n      ...FragmentOwner\n    }\n    registration {\n      ...FragmentRegistration\n    }\n    vars {\n      ...FragmentVars\n    }\n  }\n": types.serverStateDocument,
+    "\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n": types.CloudStatusDocument,
 };
 
 /**
@@ -36,10 +36,6 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(source: "\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n"): (typeof documents)["\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
@@ -64,6 +60,10 @@ export function graphql(source: "\n  fragment FragmentVars on Vars {\n    regGen
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  query serverState {\n    cloud {\n      ...FragmentCloud\n    }\n    config {\n      ...FragmentConfig\n    }\n    info {\n      os {\n        hostname\n      }\n    }\n    owner {\n      ...FragmentOwner\n    }\n    registration {\n      ...FragmentRegistration\n    }\n    vars {\n      ...FragmentVars\n    }\n  }\n"): (typeof documents)["\n  query serverState {\n    cloud {\n      ...FragmentCloud\n    }\n    config {\n      ...FragmentConfig\n    }\n    info {\n      os {\n        hostname\n      }\n    }\n    owner {\n      ...FragmentOwner\n    }\n    registration {\n      ...FragmentRegistration\n    }\n    vars {\n      ...FragmentVars\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n"): (typeof documents)["\n  query CloudStatus {\n    cloud {\n      ...FragmentCloud\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
