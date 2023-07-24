669df18f19a1692cff6ba5d0aed3cd82d410ce1e
Zack Spear <hi@zackspear.com>
2023-07-24 14:22:03 -0700

    chore: lint

--- components/Modal.vue
+++ components/Modal.vue
@@ -42,7 +42,15 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
 
 <template>
   <TransitionRoot appear :show="open" as="template">
-    <div @keyup.esc="closeModal" ref="trapRef" class="fixed inset-0 z-10 overflow-y-auto" role="dialog" aria-dialog="true" :aria-labelledby="ariaLablledById" tabindex="-1">
+    <div
+      ref="trapRef"
+      class="fixed inset-0 z-10 overflow-y-auto"
+      role="dialog"
+      aria-dialog="true"
+      :aria-labelledby="ariaLablledById"
+      tabindex="-1"
+      @keyup.esc="closeModal"
+    >
       <TransitionChild
         appear
         as="template"
@@ -54,9 +62,9 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
         leave-to="opacity-0"
       >
         <div
-          @click="closeModal"
           class="fixed inset-0 z-0 bg-black bg-opacity-80 transition-opacity"
           title="Click to close modal"
+          @click="closeModal"
         />
       </TransitionChild>
       <div class="text-center flex min-h-full items-center justify-center p-4 md:p-0">
@@ -80,7 +88,7 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
             class="text-16px text-beta bg-alpha text-left relative flex flex-col justify-around p-16px my-24px sm:p-24px border-2 border-solid shadow-xl transform overflow-hidden rounded-lg transition-all sm:w-full"
           >
             <div v-if="showCloseX" class="absolute z-20 right-0 top-0 hidden pt-2 pr-2 sm:block">
-              <button @click="closeModal" type="button" class="rounded-md text-beta bg-alpha p-2 hover:text-white focus:text-white hover:bg-unraid-red focus:bg-unraid-red focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <button type="button" class="rounded-md text-beta bg-alpha p-2 hover:text-white focus:text-white hover:bg-unraid-red focus:bg-unraid-red focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="closeModal">
                 <span class="sr-only">Close</span>
                 <XMarkIcon class="h-6 w-6" aria-hidden="true" />
               </button>
@@ -90,18 +98,20 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
               <template v-if="!$slots['header']">
                 <h1 v-if="title" :id="ariaLablledById" class="text-24px font-semibold flex flex-wrap justify-center gap-x-1">
                   {{ title }}
-                  <slot name="headerTitle"></slot>
+                  <slot name="headerTitle" />
                 </h1>
-                <h2 v-if="description" class="text-20px opacity-75">{{ description }}</h2>
+                <h2 v-if="description" class="text-20px opacity-75">
+                  {{ description }}
+                </h2>
               </template>
-              <slot name="header"></slot>
+              <slot name="header" />
             </header>
-            <slot name="main"></slot>
+            <slot name="main" />
 
             <footer v-if="$slots['footer']" class="text-14px relative -mx-16px -mb-16px sm:-mx-24px sm:-mb-24px p-4 sm:p-6">
-              <div class="absolute z-0 inset-0 opacity-10 bg-beta"></div>
+              <div class="absolute z-0 inset-0 opacity-10 bg-beta" />
               <div class="relative z-10">
-                <slot name="footer"></slot>
+                <slot name="footer" />
               </div>
             </footer>
           </div>
