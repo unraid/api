aff12d7f64de718f744679f53538e60e7f7e19a9
Zack Spear <hi@zackspear.com>
2023-07-24 14:21:41 -0700

    refactor: connect status error

--- components/UserProfile/DropdownError.vue
+++ components/UserProfile/DropdownError.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// eslint-disable vue/no-v-html
 import { storeToRefs } from 'pinia';
 
 import { useErrorsStore } from '~/store/errors';
@@ -10,12 +11,13 @@ const { errors } = storeToRefs(errorsStore);
 <template>
   <ul v-if="errors.length" class="list-reset flex flex-col gap-y-8px mb-4px border-2 border-solid border-unraid-red/90 rounded-md">
     <li v-for="(error, index) in errors" :key="index" class="flex flex-col gap-8px">
-      <h3 class="text-18px py-4px px-16px text-white bg-unraid-red/90 font-semibold text-center">
+      <h3 class="text-18px py-4px px-12px text-white bg-unraid-red/90 font-semibold">
         <span>{{ error.heading }}</span>
       </h3>
-      <div v-html="error.message" class="text-14px px-12px"></div>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <div class="text-14px px-12px" :class="{ 'pb-8px': !error.actions }" v-html="error.message" />
       <nav v-if="error.actions">
-        <li v-for="(link, index) in error.actions" :key="`link_${index}`">
+        <li v-for="(link, idx) in error.actions" :key="`link_${idx}`">
           <UpcDropdownItem :item="link" :rounded="false" />
         </li>
       </nav>
