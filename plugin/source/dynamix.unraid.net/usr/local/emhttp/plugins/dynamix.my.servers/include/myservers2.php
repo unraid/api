e950a168271363017ee4408cfbe7c79066042bc1
Zack Spear <hi@zackspear.com>
2023-07-24 16:51:36 -0700

    refactor: web components renamed to unraid-

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/myservers2.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/myservers2.php
@@ -2,14 +2,14 @@
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 require_once("$docroot/plugins/dynamix.my.servers/include/state.php");
 // var_dump($serverState);
-echo "<connect-user-profile server='" . json_encode($serverState) . "'></connect-user-profile>";
+echo "<unraid-user-profile server='" . json_encode($serverState) . "'></unraid-user-profile>";
 ?>
 <script>
 /**
  * So we're not needing to modify DefaultLayout with an additional include,
  * we'll add the Modals web component to the bottom of the body
  */
-const modalsWebComponent = 'connect-modals';
+const modalsWebComponent = 'unraid-modals';
 if (!document.getElementsByTagName(modalsWebComponent).length) {
     const $body = document.getElementsByTagName('body')[0];
     const $modals = document.createElement(modalsWebComponent);
