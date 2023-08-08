577e8f841f02f8e0e7982f365d6e8b7c7ac23328
Eli Bosley <ekbosley@gmail.com>
2023-08-08 16:01:20 -0400

    feat: don't watch libvirt until after start

--- api/src/core/utils/vms/get-hypervisor.ts
+++ api/src/core/utils/vms/get-hypervisor.ts
@@ -3,179 +3,55 @@
  * Written by: Alexis Tyler
  */
 
-import fs from 'fs';
+import { access } from 'fs/promises';
+import { constants } from 'fs';
+
 import path from 'path';
-import { ConnectListAllDomainsFlags, Hypervisor } from '@vmngr/libvirt';
-import { watch } from 'chokidar';
+import { Hypervisor } from '@vmngr/libvirt';
 import { libvirtLogger } from '@app/core/log';
-import { pubsub } from '@app/core/pubsub';
-import { sleep } from '@app/core/utils/misc/sleep';
-import type { Domain } from '@vmngr/libvirt';
+import { Exception } from 'bycontract';
 
 const uri = process.env.LIBVIRT_URI ?? 'qemu:///system';
 
 let hypervisor: Hypervisor | null;
 
-const libvirtDir = '/var/run/libvirt/';
+const libvirtPid = '/var/run/libvirt/libvirtd.pid';
 
-// Watch extra origin path for changes
-const libvirtDirWatcher = watch(libvirtDir, {
-	persistent: true,
-	ignoreInitial: true,
-});
+const isLibvirtRunning = async (): Promise<boolean> => {
+    try {
+        await access(libvirtPid, constants.F_OK | constants.R_OK);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
 
-// Restart hypervisor connection
-libvirtDirWatcher.on('all', async (event, fileName) => {
-	// VM hypervisor stopped
-	if (event === 'unlink' && fileName === '/var/run/libvirt/libvirt-sock') {
-		// Connection has already been killed
-		if (!hypervisor || hypervisor === null) {
-			return;
-		}
+export const getHypervisor = async (): Promise<Hypervisor> => {
+    // Return hypervisor if it's already connected
+    const running = await isLibvirtRunning();
 
-		// Kill connection
-		await hypervisor.connectClose().catch(() => undefined);
+    if (hypervisor && running) {
+        return hypervisor;
+    }
 
-		hypervisor = null;
-	}
-
-	// VM hypervisor started
-	if (event === 'add' && fileName === '/var/run/libvirt/libvirt-sock') {
-		// Start connection
-		hypervisor = new Hypervisor({ uri });
-		await hypervisor.connectOpen().catch((error: unknown) => {
-			libvirtLogger.error(`Failed restarting VM hypervisor connection with "${(error as Error).message}"`);
-		});
-	}
-});
-
-export const getHypervisor = async (useCache = true): Promise<Hypervisor | null> => {
-	// Return hypervisor if it's already connected
-	if (useCache && hypervisor) {
-		return hypervisor;
-	}
-
-	// Check if libvirt service is running and then connect
-	const running = fs.existsSync(path.join(libvirtDir, 'libvirtd.pid'));
 	if (!running) {
-		return null;
+		hypervisor = null;
+		throw new Error('Libvirt is not running');
 	}
 
-	hypervisor = new Hypervisor({ uri });
-	await hypervisor.connectOpen().catch((error: unknown) => {
-		libvirtLogger.error(`Failed starting VM hypervisor connection with "${(error as Error).message}"`);
-	});
 
-	return hypervisor;
+
+
+    hypervisor = new Hypervisor({ uri });
+    await hypervisor.connectOpen().catch((error: unknown) => {
+        libvirtLogger.error(
+            `Failed starting VM hypervisor connection with "${
+                (error as Error).message
+            }"`
+        );
+
+        throw error;
+    });
+
+    return hypervisor;
 };
-
-const states = {
-	0: 'NOSTATE',
-	1: 'RUNNING',
-	2: 'IDLE',
-	3: 'PAUSED',
-	4: 'SHUTDOWN',
-	5: 'SHUTOFF',
-	6: 'CRASHED',
-	7: 'PMSUSPENDED',
-};
-
-type CachedDomain = {
-	name: string;
-	uuid: string;
-	state: string;
-	autoStart: boolean;
-	features: Record<string, unknown>;
-};
-
-let cachedDomains: CachedDomain[];
-
-const watchLibvirt = async (useCache = true) => {
-	try {
-		const hypervisor = await getHypervisor(useCache);
-		if (!hypervisor) {
-			await sleep(1_000);
-			return watchLibvirt(useCache);
-		}
-
-		// We now have a hypervisor instance
-		const autoStartDomains = await hypervisor.connectListAllDomains(ConnectListAllDomainsFlags.AUTOSTART);
-		const autoStartDomainNames = await Promise.all(autoStartDomains.map(async domain => hypervisor.domainGetName(domain))) ?? [];
-
-		// Get all domains
-		const domains = await hypervisor.connectListAllDomains() as unknown[];
-		const resolvedDomains = await Promise.all(domains.map(async (domain: unknown): Promise<CachedDomain> => {
-			const info = await hypervisor.domainGetInfo(domain as Domain)
-			const name = await hypervisor.domainGetName(domain as Domain);
-			const uuid = await hypervisor.domainGetUUIDString(domain as Domain);
-			const features = {};
-			const result = {
-				name,
-				uuid,
-				state: states[info.state],
-				autoStart: autoStartDomainNames?.includes(name),
-				features,
-			};
-			return result as CachedDomain;
-		}));
-
-		// If the result is the same as the cache wait 5s then retry
-		if (JSON.stringify(cachedDomains) === JSON.stringify(resolvedDomains)) {
-			await sleep(5_000);
-			return watchLibvirt();
-		}
-
-		libvirtLogger.debug('Changes detected!');
-
-		// Update the cache with new results
-		cachedDomains = resolvedDomains;
-
-		// Update summary endpoint
-		const activeDomains = await hypervisor.connectListAllDomains(ConnectListAllDomainsFlags.ACTIVE) as unknown[];
-		const inactiveDomains = await hypervisor.connectListAllDomains(ConnectListAllDomainsFlags.INACTIVE) as unknown[];
-		const installed = activeDomains.length + inactiveDomains.length;
-		const started = activeDomains.length;
-		const summary = {
-			info: {
-				vms: {
-					installed,
-					started,
-				},
-			},
-		};
-		const full = {
-			vms: {
-				domain: cachedDomains,
-			},
-		};
-
-		// Publish changes to pub/sub
-		await pubsub.publish('info', summary).catch(error => {
-			libvirtLogger.error('Failed publishing to "info" with "%s"', error);
-		});
-
-		// Publish changes to pub/sub
-		await pubsub.publish('vms', full).catch(error => {
-			libvirtLogger.error('Failed publishing to "vms" with "%s"', error);
-		});
-
-		libvirtLogger.debug('Published full and summary data to pub/sub');
-
-		await sleep(1_000);
-		return watchLibvirt();
-	} catch (error: unknown) {
-		// We need to try and reconnect
-		if ((error as Error).message?.includes('invalid connection pointer')) {
-			libvirtLogger.warn('Reconnecting to socket...');
-			await sleep(5_000);
-			return watchLibvirt(false);
-		}
-
-		libvirtLogger.error('Failed watching with "%s"', error);
-		await sleep(5_000);
-		return watchLibvirt();
-	}
-};
-
-// Start watching libvirt for changes
-watchLibvirt();
