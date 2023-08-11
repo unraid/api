809733351044be761e88f3166b3deb7d6d3607e3
Zack Spear <hi@zackspear.com>
2023-08-11 16:02:20 -0700

    feat: add env for allowing console logs on build

--- web/helpers/functions.ts
+++ web/helpers/functions.ts
@@ -2,7 +2,7 @@
 export const OBJ_TO_STR = (obj: object): string => Object.entries(obj).reduce((str, [p, val]) => `${str}${p}: ${val}\n`, '');
 /** Removes our dev logs from prod builds */
 export const disableProductionConsoleLogs = () => {
-  if (import.meta.env.PROD) {
+  if (import.meta.env.PROD && !import.meta.env.VITE_ALLOW_CONSOLE_LOGS) {
     console.log = () => {};
     console.debug = () => {};
     console.info = () => {};
