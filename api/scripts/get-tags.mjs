543974ff2d8ca3c9f2436d198105449edb694f5a
Eli Bosley <ekbosley@gmail.com>
2023-08-31 12:09:39 -0400

    feat: fix tag

--- api/scripts/get-tags.mjs
+++ api/scripts/get-tags.mjs
@@ -1,7 +1,10 @@
+import { execSync } from 'child_process';
+
 const runCommand = (command) => {
     try {
         return execSync(command, { stdio: 'pipe' }).toString().trim();
-    } catch {
+    } catch(error) {
+        console.log('Failed to get value from tag command: ', command, error.message);
         return;
     }
 };
@@ -10,6 +13,7 @@ const getTags = () => {
     const GIT_SHA_ENV = process.env.GIT_SHA
     const IS_TAGGED_ENV = Boolean(process.env.IS_TAGGED);
     if (GIT_SHA_ENV && IS_TAGGED_ENV) {
+        console.log('GIT_SHA_ENV', GIT_SHA_ENV, 'IS_TAGGED_ENV', IS_TAGGED_ENV);
         return {
             shortSha: GIT_SHA_ENV,
             isTagged: IS_TAGGED_ENV
@@ -17,7 +21,7 @@ const getTags = () => {
     } else {
         const gitShortSHA = runCommand('git rev-parse --short HEAD');
         const isCommitTagged = runCommand('git describe --tags --abbrev=0 --exact-match') !== undefined;
-
+        console.log('gitShortSHA', gitShortSHA, 'isCommitTagged', isCommitTagged);
         return {
             shortSha: gitShortSHA,
             isTagged: isCommitTagged
