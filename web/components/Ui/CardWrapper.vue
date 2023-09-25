9994dd49f7a159adbfbcdcfa960f8bd6ef53c912
Zack Spear <hi@zackspear.com>
2023-09-25 14:39:41 -0700

    refactor(web): theme gamma opaque color for border

--- web/components/Ui/CardWrapper.vue
+++ web/components/Ui/CardWrapper.vue
@@ -6,7 +6,7 @@ withDefaults(defineProps<{ increasedPadding?: boolean }>(), {
 
 <template>
   <div
-    class="group/card p-4 text-left relative flex flex-col flex-1 text-beta bg-alpha border-2 border-solid border-gamma/50 rounded-md shadow-md hover:shadow-orange/50 transition-all"
+    class="group/card p-4 text-left relative flex flex-col flex-1 text-beta bg-alpha border-2 border-solid border-gamma-opaque rounded-md shadow-md hover:shadow-orange/50 transition-all"
     :class="increasedPadding && 'md:p-6'"
   >
     <slot></slot>
