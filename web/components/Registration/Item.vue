65f4c602070e1521c6fa3aef097e8dddebcc42a8
Zack Spear <hi@zackspear.com>
2023-09-25 18:22:22 -0700

    feat(web): WIP registration page UI UX

--- web/components/Registration/Item.vue
+++ web/components/Registration/Item.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useThemeStore } from '~/store/theme';
+
 export interface Props {
   label?: string;
   text?: number | string | undefined;
@@ -8,9 +11,16 @@ withDefaults(defineProps<Props>(), {
   label: '',
   text: '',
 });
+
+const { darkMode } = storeToRefs(useThemeStore());
+
+const evenBgColor = computed(() => {
+  return darkMode.value ? 'even:bg-grey-darkest' : 'even:bg-black/5';
+});
 </script>
+
 <template>
-  <div class="text-16px p-16px sm:px-20px sm:grid sm:grid-cols-3 sm:gap-16px even:bg-gray-200/50">
+  <div :class="evenBgColor" class="text-16px p-16px sm:px-20px sm:grid sm:grid-cols-3 sm:gap-16px">
     <dt class="font-semibold">{{ t(label) }}</dt>
     <dd class="mt-4px leading-normal sm:col-span-2 sm:mt-0">
       <span v-if="text" class="opacity-75">{{ text }}</span>
