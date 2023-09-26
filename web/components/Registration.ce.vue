17a5767108f5e7ec1b62bc741f92d2242c56c594
Zack Spear <hi@zackspear.com>
2023-09-25 20:25:19 -0700

    refactor(web): registration page UI UX

--- web/components/Registration.ce.vue
+++ web/components/Registration.ce.vue
@@ -15,6 +15,10 @@ else
   echo "Third party plugins found - PLEASE CHECK YOUR UNRAID NOTIFICATIONS AND WAIT FOR THE MESSAGE THAT IT IS SAFE TO REBOOT!"
 fi
  */
+import {
+  ShieldCheckIcon,
+  ShieldExclamationIcon,
+} from '@heroicons/vue/24/solid';
 import dayjs from 'dayjs'
 import { storeToRefs } from 'pinia';
 import { useI18n } from 'vue-i18n';
@@ -51,41 +55,59 @@ const {
 
 const items = computed(() => {
   return [
-    {
-      label: 'Registered to:',
-      text: regTo.value,
-    },
-    {
-      label: 'Registered on:',
-      text: dayjs(regTm.value).format('YYYY-MM-DD HH:mm'),
-    },
-    {
-      label: 'Updates Expire:',
-      text: dayjs(regTm.value).format('YYYY-MM-DD HH:mm'),
-    },
+    ...(regTo.value
+      ? [
+          {
+            label: 'Registered to',
+            text: regTo.value,
+          },
+        ]
+      : []
+    ),
+    ...(regTo.value
+      ? [
+          {
+            label: 'Registered on',
+            text: dayjs(regTm.value).format('YYYY-MM-DD HH:mm'),
+          },
+        ]
+      : []
+    ),
+    /**
+     * @todo factor in grandfathered users and display a different message
+     */
+    ...(regTo.value
+      ? [
+          {
+            label: 'Updates Expire',
+            text: dayjs(regTm.value).format('YYYY-MM-DD HH:mm'),
+          },
+        ]
+      : []
+    ),
     ...(state.value === 'EGUID'
       ? [
           {
-            label: 'Registered GUID:',
+            label: 'Registered GUID',
             text: regGuid.value,
           },
         ]
       : []
     ),
     {
-      label: 'Flash GUID:',
+      label: 'Flash GUID',
       text: guid.value,
     },
     {
-      label: 'Flash Vendor:',
+      label: 'Flash Vendor',
       text: flashVendor.value,
     },
     {
-      label: 'Flash Product:',
+      label: 'Flash Product',
       text: flashProduct.value,
     },
     {
-      label: 'Attached Storage Devices:',
+      label: 'Attached Storage Devices',
       text: deviceCount.value,
     },
   ];
@@ -93,38 +115,40 @@ const items = computed(() => {
 </script>
 
 <template>
-  <UiPageContainer>
-    <UiCardWrapper>
-      <div class="p-16px sm:px-20px">
-        <h3
-          class="text-24px font-semibold"
-          :class="{
-            'text-red-500': stateDataError,
-            'text-green-500': !stateDataError,
-          }"
-        >
-          {{ stateData.heading }}
-        </h3>
-        <div
-          v-if="stateData.message"
-          v-html="stateData.message"
-          class="text-16px leading-normal opacity-75"></div>
-      </div>
-      <div>
-        <dl>
-          <RegistrationItem
-            v-for="item in items"
-            :key="item.label"
-            :label="item.label"
-            :text="item.text"
-            :t="t" />
-          <div class="p-16px sm:px-20px sm:grid sm:grid-cols-3 sm:gap-16px">
-            <dt class="text-16px font-medium leading-normal">&nbsp;</dt>
-            <dd class="mt-2 text-16px sm:col-span-2 sm:mt-0">
-              <KeyActions :t="t" />
-            </dd>
-          </div>
-        </dl>
+  <UiPageContainer class="max-w-800px">
+    <UiCardWrapper :increased-padding="true">
+      <div class="flex flex-col gap-20px sm:gap-24px">
+        <header class="grid gap-y-16px">
+          <h3
+            class="text-20px md:text-24px font-semibold leading-normal flex flex-row items-center gap-8px"
+            :class="stateDataError ? 'text-unraid-red' : 'text-green-500'"
+          >
+            <component :is="stateDataError ? ShieldExclamationIcon : ShieldCheckIcon" class="w-24px h-24px" />
+            <span>
+              {{ stateData.heading }}
+            </span>
+          </h3>
+          <div
+            v-if="stateData.message"
+            v-html="stateData.message"
+            class="prose text-16px leading-relaxed whitespace-normal opacity-75"></div>
+        </header>
+        <section>
+          <dl>
+            <RegistrationItem
+              v-for="item in items"
+              :key="item.label"
+              :label="item.label"
+              :text="item.text"
+              :t="t" />
+            <div class="p-16px sm:px-20px sm:grid sm:grid-cols-3 sm:gap-16px">
+              <dt class="text-16px font-medium leading-normal">&nbsp;</dt>
+              <dd class="mt-2 text-16px sm:col-span-2 sm:mt-0">
+                <KeyActions :t="t" />
+              </dd>
+            </div>
+          </dl>
+        </section>
       </div>
     </UiCardWrapper>
   </UiPageContainer>
