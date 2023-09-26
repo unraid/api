116a5e0135cc6ec5cfa114c701231c8f111cf2ee
Zack Spear <hi@zackspear.com>
2023-09-25 20:25:19 -0700

    refactor(web): registration page UI UX

--- web/components/Ui/PageContainer.vue
+++ web/components/Ui/PageContainer.vue
@@ -8,7 +8,7 @@ withDefaults(defineProps<{
 
 <template>
   <div
-    class="grid gap-y-24px mx-auto px-16px"
+    class="grid gap-y-24px w-full mx-auto px-16px"
     :class="maxWidth"
   >
     <slot></slot>
