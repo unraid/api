559e5b8698d5df80ca57f530a2bf2cb6f01e30c7
Zack Spear <hi@zackspear.com>
2023-09-25 18:22:22 -0700

    feat(web): WIP registration page UI UX

--- web/components/Ui/CardWrapper.vue
+++ web/components/Ui/CardWrapper.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 withDefaults(defineProps<{
+  hover?: boolean;
   increasedPadding?: boolean;
-  noPadding?: boolean;
+  padding?: boolean;
 }>(), {
+  hover: true,
   increasedPadding: false,
-  noPadding: false,
+  padding: true,
 });
 </script>
 
 <template>
   <div
-    class="group/card text-left relative flex flex-col flex-1 text-beta bg-alpha border-2 border-solid border-gamma-opaque rounded-md shadow-md hover:shadow-orange/50 transition-all"
+    class="group/card text-left relative flex flex-col flex-1 text-beta bg-alpha border-2 border-solid border-gamma-opaque rounded-md shadow-md "
     :class="[
-      !noPadding && 'p-4',
+      padding && 'p-4',
       increasedPadding && 'md:p-6',
+      hover && 'hover:shadow-orange/50 transition-all',
     ]"
   >
     <slot></slot>
--- web/components/Ui/PageContainer.vue
+++ web/components/Ui/PageContainer.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  maxWidth?: string;
+}>(), {
+  maxWidth: 'max-w-1024px',
+});
+</script>
+
 <template>
-  <div class="grid gap-y-24px max-w-1024px mx-auto px-16px">
+  <div
+    class="grid gap-y-24px mx-auto px-16px"
+    :class="maxWidth"
+  >
     <slot></slot>
   </div>
 </template>
