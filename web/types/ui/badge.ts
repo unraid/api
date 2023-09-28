0487c88223d8614eb0ac91e39821504593247c04
Zack Spear <hi@zackspear.com>
2023-09-27 20:12:07 -0700

    fix(web): replaceCheck type

---
+++ web/types/ui/badge.ts
@@ -0,0 +1,12 @@
+import { XCircleIcon } from '@heroicons/vue/24/solid';
+import type { Component } from 'vue';
+
+export type UiBadgePropsColor = 'alpha' | 'beta' | 'gamma' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink' | 'orange' | 'black' | 'white' | 'transparent' | 'current' | 'custom';
+
+export interface UiBadgeProps {
+  color?: UiBadgePropsColor;
+  icon?: typeof XCircleIcon | Component;
+  iconRight?: typeof XCircleIcon | Component;
+  iconStyles?: string;
+  size?: '12px' | '14px' | '16px' | '18px' | '20px' | '24px';
+}
