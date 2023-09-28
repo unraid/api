2483d08dd44eb9ec23ee0f20ebfbc455491f95bf
Zack Spear <hi@zackspear.com>
2023-09-28 13:06:45 -0700

    refactor(web): CardWrapper error styles prop

--- web/components/Ui/CardWrapper.vue
+++ web/components/Ui/CardWrapper.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 withDefaults(defineProps<{
+  error?: boolean,
   hover?: boolean;
   increasedPadding?: boolean;
   padding?: boolean;
 }>(), {
+  error: false,
   hover: true,
   increasedPadding: false,
   padding: true,
@@ -12,11 +14,12 @@ withDefaults(defineProps<{
 
 <template>
   <div
-    class="group/card text-left relative flex flex-col flex-1 text-beta bg-alpha border-2 border-solid border-gamma-opaque rounded-md shadow-md "
+    class="group/card text-left relative flex flex-col flex-1 border-2 border-solid rounded-md shadow-md "
     :class="[
       padding && 'p-4',
       increasedPadding && 'md:p-6',
       hover && 'hover:shadow-orange/50 transition-all',
+      error ? 'text-white bg-unraid-red' : 'text-beta bg-alpha border-gamma-opaque',
     ]"
   >
     <slot></slot>
