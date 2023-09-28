ede1e26b08e3e97156263ad76a8d4d14143b9bff
Zack Spear <hi@zackspear.com>
2023-09-27 21:09:15 -0700

    refactor(web): registration ux/ui button placement

--- web/components/Registration/ReplaceCheck.vue
+++ web/components/Registration/ReplaceCheck.vue
@@ -5,6 +5,7 @@ import {
 } from '@heroicons/vue/24/solid';
 import { storeToRefs } from 'pinia';
 
+import { DOCS_REGISTRATION_REPLACE_KEY } from '~/helpers/urls';
 import BrandLoadingWhite from '~/components/Brand/LoadingWhite.vue';
 import { useReplaceCheckStore } from '~/store/replaceCheck';
 
@@ -17,30 +18,31 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-wrap items-start justify-between gap-8px">
     <BrandButton
       v-if="status === 'checking' || status === 'ready'"
       @click="replaceCheckStore.check"
       :disabled="status !== 'ready'"
       :icon="status === 'checking' ? BrandLoadingWhite : KeyIcon"
       :text="t('Check Eligibility')"
-      class="w-full sm:max-w-300px" />
+      class="flex-grow" />
 
-    <p
+    <UiBadge
       v-else-if="statusOutput"
-      class="flex flex-col sm:flex-row items-start justify-between gap-4px"
+      :color="statusOutput.color"
+      :icon="statusOutput.icon"
+      size="16px"
     >
-      <UiBadge :color="statusOutput.color" :icon="statusOutput.icon" size="16px">
-        {{ t(statusOutput.text) }}
-      </UiBadge>
-      <BrandButton
-        v-if="status !== 'checking' || status === 'ready'"
-        btn-style="underline"
-        :external="true"
-        :href="'https://docs.unraid.net/unraid-os/manual/changing-the-flash-device/'"
-        :iconRight="ArrowTopRightOnSquareIcon"
-        :text="t('Learn More')"
-      />
-    </p>
+      {{ t(statusOutput.text) }}
+    </UiBadge>
+
+    <BrandButton
+      btn-style="underline"
+      :external="true"
+      :href="DOCS_REGISTRATION_REPLACE_KEY.toString()"
+      :iconRight="ArrowTopRightOnSquareIcon"
+      :text="t('Learn More')"
+      class="text-14px"
+    />
   </div>
 </template>
--- web/components/Registration/UpgradeExpiration.vue
+++ web/components/Registration/UpgradeExpiration.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/solid';
 import { storeToRefs } from 'pinia';
 
 import useTimeHelper from '~/composables/time';
+import { DOCS_REGISTRATION_LICENSING } from '~/helpers/urls';
 import { useServerStore } from '~/store/server';
 
 export interface Props {
@@ -10,9 +12,8 @@ export interface Props {
 
 const props = defineProps<Props>();
 
-
 const serverStore = useServerStore();
-const { dateTimeFormat, regTy, regExp, regUpdatesExpired, state } = storeToRefs(serverStore);
+const { dateTimeFormat, regTy, regExp, regUpdatesExpired, renewAction, state } = storeToRefs(serverStore);
 
 const { buildStringFromValues, dateDiff, formatDate } = useTimeHelper(dateTimeFormat.value, props.t);
 
@@ -51,7 +52,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <span v-if="output" class="flex flex-col gap-y-4px">
+  <div v-if="output" class="flex flex-col gap-8px">
     <p
       :title="output.title"
     >
@@ -60,5 +61,27 @@ onBeforeUnmount(() => {
     <p v-if="regUpdatesExpired" class="text-14px opacity-90">
       <em>{{ t('Renew your license key to continue receiving OS updates.') }}</em>
     </p>
-  </span>
+    <div class="flex flex-wrap items-start justify-between gap-8px">
+      <BrandButton
+        v-if="regUpdatesExpired" 
+        :btn-style="'gray'"
+        :disabled="renewAction?.disabled"
+        :external="renewAction?.external"
+        :icon="renewAction.icon"
+        :text="t('Renew Key')"
+        @click="renewAction.click()"
+        :title="t('Renew your license key to continue receiving OS updates.')"
+        class="flex-grow"
+      />
+
+      <BrandButton
+        btn-style="underline"
+        :external="true"
+        :href="DOCS_REGISTRATION_LICENSING.toString()"
+        :iconRight="ArrowTopRightOnSquareIcon"
+        :text="t('Learn More')"
+        class="text-14px"
+      />
+    </div>
+  </div>
 </template>
