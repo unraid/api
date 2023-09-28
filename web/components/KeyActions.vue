2581254a0290e90bd87f643056baa5a7efc13ffb
Zack Spear <hi@zackspear.com>
2023-09-27 20:13:23 -0700

    refactor(web): key actions component filter props

--- web/components/KeyActions.vue
+++ web/components/KeyActions.vue
@@ -3,16 +3,28 @@ import { storeToRefs } from 'pinia';
 
 import { useServerStore } from '~/store/server';
 
-defineProps<{
+const props = defineProps<{
+  filterBy?: string[] | undefined;
+  filterOut?: string[] | undefined;
   t: any;
 }>();
 
 const { keyActions } = storeToRefs(useServerStore());
+
+const filteredKeyActions = computed(() => {
+  if (!keyActions.value || (!props.filterOut && !props.filterBy)) return keyActions.value;
+
+  return keyActions.value.filter((action: { name: string; }) => {
+    return props.filterOut
+      ? !props.filterOut?.includes(action.name)
+      : props.filterBy?.includes(action.name);
+  });
+});
 </script>
 
 <template>
-  <ul v-if="keyActions" class="flex flex-col gap-y-8px">
-    <li v-for="action in keyActions" :key="action.name">
+  <ul v-if="filteredKeyActions" class="flex flex-col gap-y-8px">
+    <li v-for="action in filteredKeyActions" :key="action.name">
       <BrandButton
         class="w-full sm:max-w-300px"
         :disabled="action?.disabled"
