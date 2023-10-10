7d71e307db496db3fe797439b25ff44168a8017e
Zack Spear <hi@zackspear.com>
2023-10-10 17:11:04 -0500

    refactor(web): button disabled styles

--- web/components/Brand/Button.vue
+++ web/components/Brand/Button.vue
@@ -36,7 +36,7 @@ defineEmits(['click']);
 
 const classes = computed(() => {
   /** @todo consider underline for all buttons to improve accessibility and quick readability */
-  const buttonDefaults = 'group text-center font-semibold leading-none relative z-0 flex flex-row items-center justify-center border-2 border-solid shadow-none cursor-pointer rounded-md hover:shadow-md focus:shadow-md disabled:opacity-50 disabled:hover:opacity-50 disabled:focus:opacity-50 disabled:cursor-not-allowed';
+  const buttonDefaults = 'group text-center font-semibold leading-none relative z-0 flex flex-row items-center justify-center border-2 border-solid shadow-none cursor-pointer rounded-md hover:shadow-md focus:shadow-md disabled:opacity-25 disabled:hover:opacity-25 disabled:focus:opacity-25 disabled:cursor-not-allowed';
   let buttonColors = '';
   let buttonSize = '';
   let iconSize = '';
