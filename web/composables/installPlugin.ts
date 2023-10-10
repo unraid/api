b0bee7623348dbc9f6559d2c00efb25b2caddec0
Zack Spear <hi@zackspear.com>
2023-10-10 17:14:11 -0500

    fix(web): installPlugin composable for os updates

--- web/composables/installPlugin.ts
+++ web/composables/installPlugin.ts
@@ -9,12 +9,16 @@ const useInstallPlugin = () => {
     try {
       // @ts-ignore – `openPlugin` will be included in 6.10.4+ DefaultPageLayout
       if (typeof openPlugin === 'function') {
+        const plgUrl = new URL(payload.pluginUrl);
+        const installString = `${plgUrl.pathname.replace('.plg', '').substring(1)}:install`; // mimic what is done on the install plg page JS but without the regex that's hard to read
         // @ts-ignore
         openPlugin(
-          `plugin ${payload.update ? 'update' : 'install'} ${payload.pluginUrl}`,
-          payload.modalTitle,
-          '',
-          'refresh',
+          `plugin ${payload.update ? 'update' : 'install'} ${payload.pluginUrl}`, // command
+          payload.modalTitle, // title
+          installString, // plg
+          'refresh', // function defined in DefaultPageLayout.php
+          0, // run command only when not already running (default)
+          1, // hide close button
         );
       } else {
         // `openBox()` is defined in the webgui's DefaultPageLayout.php and used when openPlugin is not available
