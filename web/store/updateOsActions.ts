13936bb157f9097a19c7498fce252f3f86526ccb
Zack Spear <hi@zackspear.com>
2023-11-06 14:14:03 -0800

    fix(web): upc dropdown updates external icon

--- web/store/updateOsActions.ts
+++ web/store/updateOsActions.ts
@@ -104,7 +104,7 @@ export const useUpdateOsActionsStore = defineStore('updateOsActions', () => {
           serverStore.inIframe ? 'newTab' : undefined,
         );
       },
-      external: !!updateOsStore.available,
+      external: true,
       icon: updateOsStore.available ? BellAlertIcon : ArrowPathIcon,
       name: 'updateOs',
       text: updateOsStore.available ? 'Unraid OS {0} Update Available' : 'Check for OS Updates',
