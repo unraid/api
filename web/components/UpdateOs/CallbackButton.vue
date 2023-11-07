9b9a6998b783f5786d4ffa376a3775996ef7751f
Zack Spear <hi@zackspear.com>
2023-11-07 10:17:07 -0800

    refactor(web): update page redirect

--- web/components/UpdateOs/CallbackButton.vue
+++ web/components/UpdateOs/CallbackButton.vue
@@ -3,23 +3,11 @@ import { ArrowPathIcon, ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/soli
 import { storeToRefs } from 'pinia';
 import { onBeforeMount } from 'vue';
 
-import { WEBGUI_TOOLS_UPDATE } from '~/helpers/urls';
-import { useUpdateOsActionsStore } from '~/store/updateOsActions';
-
 defineProps<{
   t: any;
 }>();
 
 const updateOsActionsStore = useUpdateOsActionsStore();
-
-const { rebootType } = storeToRefs(updateOsActionsStore);
-
-onBeforeMount(() => {
-  /** when we're not prompting for reboot /Tools/Update will automatically send the user to account.unraid.net/server/update-os */
-  if (window.location.pathname === WEBGUI_TOOLS_UPDATE.pathname && rebootType.value === '') {
-    updateOsActionsStore.executeUpdateOsCallback(true);
-  }
-});
 </script>
 
 <template>
