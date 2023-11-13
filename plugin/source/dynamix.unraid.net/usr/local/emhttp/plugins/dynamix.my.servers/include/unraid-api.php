96d27a3124adec300c3eccee17ac2d77e3cd3fa5
Zack Spear <hi@zackspear.com>
2023-11-13 14:56:12 -0800

    chore: copyright comments

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/unraid-api.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/unraid-api.php
@@ -1,6 +1,5 @@
 <?PHP
 /* Copyright 2005-2023, Lime Technology
- * Copyright 2012-2023, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -9,8 +8,6 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  */
-?>
-<?
 $cli = php_sapi_name() == 'cli';
 
 $docroot ??= ($_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp');
