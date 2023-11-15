81f7f41b3b7aa47b6c70b9c7d0401d8d0d575840
Zack Spear <hi@zackspear.com>
2023-11-15 13:01:13 -0800

    fix(web): use dateTime format from server

--- web/components/UserProfile/UptimeExpire.vue
+++ web/components/UserProfile/UptimeExpire.vue
@@ -34,6 +34,8 @@ const countUp = computed<boolean>(() => {
   return state.value !== 'TRIAL' && state.value !== 'ENOCONN';
 });
 
+console.debug('[useDateTimeHelper] params', dateTimeFormat.value, props.t, false, time.value, countUp.value);
+
 const {
   outputDateTimeReadableDiff: readableDiff,
   outputDateTimeFormatted: formatted,
