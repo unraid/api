47f08ea3594a91098f67718c0123110c7b5f86f7
Zack Spear <hi@zackspear.com>
2024-04-30 16:08:20 -0700

    feat: postbuild script to add timestamp to web component manifest

---
+++ web/scripts/add-timestamp-webcomponent-manifest.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
+// Read the JSON file
+const filePath = '../web/.nuxt/nuxt-custom-elements/dist/unraid-components/manifest.json';
+const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+// Add timestamp (ts) to the JSON data
+const timestamp = Math.floor(new Date().getTime() / 1000); // Current timestamp epoch
+jsonData.ts = timestamp;
+
+// Write the updated JSON back to the file
+fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+
+console.log(`Timestamp: ${timestamp} added to the web component manifest.`);
