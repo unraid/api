1d944781cf528d9dc703a490ec4003568e66fce1
Eli Bosley <ekbosley@gmail.com>
2024-05-06 13:40:42 -0400

    feat: add a timestamp to flash backup (#877)
    * feat: add a timestamp to flash backup
    
    * feat: update gitignore
    
    * feat: random interval is now 30 minutes

--- api/src/store/modules/paths.ts
+++ api/src/store/modules/paths.ts
@@ -2,29 +2,54 @@ import { createSlice } from '@reduxjs/toolkit';
 import { join, resolve as resolvePath } from 'path';
 
 const initialState = {
-	core: __dirname,
-	'unraid-api-base': '/usr/local/bin/unraid-api/' as const,
-	'unraid-data': resolvePath(process.env.PATHS_UNRAID_DATA ?? '/boot/config/plugins/dynamix.my.servers/data/' as const),
-	'docker-autostart': '/var/lib/docker/unraid-autostart' as const,
-	'docker-socket': '/var/run/docker.sock' as const,
-	'parity-checks': '/boot/config/parity-checks.log' as const,
-	htpasswd: '/etc/nginx/htpasswd' as const,
-	'emhttpd-socket': '/var/run/emhttpd.socket' as const,
-	states: resolvePath(process.env.PATHS_STATES ?? '/usr/local/emhttp/state/' as const),
-	'dynamix-base': resolvePath(process.env.PATHS_DYNAMIX_BASE ?? '/boot/config/plugins/dynamix/' as const),
-	'dynamix-config': resolvePath(process.env.PATHS_DYNAMIX_CONFIG ?? '/boot/config/plugins/dynamix/dynamix.cfg' as const),
-	'myservers-base': '/boot/config/plugins/dynamix.my.servers/' as const,
-	'myservers-config': resolvePath(process.env.PATHS_MY_SERVERS_CONFIG ?? '/boot/config/plugins/dynamix.my.servers/myservers.cfg' as const),
-	'myservers-config-states': join(resolvePath(process.env.PATHS_STATES ?? '/usr/local/emhttp/state/' as const), 'myservers.cfg' as const),
-	'myservers-env': '/boot/config/plugins/dynamix.my.servers/env' as const,
-	'keyfile-base': resolvePath(process.env.PATHS_KEYFILE_BASE ?? '/boot/config' as const),
-	'machine-id': resolvePath(process.env.PATHS_MACHINE_ID ?? '/var/lib/dbus/machine-id' as const),
-	'log-base': resolvePath('/var/log/unraid-api/' as const),
-	'var-run': '/var/run' as const,
+    core: __dirname,
+    'unraid-api-base': '/usr/local/bin/unraid-api/' as const,
+    'unraid-data': resolvePath(
+        process.env.PATHS_UNRAID_DATA ??
+            ('/boot/config/plugins/dynamix.my.servers/data/' as const)
+    ),
+    'docker-autostart': '/var/lib/docker/unraid-autostart' as const,
+    'docker-socket': '/var/run/docker.sock' as const,
+    'parity-checks': '/boot/config/parity-checks.log' as const,
+    htpasswd: '/etc/nginx/htpasswd' as const,
+    'emhttpd-socket': '/var/run/emhttpd.socket' as const,
+    states: resolvePath(
+        process.env.PATHS_STATES ?? ('/usr/local/emhttp/state/' as const)
+    ),
+    'dynamix-base': resolvePath(
+        process.env.PATHS_DYNAMIX_BASE ??
+            ('/boot/config/plugins/dynamix/' as const)
+    ),
+    'dynamix-config': resolvePath(
+        process.env.PATHS_DYNAMIX_CONFIG ??
+            ('/boot/config/plugins/dynamix/dynamix.cfg' as const)
+    ),
+    'myservers-base': '/boot/config/plugins/dynamix.my.servers/' as const,
+    'myservers-config': resolvePath(
+        process.env.PATHS_MY_SERVERS_CONFIG ??
+            ('/boot/config/plugins/dynamix.my.servers/myservers.cfg' as const)
+    ),
+    'myservers-config-states': join(
+        resolvePath(
+            process.env.PATHS_STATES ?? ('/usr/local/emhttp/state/' as const)
+        ),
+        'myservers.cfg' as const
+    ),
+    'myservers-env': '/boot/config/plugins/dynamix.my.servers/env' as const,
+    'myservers-keepalive':
+        process.env.PATHS_MY_SERVERS_FB ?? ('/boot/config/plugins/dynamix.my.servers/fb_keepalive' as const), 
+    'keyfile-base': resolvePath(
+        process.env.PATHS_KEYFILE_BASE ?? ('/boot/config' as const)
+    ),
+    'machine-id': resolvePath(
+        process.env.PATHS_MACHINE_ID ?? ('/var/lib/dbus/machine-id' as const)
+    ),
+    'log-base': resolvePath('/var/log/unraid-api/' as const),
+    'var-run': '/var/run' as const,
 };
 
 export const paths = createSlice({
-	name: 'paths',
-	initialState,
-	reducers: {},
+    name: 'paths',
+    initialState,
+    reducers: {},
 });
---
+++ api/src/unraid-api/cron/write-flash-file.service.ts
@@ -0,0 +1,55 @@
+import { ONE_DAY_MS, THIRTY_MINUTES_MS } from '@app/consts';
+import { sleep } from '@app/core/utils/misc/sleep';
+import { convertToFuzzyTime } from '@app/mothership/utils/convert-to-fuzzy-time';
+import { getters } from '@app/store/index';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class WriteFlashFileService {
+    constructor() {}
+    private readonly logger = new Logger(WriteFlashFileService.name);
+    private fileLocation = getters.paths()['myservers-keepalive'];
+    public randomizeWriteTime = true;
+    public writeNewTimestamp = async (): Promise<number> => {
+        const wait = this.randomizeWriteTime
+            ? convertToFuzzyTime(0, THIRTY_MINUTES_MS)
+            : 0;
+        await sleep(wait);
+        const newDate = new Date();
+        try {
+            await writeFile(this.fileLocation, newDate.toISOString());
+        } catch (error) {
+            this.logger.error(error);
+        }
+        return newDate.getTime();
+    };
+
+    public getOrCreateTimestamp = async (): Promise<number> => {
+        try {
+            const file = (
+                await readFile(this.fileLocation, 'utf-8')
+            ).toString();
+            return Date.parse(file);
+        } catch (error) {
+            return await this.writeNewTimestamp();
+        }
+    };
+
+    @Cron('0 * * * *')
+    async handleCron() {
+        try {
+            const currentDate = new Date().getTime();
+            const prevDate = await this.getOrCreateTimestamp();
+            if (currentDate - prevDate > ONE_DAY_MS * 7) {
+                // Write new timestamp
+                await this.writeNewTimestamp();
+            }
+        } catch (error) {
+            // File does not exist, write it
+            await this.writeNewTimestamp();
+            this.logger.error(error);
+        }
+    }
+}
