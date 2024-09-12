fff0861f7540a84aa1e2c973246cf351d12b0626
Pujit Mehrotra <pujitm2009@gmail.com>
2024-09-12 17:18:31 -0400

    feat: wrap Notifications in a GraphQL Node & implement notification overviews

--- api/src/unraid-api/graph/resolvers/notifications/notifications.resolver.ts
+++ api/src/unraid-api/graph/resolvers/notifications/notifications.resolver.ts
@@ -1,10 +1,11 @@
 import {type NotificationFilter} from '@app/graphql/generated/api/types';
-import {Args, Query, Resolver, Subscription} from '@nestjs/graphql';
+import {Args, Query, ResolveField, Resolver, Subscription} from '@nestjs/graphql';
 import {UseRoles} from 'nest-access-control';
 import {createSubscription, PUBSUB_CHANNEL} from '@app/core/pubsub';
 import {NotificationsService} from './notifications.service';
+import { getServerIdentifier } from '@app/core/utils/server-identifier';
 
-@Resolver()
+@Resolver('Notifications')
 export class NotificationsResolver {
     constructor(readonly notificationsService: NotificationsService) {}
 
@@ -14,20 +15,43 @@ export class NotificationsResolver {
         action: 'read',
         possession: 'any',
     })
-    public async notifications(
+    public async notifications() {
+        return {
+            id: getServerIdentifier('notifications'),
+        }
+    }
+
+    @ResolveField()
+    public async overview() {
+        return await this.notificationsService.getOverview();
+    }
+
+
+    @ResolveField()
+    public async data(
         @Args('filter')
         filters: NotificationFilter
     ) {
         return await this.notificationsService.getNotifications(filters);
     }
 
-    @Subscription('notificationAdded')
+    @Subscription()
     @UseRoles({
         resource: 'notifications',
         action: 'read',
         possession: 'any',
     })
     async notificationAdded() {
-        return createSubscription(PUBSUB_CHANNEL.NOTIFICATION);
+        return createSubscription(PUBSUB_CHANNEL.NOTIFICATION_ADDED);
+    }
+
+    @Subscription()
+    @UseRoles({
+        resource: 'notifications',
+        action: 'read',
+        possession: 'any',
+    })
+    async notificationsOverview() {
+        return createSubscription(PUBSUB_CHANNEL.NOTIFICATION_OVERVIEW);
     }
 }
