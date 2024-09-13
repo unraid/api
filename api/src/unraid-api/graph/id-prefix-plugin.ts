1ed4bc0dce8f1a96f4f79d3970df7beb0c309d04
Eli Bosley <ekbosley@gmail.com>
2024-09-13 09:34:23 -0400

    fix: convert updateId function to iterative instead of recursive

--- api/src/unraid-api/graph/id-prefix-plugin.ts
+++ api/src/unraid-api/graph/id-prefix-plugin.ts
@@ -4,11 +4,25 @@ import { getServerIdentifier } from "@app/core/utils/server-identifier";
 const serverId = getServerIdentifier();
 
 const updateId = (obj: any) => {
-    if (obj && typeof obj === 'object') {
-        if ('id' in obj && typeof obj.id === 'string') {
-            obj.id = `${serverId}-${obj.id}`;
+    const stack = [obj];
+    let iterations = 0;
+    // Prevent infinite loops
+    while (stack.length > 0 && iterations < 100) {
+        const current = stack.pop();
+
+        if (current && typeof current === 'object') {
+            if ('id' in current && typeof current.id === 'string') {
+                current.id = `${serverId}-${current.id}`;
+            }
+
+            for (const value of Object.values(current)) {
+                if (value && typeof value === 'object') {
+                    stack.push(value);
+                }
+            }
         }
-        Object.values(obj).forEach((value) => updateId(value));
+
+        iterations++;
     }
 };
 
