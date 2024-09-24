2f9e2eef2db61221be66683caa4e75368aa475e0
Zack Spear <hi@zackspear.com>
2024-09-24 13:54:36 -0700

    feat: wip Notification UI starter

---
+++ web/components/Notifications/OpenButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useNotificationsStore } from '~/store/notifications';
+
+const notificationsStore = useNotificationsStore();
+</script>
+
+<template>
+  <BrandButton text="My Button" @click="notificationsStore.toggle" />
+
+  <NotificationsSidebar />
+</template>
+
