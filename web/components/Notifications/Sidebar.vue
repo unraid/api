5a90b3285ad8524cea58cbaca8293675b3dc257b
Zack Spear <hi@zackspear.com>
2024-09-24 18:39:21 -0700

    feat: WIP notifications w/ shadcn
    Currently the build doesn't work in webgui

--- web/components/Notifications/Sidebar.vue
+++ web/components/Notifications/Sidebar.vue
@@ -1,14 +1,95 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
+import { BellIcon } from '@heroicons/vue/24/solid';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/shadcn/sheet';
+import type { NotificationItemProps } from '~/types/ui/notification';
 
-import { useNotificationsStore } from '~/store/notifications';
+const unreadNotifications: NotificationItemProps[] = [
+  {
+    id: '1',
+    subject: 'New User Registration',
+    message: 'A new user has registered on your platform.',
+    type: 'success',
+  },
+  {
+    id: '2',
+    subject: 'Drive Pre-Failure Detected',
+    message: 'Drive 1 has been detected as pre-failure.',
+    type: 'alert',
+  },
+  {
+    id: '3',
+    subject: 'Server Maintenance',
+    message: 'Your server will be undergoing maintenance at 12:00 AM.',
+    type: 'warning',
+  },
+];
 
-const notificationsStore = useNotificationsStore();
-const { isOpen } = storeToRefs(notificationsStore);
+const archiveNotifications: NotificationItemProps[] = [
+  {
+    id: '1',
+    subject: 'Archived New User Registration',
+    message: 'A new user has registered on your platform.',
+    type: 'success',
+  },
+  {
+    id: '2',
+    subject: 'Archived Drive Pre-Failure Detected',
+    message: 'Drive 1 has been detected as pre-failure.',
+    type: 'alert',
+  },
+  {
+    id: '3',
+    subject: 'Archived Server Maintenance',
+    message: 'Your server will be undergoing maintenance at 12:00 AM.',
+    type: 'warning',
+  },
+];
 </script>
 
 <template>
-  <div v-if="isOpen" class="h-full w-full max-w-36">
-    This is my sidebar
-  </div>
+  <Sheet>
+    <SheetTrigger>
+      <span class="sr-only">Notifications</span>
+      <BellIcon class="w-6 h-6" />
+    </SheetTrigger>
+
+    <SheetContent class="w-full max-w-[400px] sm:max-w-[540px]">
+      <SheetHeader>
+        <SheetTitle>Notifications</SheetTitle>
+
+        <Tabs default-value="unread">
+          <TabsList>
+            <TabsTrigger value="unread">
+              Unread
+            </TabsTrigger>
+            <TabsTrigger value="archived">
+              Archived
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="unread" class="divide-y divide-gray-200">
+            <NotificationsItem
+              v-for="notification in unreadNotifications"
+              :key="notification.id"
+              v-bind="notification"
+            />
+          </TabsContent>
+          <TabsContent value="archived" class="divide-y divide-gray-200">
+            <NotificationsItem
+              v-for="notification in archiveNotifications"
+              :key="notification.id"
+              v-bind="notification"
+            />
+          </TabsContent>
+        </Tabs>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
 </template>
