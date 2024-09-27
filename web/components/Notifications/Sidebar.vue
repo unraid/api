8a0a053a80b805fe792fd7d40dc7e4564e8dcfed
mdatelle <mike@datelle.net>
2024-09-27 15:39:23 -0400

    test: add notification component directory

---
+++ web/components/Notifications/Sidebar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const visible = ref<boolean>(false);
+</script>
+
+<template>
+  <div class="card flex justify-center">
+    <Drawer v-model:visible="visible" header="Drawers" position="right" class="!w-full md:!w-80 lg:!w-[30rem]">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Drawer>
+    <Button label="Open" class="p-button-rounded p-button-outlined" @click="visible = true" />
+  </div>
+</template>
