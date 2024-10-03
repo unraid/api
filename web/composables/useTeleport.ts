e933f14c24cd9420a9ecaf8167b146b7e0678355
Zack Spear <hi@zackspear.com>
2024-10-03 14:40:31 -0700

    feat: WIP create teleport composable

---
+++ web/composables/useTeleport.ts
@@ -0,0 +1,27 @@
+import { ref, onMounted } from "vue";
+
+const useTeleport = () => {
+  const teleportTarget = ref<string | HTMLElement | Element>("#modals");
+
+  const determineTeleportTarget = () => {
+    const myModalsComponent = document.querySelector("unraid-modals");
+    if (!myModalsComponent?.shadowRoot) return;
+
+    const potentialTarget = myModalsComponent.shadowRoot.querySelector("#modals");
+    if (!potentialTarget) return;
+
+    teleportTarget.value = potentialTarget;
+    console.log("[determineTeleportTarget] teleportTarget", teleportTarget.value);
+  };
+
+  onMounted(() => {
+    determineTeleportTarget();
+  });
+
+  return {
+    teleportTarget,
+    determineTeleportTarget,
+  };
+};
+
+export default useTeleport;
