ba77ff4a4c163c01de87b7058fd57ef25d8ce6d1
Eli Bosley <ekbosley@gmail.com>
2024-10-09 13:31:15 -0400

    feat: remove console log disabler

--- web/helpers/functions.ts
+++ web/helpers/functions.ts
@@ -1,10 +1,2 @@
 /** Output key + value as string for each item in the object. Adds new line after each item. */
 export const OBJ_TO_STR = (obj: object): string => Object.entries(obj).reduce((str, [p, val]) => `${str}${p}: ${val}\n`, '');
-/** Removes our dev logs from prod builds */
-export const disableProductionConsoleLogs = () => {
-  if (import.meta.env.PROD && !import.meta.env.VITE_ALLOW_CONSOLE_LOGS) {
-    console.log = () => {};
-    console.debug = () => {};
-    console.info = () => {};
-  }
-};
