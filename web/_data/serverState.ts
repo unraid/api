2d320338524515360a90f728017ba4e420f4bf16
Pujit Mehrotra <pujitm2009@gmail.com>
2024-10-17 10:53:22 -0400

    fix(web): replace incorrect custom types with codegen from gql & update values to match expected shapes

--- web/_data/serverState.ts
+++ web/_data/serverState.ts
@@ -136,6 +136,7 @@ export const serverState: Server = {
   apiKey: "unupc_fab6ff6ffe51040595c6d9ffb63a353ba16cc2ad7d93f813a2e80a5810",
   avatar: "https://source.unsplash.com/300x300/?portrait",
   config: {
+    id: 'config-id',
     error: null,
     valid: false,
   },
--- web/store/notifications.ts
+++ web/store/notifications.ts
@@ -1,17 +1,17 @@
 import { defineStore, createPinia, setActivePinia } from 'pinia';
-import type { NotificationItemProps } from "~/types/ui/notification";
+import type { NotificationFragmentFragment } from '~/composables/gql/graphql';
 
 setActivePinia(createPinia());
 
 export const useNotificationsStore = defineStore('notifications', () => {
-  const notifications = ref<NotificationItemProps[]>([]);
+  const notifications = ref<NotificationFragmentFragment[]>([]);
   const isOpen = ref<boolean>(false);
 
   const title = computed<string>(() => isOpen.value ? 'Notifications Are Open' : 'Notifications Are Closed');
 
   const toggle = () => isOpen.value = !isOpen.value;
 
-  const setNotifications = (newNotifications: NotificationItemProps[]) => {
+  const setNotifications = (newNotifications: NotificationFragmentFragment[]) => {
     notifications.value = newNotifications;
   };
 
