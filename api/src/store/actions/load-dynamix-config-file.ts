5f4cc07473e8ef6c5c9837af565d39c7cee3b8d9
Pujit Mehrotra <pujitm2009@gmail.com>
2024-10-18 10:00:44 -0400

    fix(api): load dynamix config in the same way as the webgui
    merge defaults + custom config
    
    see original php implementation in the webgui:
    [link](https://github.com/unraid/webgui/blob/95c6913c62e64314b985e08222feb3543113b2ec/emhttp/plugins/dynamix/include/Wrappers.php#L42)

--- api/src/store/actions/load-dynamix-config-file.ts
+++ api/src/store/actions/load-dynamix-config-file.ts
@@ -1,11 +1,30 @@
 import { parseConfig } from '@app/core/utils/misc/parse-config';
-import {
-    createAsyncThunk,
-} from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { access } from 'fs/promises';
 import { F_OK } from 'constants';
 import { type RecursivePartial, type RecursiveNullable } from '@app/types';
 import { type DynamixConfig } from '@app/core/types/ini';
+import { batchProcess } from '@app/utils';
+
+/**
+ * Loads a configuration file from disk, parses it to a RecursivePartial of the provided type, and returns it.
+ *
+ * If the file is inaccessible, an empty object is returned instead.
+ *
+ * @param path The path to the configuration file on disk.
+ * @returns A parsed RecursivePartial of the provided type.
+ */
+async function loadConfigFile<ConfigType>(path: string): Promise<RecursivePartial<ConfigType>> {
+    const fileIsAccessible = await access(path, F_OK)
+        .then(() => true)
+        .catch(() => false);
+    return fileIsAccessible
+        ? parseConfig<RecursivePartial<ConfigType>>({
+              filePath: path,
+              type: 'ini',
+          })
+        : {};
+}
 
 /**
  * Load the dynamix.cfg into the store.
@@ -16,18 +35,12 @@ export const loadDynamixConfigFile = createAsyncThunk<
     RecursiveNullable<RecursivePartial<DynamixConfig>>,
     string | undefined
 >('config/load-dynamix-config-file', async (filePath) => {
+    if (filePath) {
+        return loadConfigFile<DynamixConfig>(filePath);
+    }
     const store = await import('@app/store');
-    const paths = store.getters.paths();
-    const path = filePath ?? paths['dynamix-config'];
-    const fileExists = await access(path, F_OK)
-        .then(() => true)
-        .catch(() => false);
-    const file: RecursivePartial<DynamixConfig> = fileExists
-        ? parseConfig<RecursivePartial<DynamixConfig>>({
-              filePath: path,
-              type: 'ini',
-          })
-        : {};
-
-    return file;
+    const paths = store.getters.paths()['dynamix-config'];
+    const { data: configs } = await batchProcess(paths, (path) => loadConfigFile<DynamixConfig>(path));
+    const [defaultConfig = {}, customConfig = {}] = configs;
+    return { ...defaultConfig, ...customConfig };
 });
