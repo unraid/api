b20f69c2089b8a71b41cb256badbe0bcc3913cea
Eli Bosley <ekbosley@gmail.com>
2024-10-23 18:50:34 -0400

    feat: pm2 fully working

--- api/src/cli/index.ts
+++ api/src/cli/index.ts
@@ -4,6 +4,7 @@ import { type Flags, mainOptions, options, args } from '@app/cli/options';
 import { setEnv } from '@app/cli/set-env';
 import { env } from '@app/dotenv';
 import { getters } from '@app/store';
+import { execSync } from 'child_process';
 
 const command = mainOptions.command as unknown as string;
 
@@ -46,11 +47,13 @@ export const main = async (...argv: string[]) => {
         start: import('@app/cli/commands/start').then((pkg) => pkg.start),
         stop: import('@app/cli/commands/stop').then((pkg) => pkg.stop),
         restart: import('@app/cli/commands/restart').then((pkg) => pkg.restart),
+        logs: async () => execSync('pm2 logs unraid-api --lines 200', { stdio: 'inherit' }),
         'switch-env': import('@app/cli/commands/switch-env').then((pkg) => pkg.switchEnv),
         version: import('@app/cli/commands/version').then((pkg) => pkg.version),
         status: import('@app/cli/commands/status').then((pkg) => pkg.status),
         report: import('@app/cli/commands/report').then((pkg) => pkg.report),
         'validate-token': import('@app/cli/commands/validate-token').then((pkg) => pkg.validateToken),
+        
     };
 
     // Unknown command
