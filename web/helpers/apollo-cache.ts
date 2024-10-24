a6326341ecaa42448ab8a611c04bf349757eb328
Pujit Mehrotra <pujitm2009@gmail.com>
2024-10-24 16:41:36 -0400

    feat(web): reconcile pagination with notifications apollo cache

--- web/helpers/apollo-cache.ts
+++ web/helpers/apollo-cache.ts
@@ -2,6 +2,23 @@ import { InMemoryCache, type InMemoryCacheConfig } from "@apollo/client/core";
 
 const defaultCacheConfig: InMemoryCacheConfig = {
   typePolicies: {
+    Notifications: {
+      fields: {
+        list: {
+          keyArgs: ["filter", ["type", "importance"]],
+          merge(existing = [], incoming, { args }) {
+            // copied from https://www.apollographql.com/docs/react/pagination/core-api#improving-the-merge-function
+            const offset = args?.filter?.offset ?? 0;
+            const merged = existing.slice(0);
+
+            for (let i = 0; i < incoming.length; ++i) {
+              merged[offset + i] = incoming[i];
+            }
+            return merged;
+          },
+        },
+      },
+    },
     Mutation: {
       fields: {
         archiveAll: {
