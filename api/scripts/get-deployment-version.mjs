8fb8cb304ee1b0f007b81a679dc3eadf098f6b4b
Eli Bosley <ekbosley@gmail.com>
2024-10-24 12:07:12 -0400

    feat: simplify getting version

--- api/scripts/get-deployment-version.mjs
+++ api/scripts/get-deployment-version.mjs
@@ -3,20 +3,19 @@ import { execSync } from 'child_process';
 const runCommand = (command) => {
     try {
         return execSync(command, { stdio: 'pipe' }).toString().trim();
-    } catch(error) {
+    } catch (error) {
         console.log('Failed to get value from tag command: ', command, error.message);
         return;
     }
 };
 
-const getTags = (env = process.env) => {
-
-    if (env.GIT_SHA) {
-        console.log(`Using env vars for git tags: ${env.GIT_SHA} ${env.IS_TAGGED}`)
-        return {
-            shortSha: env.GIT_SHA,
-            isTagged: Boolean(env.IS_TAGGED)
-        }
+export const getDeploymentVersion = (env = process.env, packageVersion) => {
+    if (env.API_VERSION) {
+        console.log(`Using env var for version: ${env.API_VERSION}`);
+        return env.API_VERSION;
+    } else if (env.GIT_SHA && env.IS_TAGGED) {
+        console.log(`Using env vars for git tags: ${env.GIT_SHA} ${env.IS_TAGGED}`);
+        return env.IS_TAGGED ? packageVersion : `${packageVersion}+${env.GIT_SHA}`;
     } else {
         const gitShortSHA = runCommand('git rev-parse --short HEAD');
         const isCommitTagged = runCommand('git describe --tags --abbrev=0 --exact-match') !== undefined;
@@ -25,11 +24,6 @@ const getTags = (env = process.env) => {
             console.error('Failed to get git short SHA');
             process.exit(1);
         }
-        return {
-            shortSha: gitShortSHA,
-            isTagged: isCommitTagged
-        }
+        return isCommitTagged ? packageVersion : `${packageVersion}+${gitShaShort}`;
     }
-}
-
-export default getTags;
+};
