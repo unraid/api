fed165eab0358fe032a99e5afbdb19813b00b741
Eli Bosley <ekbosley@gmail.com>
2024-10-25 23:44:55 -0400

    fix: unit test failure

--- api/src/unraid-api/auth/auth.service.spec.ts
+++ api/src/unraid-api/auth/auth.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, type TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '@app/unraid-api/auth/users.service';
+import { CookieService } from '@app/unraid-api/auth/cookie.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+        providers: [
+            AuthService,
+            { provide: 'USERS_SERVICE', useClass: UsersService },
+            { provide: 'COOKIE_SERVICE', useClass: CookieService },
+            { provide: 'SESSION_COOKIE_CONFIG', useValue: { name: 'session' } },
+        ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
