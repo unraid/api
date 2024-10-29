6aa63a0dc71fc799b41809b757e238127c014c7c
Pujit Mehrotra <pujitm2009@gmail.com>
2024-10-29 11:31:29 -0400

    refactor(web): extract notification object cache prefix to a constant

---
+++ web/helpers/apollo-cache/prefixes.ts
@@ -0,0 +1,5 @@
+/**
+ * The magic 'Notification' prefix comes from inspecting the apollo cachel.
+ * I think it comes from the __typename when apollo caches an object (by default)
+ */
+export const NOTIFICATION_CACHE_PREFIX = "Notification";
