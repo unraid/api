a32374a3ac62d4d12c18ca7c37836e5322784dcb
Eli Bosley <ekbosley@gmail.com>
2024-11-05 09:27:06 -0500

    feat: attempt to fix pm2

--- api/src/dotenv.ts
+++ api/src/dotenv.ts
@@ -1,8 +1,10 @@
+import { logger } from '@app/core/log';
 import { config } from 'dotenv';
 
 export const env =
     process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-        ? config({ debug: true, path: `./.env.${process.env.NODE_ENV}` })
+        ? config({ debug: true, path: `./.env.${process.env.NODE_ENV}`, encoding: 'utf-8' })
         : config({
               path: '/usr/local/unraid-api/.env',
+              encoding: 'utf-8',
           });
