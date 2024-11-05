62693cfcc068837e4288d3b888732b79d9e3ac10
Eli Bosley <ekbosley@gmail.com>
2024-11-05 12:48:44 -0500

    feat: cleanup unused variables

--- api/src/unraid-api/app/app.module.ts
+++ api/src/unraid-api/app/app.module.ts
@@ -1,10 +1,8 @@
 import { apiLogger } from '@app/core/log';
-import { setupPermissions } from '@app/core/permissions';
 import { AuthModule } from '@app/unraid-api/auth/auth.module';
 import { GraphModule } from '@app/unraid-api/graph/graph.module';
 import { RestModule } from '@app/unraid-api/rest/rest.module';
 import { Module } from '@nestjs/common';
-import { ACGuard, AccessControlModule } from 'nest-access-control';
 import { LoggerModule } from 'nestjs-pino';
 import { CronModule } from '@app/unraid-api/cron/cron.module';
 
