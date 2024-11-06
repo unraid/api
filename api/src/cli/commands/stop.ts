925866d389e337fcb8c249ead929e1f65854465b
Eli Bosley <ekbosley@gmail.com>
2024-11-06 15:21:08 -0500

    fix: forced restarting on commands

--- api/src/cli/commands/stop.ts
+++ api/src/cli/commands/stop.ts
@@ -1,9 +1,6 @@
 import { PM2_PATH } from '@app/consts';
 import { execSync } from 'child_process';
 
-export const stop = async (exit = false) => {
+export const stop = async () => {
     execSync(`${PM2_PATH} stop unraid-api`, { stdio: 'inherit' });
-    if (exit) {
-        process.exit(0);
-    }
 };
