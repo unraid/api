2c65e4e1050332eb106ed218aeba27d51a0c314a
mdatelle <mike@datelle.net>
2024-11-08 16:07:51 -0500

    fix: get auth module back to iriginal state before conflict

--- api/src/unraid-api/auth/auth.module.ts
+++ api/src/unraid-api/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { AuthZModule, AUTHZ_ENFORCER } from 'nest-authz';
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { newEnforcer, Model as CasbinModel, StringAdapter } from 'casbin';
 
@@ -10,9 +9,6 @@ import { CASBIN_MODEL, BASE_POLICY } from './casbin';
 import { CookieService, SESSION_COOKIE_CONFIG } from './cookie.service';
 import { ServerHeaderStrategy } from './header.strategy';
 import { UserCookieStrategy } from './cookie.strategy';
-import { GraphqlAuthGuard } from '@app/unraid-api/auth/auth.guard';
-import { AccessControlModule, ACGuard } from 'nest-access-control';
-import { setupPermissions } from '@app/core/permissions';
 
 @Module({
     imports: [
@@ -65,15 +61,6 @@ import { setupPermissions } from '@app/core/permissions';
         ServerHeaderStrategy,
         UserCookieStrategy,
         CookieService,
-        { provide: SESSION_COOKIE_CONFIG, useValue: CookieService.defaultOpts() },
-        { provide: 'AUTH_SERVICE', useClass: AuthService },
-        { provide: 'COOKIE_SERVICE', useClass: CookieService },
-        { provide: 'APP_GUARD', useClass: GraphqlAuthGuard },
-        {
-            provide: 'APP_GUARD',
-            useClass: ACGuard,
-        },
     ],
-    exports: [PassportModule],
 })
 export class AuthModule {}
