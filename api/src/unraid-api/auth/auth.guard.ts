ab4f249e9bbe58c14f0ec5819c789606aeba06a5
Eli Bosley <ekbosley@gmail.com>
2024-11-12 11:31:52 -0500

    fix: remove unused reflector decorator

--- api/src/unraid-api/auth/auth.guard.ts
+++ api/src/unraid-api/auth/auth.guard.ts
@@ -50,15 +50,6 @@ export class GraphqlAuthGuard
      * @returns
      */
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-            context.getHandler(),
-            context.getClass(),
-        ]);
-
-        if (isPublic) {
-            return true;
-        }
-
         return super.canActivate(context);
     }
 
