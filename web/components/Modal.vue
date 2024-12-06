13c1e2734199619f845264e87b2b3455195dd0c8
Eli Bosley <ekbosley@gmail.com>
2024-12-06 14:27:26 -0500

    fix: more color work

--- web/components/Modal.vue
+++ web/components/Modal.vue
@@ -94,11 +94,11 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
               success ? 'shadow-green-600/30 border-green-600/10' : '',
               !error && !success ? 'shadow-orange/10 border-white/10' : '',
             ]"
-            class="text-16px text-beta bg-alpha text-left relative z-10 flex flex-col justify-around border-2 border-solid shadow-xl transform overflow-hidden rounded-lg transition-all sm:w-full"
+            class="text-16px text-foreground bg-muted dark:bg-background text-left relative z-10 flex flex-col justify-around border-2 border-solid shadow-xl transform overflow-hidden rounded-lg transition-all sm:w-full"
           >
             <div v-if="showCloseX" class="absolute z-20 right-0 top-0 pt-4px pr-4px hidden sm:block">
               <button
-                class="rounded-md text-beta bg-transparent p-2 hover:text-white focus:text-white hover:bg-unraid-red focus:bg-unraid-red focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                class="rounded-md text-foreground bg-transparent p-2 hover:text-white focus:text-white hover:bg-unraid-red focus:bg-unraid-red focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 type="button"
                 @click="closeModal"
               >
@@ -115,7 +115,7 @@ const ariaLablledById = computed((): string|undefined => props.title ? `ModalTit
                 'justify-center': !$slots['header'],
               }"
             >
-              <div class="absolute -z-10 inset-0 opacity-10 bg-beta" />
+              <div class="absolute -z-10 inset-0 opacity-10 bg-card" />
               <template v-if="!$slots['header']">
                 <h1 v-if="title" :id="ariaLablledById" class="text-center text-20px sm:text-24px font-semibold flex flex-wrap justify-center gap-x-4px">
                   {{ title }}
