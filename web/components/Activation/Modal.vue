125445d2782f36bf3e6bd787207faadc1ba16e99
Zack Spear <hi@zackspear.com>
2024-12-09 15:13:46 -0800

    feat: enhance Activation Modal component with dynamic title and description, and add documentation buttons

--- web/components/Activation/Modal.vue
+++ web/components/Activation/Modal.vue
@@ -5,6 +5,7 @@ import type { ComposerTranslation } from 'vue-i18n';
 
 import { useActivationCodeStore } from '~/store/activationCode';
 import { usePurchaseStore } from '~/store/purchase';
+import type { ButtonProps } from '~/types/ui/button';
 
 export interface Props {
   t: ComposerTranslation;
@@ -14,6 +15,29 @@ defineProps<Props>();
 
 const { partnerName, showModal } = storeToRefs(useActivationCodeStore());
 const purchaseStore = usePurchaseStore();
+
+const title = computed<string>(() => partnerName.value ? `Welcome to your new ${partnerName.value} system, powered by Unraid!` : 'Welcome to Unraid!');
+const description = computed<string>(() => `To get started, let's activate your license and create an unraid.net account to provide access to account features like key management and support.`);
+const docsButtons = computed<ButtonProps[]>(() => {
+  return [
+    {
+      btnStyle: 'underline',
+      external: true,
+      href: 'https://docs.unraid.net/unraid-os/faq/licensing-faq/',
+      iconRight: ArrowTopRightOnSquareIcon,
+      size: '14px',
+      text: 'More about Licensing',
+    },
+    {
+      btnStyle: 'underline',
+      external: true,
+      href: 'https://docs.unraid.net/account/',
+      iconRight: ArrowTopRightOnSquareIcon,
+      size: '14px',
+      text: 'More about Unraid.net Accounts',
+    },
+  ];
+});
 </script>
 
 <template>
@@ -22,22 +46,20 @@ const purchaseStore = usePurchaseStore();
     :t="t"
     :open="showModal"
     :show-close-x="false"
-    :title="partnerName ? `Activate your ${partnerName} powered by Unraid license` : 'Activate your Unraid license'"
+    :title="title"
     :title-in-main="true"
-    :description="'Blurb about how Unraid licensing works and that they will be prompted to create an Unraid.net account to activate their license.'"
-    max-width="max-w-800px"
+    :description="description"
+    max-width="max-w-640px"
   >
     <template #header>
       <img src="https://placehold.co/300x100/004225/white?text=Gridstack+powered+by+Unraid" class="max-w-[175px]">
     </template>
     <template #main>
-      <div class="flex justify-center mx-auto w-full">
+      <div class="flex justify-center gap-4 mx-auto w-full">
         <BrandButton
-          :text="'More about Licensing'"
-          :icon-right="ArrowTopRightOnSquareIcon"
-          btn-style="underline"
-          size="14px"
-          href="https://docs.unraid.net"
+          v-for="button in docsButtons"
+          :key="button.text"
+          v-bind="button"
         />
       </div>
     </template>
