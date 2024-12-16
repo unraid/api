d427054443176563faa3e44249219c1d938e4b07
Eli Bosley <ekbosley@gmail.com>
2024-12-16 14:28:23 -0500

    feat: responsive notifications

--- web/components/Notifications/Sidebar.vue
+++ web/components/Notifications/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import { Button } from '@/components/shadcn/button';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/shadcn/sheet';
 import { useMutation, useQuery } from '@vue/apollo-composable';
-import { Button } from '@/components/shadcn/button';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports -- false positive :(
 import { Importance, NotificationType } from '~/composables/gql/graphql';
 import {
@@ -47,86 +47,80 @@ const overview = computed(() => {
       <span class="sr-only">Notifications</span>
       <NotificationsIndicator :overview="overview" />
     </SheetTrigger>
-
-    <!-- We remove the horizontal padding from the container to keep the NotificationList's scrollbar in the right place -->
     <SheetContent
       :to="teleportTarget"
-      class="w-full max-w-[100vw] sm:max-w-[540px] h-screen px-0"
+      class="w-full max-w-[100vw] sm:max-w-[540px] max-h-screen h-screen min-h-screen px-0 flex flex-col gap-5 pb-0"
     >
-      <div class="flex flex-col h-full gap-5">
-        <SheetHeader class="ml-1 px-6 flex items-baseline gap-1">
-          <SheetTitle class="text-2xl">Notifications</SheetTitle>
-          <a href="/Settings/Notifications">
-            <Button variant="link" size="sm" class="p-0 h-auto"> Edit Settings </Button>
-          </a>
-        </SheetHeader>
+    <div class="relative flex flex-col h-full w-full">
+      <SheetHeader class="ml-1 px-6 items-baseline gap-1">
+        <SheetTitle class="text-2xl">Notifications</SheetTitle>
+        <a href="/Settings/Notifications">
+        <Button variant="link" size="sm" class="p-0 h-auto"> Edit Settings </Button>
+        </a>
+      </SheetHeader>
+      <Tabs default-value="unread" class="flex flex-1 flex-col min-h-0">
+      <div class="flex flex-row justify-between items-center flex-wrap gap-5 px-6">
+        <TabsList class="flex">
+        <TabsTrigger value="unread">
+          Unread <span v-if="overview">({{ overview.unread.total }})</span>
+        </TabsTrigger>
+        <TabsTrigger value="archived">
+          Archived <span v-if="overview">({{ overview.archive.total }})</span>
+        </TabsTrigger>
+        </TabsList>
+        <TabsContent value="unread" class="flex-col items-end">
+        <Button
+          :disabled="loadingArchiveAll"
+          variant="link"
+          size="sm"
+          class="text-foreground hover:text-destructive transition-none"
+          @click="confirmAndArchiveAll"
+        >
+          Archive All
+        </Button>
+        </TabsContent>
+        <TabsContent value="archived" class="flex-col items-end">
+        <Button
+          :disabled="loadingDeleteAll"
+          variant="link"
+          size="sm"
+          class="text-foreground hover:text-destructive transition-none"
+          @click="confirmAndDeleteArchives"
+        >
+          Delete All
+        </Button>
+        </TabsContent>
 
-        <!-- min-h-0 prevents the flex container from expanding beyond its containing bounds. -->
-        <!-- this is necessary because flex items have a default min-height: auto, -->
-        <!-- which means they won't shrink below the height of their content, even if you use flex-1 or other flex properties. -->
-        <Tabs default-value="unread" class="flex-1 flex flex-col min-h-0" activation-mode="manual">
-          <div class="flex flex-row justify-between items-center flex-wrap gap-5 px-6">
-            <TabsList class="ml-[1px]">
-              <TabsTrigger value="unread">
-                Unread <span v-if="overview">({{ overview.unread.total }})</span>
-              </TabsTrigger>
-              <TabsTrigger value="archived">
-                Archived <span v-if="overview">({{ overview.archive.total }})</span>
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="unread">
-              <Button
-                :disabled="loadingArchiveAll"
-                variant="link"
-                size="sm"
-                class="text-foreground hover:text-destructive transition-none"
-                @click="confirmAndArchiveAll"
-              >
-                Archive All
-              </Button>
-            </TabsContent>
-            <TabsContent value="archived">
-              <Button
-                :disabled="loadingDeleteAll"
-                variant="link"
-                size="sm"
-                class="text-foreground hover:text-destructive transition-none"
-                @click="confirmAndDeleteArchives"
-              >
-                Delete All
-              </Button>
-            </TabsContent>
+        <Select
+        @update:model-value="
+          (val) => {
+          importance = val === 'all' ? undefined : (val as Importance);
+          }
+        "
+        >
+        <SelectTrigger class="h-auto">
+          <SelectValue class="text-gray-400 leading-6" placeholder="Filter By" />
+        </SelectTrigger>
+        <SelectContent :to="teleportTarget">
+          <SelectGroup>
+          <SelectLabel>Notification Types</SelectLabel>
+          <SelectItem value="all">All Types</SelectItem>
+          <SelectItem :value="Importance.Alert"> Alert </SelectItem>
+          <SelectItem :value="Importance.Info">Info</SelectItem>
+          <SelectItem :value="Importance.Warning">Warning</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+        </Select>
+      </div>
 
-            <Select
-              @update:model-value="
-                (val) => {
-                  importance = val === 'all' ? undefined : (val as Importance);
-                }
-              "
-            >
-              <SelectTrigger class="h-auto">
-                <SelectValue class="text-gray-400 leading-6" placeholder="Filter By" />
-              </SelectTrigger>
-              <SelectContent :to="teleportTarget">
-                <SelectGroup>
-                  <SelectLabel>Notification Types</SelectLabel>
-                  <SelectItem value="all">All Types</SelectItem>
-                  <SelectItem :value="Importance.Alert"> Alert </SelectItem>
-                  <SelectItem :value="Importance.Info">Info</SelectItem>
-                  <SelectItem :value="Importance.Warning">Warning</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
+      <TabsContent value="unread" class="flex-col flex-1 min-h-0">
+        <NotificationsList :importance="importance" :type="NotificationType.Unread" />
+      </TabsContent>
 
-          <TabsContent value="unread" class="flex-1 min-h-0 mt-5">
-            <NotificationsList :importance="importance" :type="NotificationType.Unread" />
-          </TabsContent>
-
-          <TabsContent value="archived" class="flex-1 min-h-0 mt-5">
-            <NotificationsList :importance="importance" :type="NotificationType.Archive" />
-          </TabsContent>
-        </Tabs>
+      <TabsContent value="archived" class="flex-col flex-1 min-h-0">
+        <NotificationsList :importance="importance" :type="NotificationType.Archive" />
+      </TabsContent>
+      </Tabs>
       </div>
     </SheetContent>
   </Sheet>
