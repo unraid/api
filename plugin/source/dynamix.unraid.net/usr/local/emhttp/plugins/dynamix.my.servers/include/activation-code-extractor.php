47d25a31956837ae87a8b00b8751b8f870220a3f
Zack Spear <hi@zackspear.com>
2024-12-18 17:53:54 -0800

    feat: enhance ActivationCodeExtractor with partner logo handling and metadata extraction
    - Added constants for document root and web GUI images directory.
    - Introduced properties for partner name, URL, logo path, and logo file type.
    - Implemented methods to retrieve partner logo path, render logo string, partner name, and URL.
    - Enhanced data extraction logic to include partner-related information from JSON data.
    - Added debug method for outputting internal state for troubleshooting.

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/activation-code-extractor.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/activation-code-extractor.php
@@ -14,7 +14,31 @@ class ActivationCodeExtractor {
     public const DIR = '/boot/config/activation';
     public const FILE_PATTERN = '/activation_code_([A-Za-z0-9]+)\.activationcode/';
 
+    public const DOCROOT = '/usr/local/emhttp';
+    public const WEBGUI_IMAGES_BASE_DIR = '/webGui/images';
+    public const PARTNER_LOGO_FILE_NAME = 'partner-logo';
+    public const DEFAULT_LOGO = self::DOCROOT . self::WEBGUI_IMAGES_BASE_DIR . '/UN-logotype-gradient.svg';
+
+    /** @var array{
+     *     code: string,
+     *     partnerName: string,
+     *     partnerUrl?: string,
+     *     sysModel?: string,
+     *     comment?: string,
+     *     caseIcon: string,
+     *     header?: string,
+     *     headermetacolor?: string,
+     *     background?: string,
+     *     showBannerGradient?: string,
+     *     partnerLogoFileType?: "png"|"svg",
+     *     theme?: "azure" | "black" | "gray" | "white
+     * }
+     */
     private array $data = [];
+    private string $partnerName = '';
+    private string $partnerUrl = 'https://unraid.net';
+    private string $partnerLogoPath = '';
+    private string $partnerLogoFileType = '';
 
     /**
      * Constructor to automatically fetch JSON data from all matching files.
@@ -59,7 +83,37 @@ class ActivationCodeExtractor {
             }
         }
 
+        if (isset($data['partnerName'])) {
+            $this->partnerName = $data['partnerName'];
+        }
+
+        if (isset($data['partnerUrl'])) {
+            $this->partnerUrl = $data['partnerUrl'];
+        }
+
+        /**
+         * During the plg install, the partner logo asset is copied to the webgui images dir.
+         * We need to set the partner logo path to the copied asset.
+         */
+        if (isset($data['partnerLogoFileType'])) {
+            $this->partnerLogoFileType = $data['partnerLogoFileType'];
+
+            $logo = self::DOCROOT . self::WEBGUI_IMAGES_BASE_DIR . '/' . self::PARTNER_LOGO_FILE_NAME . '.' . $this->partnerLogoFileType;
+            if (file_exists($logo)) {
+                $this->partnerLogoPath = $logo;
+            }
+        }
+
         return $data;
+    }
+
+    /**
+     * Get the partner logo path.
+     * 
+     * @return string
+     */
+    public function getPartnerLogoPath(): string {
+        return $this->partnerLogoPath;
     }
 
     /**
@@ -79,5 +133,61 @@ class ActivationCodeExtractor {
     public function getDataForHtmlAttr(): string {
         $json = json_encode($this->getData());
         return htmlspecialchars($json, ENT_QUOTES, 'UTF-8');
+    }
+
+    /**
+     * Get the partner logo render string.
+     * 
+     * @return string
+     */
+    public function getPartnerLogoRenderString(): string {
+        if (empty($this->partnerLogoPath)) { // default logo
+            return file_get_contents(self::DEFAULT_LOGO);
+        }
+
+        switch ($this->partnerLogoFileType) {
+            case 'svg':
+                return file_get_contents($this->partnerLogoPath);
+            case 'png':
+                // Making it relative for the front-end
+                $logo = str_replace(self::DOCROOT, '', $this->partnerLogoPath);
+                return "<img src='$logo' alt='$this->partnerName' />";
+            default:
+            return file_get_contents(self::DEFAULT_LOGO);
+        }
+    }
+
+    /**
+     * Get the partner name.
+     * 
+     * @return string
+     */
+    public function getPartnerName(): string {
+        return $this->partnerName;
+    }
+
+    /**
+     * Get the partner URL.
+     * 
+     * @return string
+     */
+    public function getPartnerUrl(): string {
+        return $this->partnerUrl;
+    }
+
+    /**
+     * Output all variables to for debugging
+     * 
+     * @see https://tower.local/plugins/dynamix.my.servers/data/activation-data.php
+     * @return void
+     */
+    public function debug(): void {
+        echo "data: "; var_dump($this->data);
+        echo "partnerName: "; var_dump($this->partnerName);
+        echo "partnerUrl: "; var_dump($this->partnerUrl);
+        echo "partnerLogoFileType: "; var_dump($this->partnerLogoFileType);
+        echo "partnerLogoPath: "; var_dump($this->partnerLogoPath);
+
+        echo $this->getPartnerLogoRenderString();
     }
 }
