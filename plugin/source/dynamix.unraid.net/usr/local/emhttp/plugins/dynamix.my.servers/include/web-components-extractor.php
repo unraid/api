ef63c30c9810e0b94d1ea4c201065f8e3ff4c2f0
Zack Spear <hi@zackspear.com>
2024-12-19 15:29:39 -0800

    refactor: streamline WebComponentsExtractor class and update method names
    - Removed outdated copyright comments from web-components-extractor.php for improved readability.
    - Renamed method getJsFileUrl() to getJSFileRelativePath() to enhance clarity and consistency in naming conventions.
    - Adjusted indentation for better code formatting and readability.

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/web-components-extractor.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/web-components-extractor.php
@@ -1,13 +1,4 @@
 <?php
-/* Copyright 2005-2023, Lime Technology
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- */
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
 class WebComponentsExtractor {
@@ -18,28 +9,28 @@ class WebComponentsExtractor {
     private string $jsFileName = '';
 
     public function __construct() {
-      $localManifest = json_decode(file_get_contents(self::MANIFEST_FILE), true);
+        $localManifest = json_decode(file_get_contents(self::MANIFEST_FILE), true);
 
-      foreach ($localManifest as $key => $value) {
-          if (strpos($key, self::SEARCH_TEXT) !== false && isset($value["file"])) {
-              $this->jsFileName = $value["file"];
-              break;
-          }
-      }
+        foreach ($localManifest as $key => $value) {
+            if (strpos($key, self::SEARCH_TEXT) !== false && isset($value["file"])) {
+                $this->jsFileName = $value["file"];
+                break;
+            }
+        }
     }
 
     public function getJsFileName(): string {
         return $this->jsFileName;
     }
 
-    public function getJsFileUrl(): string {
+    public function getJSFileRelativePath(): string {
         return self::PREFIXED_PATH . $this->jsFileName;
     }
 
     public function getScriptTagHtml(): string {
         if (empty($this->jsFileName)) {
-          return '<script>console.error("%cNo matching key containing \'' . self::SEARCH_TEXT . '\' found.", "font-weight: bold; color: white; background-color: red");</script>';
+            return '<script>console.error("%cNo matching key containing \'' . self::SEARCH_TEXT . '\' found.", "font-weight: bold; color: white; background-color: red");</script>';
         }
-        return '<script src="' . $this->getJsFileUrl() . '"></script>';
+        return '<script src="' . $this->getJSFileRelativePath() . '"></script>';
     }
 }
