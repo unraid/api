dab33dcc1cbc9f3481677047081bb4caf7ed9c7a
Zack Spear <hi@zackspear.com>
2024-12-19 15:34:07 -0800

    refactor: simplify partner logo handling in activation code script
    - Removed unnecessary variable initialization and included only essential requires for improved clarity.
    - Streamlined the code in partner-logo.php to focus on the core functionality of displaying the partner logo.
    - Enhanced maintainability by reducing complexity in the script.

--- plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/partner-logo.php
+++ plugin/source/dynamix.unraid.net/usr/local/emhttp/plugins/dynamix.my.servers/include/partner-logo.php
@@ -3,11 +3,7 @@
  * Display the partner logo that was installed with the activation code.
  */
 $docroot ??= ($_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp');
-$var = (array)parse_ini_file('state/var.ini');
 
-require_once "$docroot/webGui/include/Wrappers.php";
-require_once "$docroot/webGui/include/Helpers.php";
-extract(parse_plugin_cfg('dynamix',true));
 require_once "$docroot/plugins/dynamix.my.servers/include/activation-code-extractor.php";
 
 $activationCodeExtractor = new ActivationCodeExtractor();
