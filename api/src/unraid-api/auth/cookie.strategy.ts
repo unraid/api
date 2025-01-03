e688ba55a972c564a663ff18339b8544af44264a
Pujit Mehrotra <pujit@lime-technology.com>
2025-01-03 12:14:54 -0500

    tmp: log cookies in production

--- api/src/unraid-api/auth/cookie.strategy.ts
+++ api/src/unraid-api/auth/cookie.strategy.ts
@@ -18,6 +18,8 @@ export class UserCookieStrategy extends PassportStrategy(Strategy, strategyName)
     }
 
     public validate = async (req: CustomRequest): Promise<any> => {
+        this.logger.log(req.headers.cookie, 'headers.cookie')
+        this.logger.log(req.headers['set-cookie'], 'headers.set-cookie')
         return this.authService.validateCookiesCasbin(req.cookies);
     };
 }
