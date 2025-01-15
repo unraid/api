b73e76cb53c2f147e435bae1eb83fa681ac8240f
Pujit Mehrotra <pujit@lime-technology.com>
2025-01-15 16:22:13 -0500

    feat(api): use custom apollo plugin to render sandbox

--- api/src/unraid-api/graph/graph.module.ts
+++ api/src/unraid-api/graph/graph.module.ts
@@ -3,7 +3,6 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { NoUnusedVariablesRule, print } from 'graphql';
 import {
     DateTimeResolver,
@@ -16,36 +15,15 @@ import {
 import { GRAPHQL_INTROSPECTION } from '@app/environment';
 import { GraphQLLong } from '@app/graphql/resolvers/graphql-type-long';
 import { typeDefs } from '@app/graphql/schema/index';
-import { getters } from '@app/store';
 import { idPrefixPlugin } from '@app/unraid-api/graph/id-prefix-plugin';
 
 import { ConnectResolver } from './connect/connect.resolver';
 import { ConnectService } from './connect/connect.service';
 import { NetworkResolver } from './network/network.resolver';
 import { ResolversModule } from './resolvers/resolvers.module';
+import { sandboxPlugin } from './sandbox-plugin';
 import { ServicesResolver } from './services/services.resolver';
 import { SharesResolver } from './shares/shares.resolver';
-
-/** The initial query displayed in the Apollo sandbox */
-const initialDocument = `query ExampleQuery {
-  notifications {
-    id
-    overview {
-      unread {
-        info
-        warning
-        alert
-        total
-      }
-      archive {
-        info
-        warning
-        alert
-        total
-      }
-    }
-  }
-}`;
 
 @Module({
     imports: [
@@ -59,23 +37,7 @@ const initialDocument = `query ExampleQuery {
                 extra,
             }),
             playground: false,
-            plugins: GRAPHQL_INTROSPECTION
-                ? [
-                      ApolloServerPluginLandingPageLocalDefault({
-                          footer: false,
-                          includeCookies: true,
-                          document: initialDocument,
-                          embed: {
-                              initialState: {
-                                  sharedHeaders: {
-                                      'x-csrf-token': getters.emhttp().var.csrfToken ?? 'no csrf token',
-                                  },
-                              },
-                          },
-                      }),
-                      idPrefixPlugin,
-                  ]
-                : [idPrefixPlugin],
+            plugins: GRAPHQL_INTROSPECTION ? [sandboxPlugin, idPrefixPlugin] : [idPrefixPlugin],
             subscriptions: {
                 'graphql-ws': {
                     path: '/graphql',
