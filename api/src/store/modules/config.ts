943e73fa696b6ecec3227be914ab4962c4fee79d
Eli Bosley <ekbosley@gmail.com>
2025-01-19 15:10:45 -0500

    feat: cleanup config entries

--- api/src/store/modules/config.ts
+++ api/src/store/modules/config.ts
@@ -1,32 +1,29 @@
-import { parseConfig } from '@app/core/utils/misc/parse-config';
-import {
-    type MyServersConfig,
-    type MyServersConfigMemory,
-} from '@app/types/my-servers-config';
-import {
-    createAsyncThunk,
-    createSlice,
-    type PayloadAction,
-} from '@reduxjs/toolkit';
-import { access } from 'fs/promises';
-import merge from 'lodash/merge';
-import { FileLoadStatus } from '@app/store/types';
 import { F_OK } from 'constants';
-import { type RecursivePartial } from '@app/types';
-import { DynamicRemoteAccessType, MinigraphStatus, type Owner } from '@app/graphql/generated/api/types';
-import { type RootState } from '@app/store';
 import { randomBytes } from 'crypto';
-import { logger } from '@app/core/log';
-import { setGraphqlConnectionStatus } from '@app/store/actions/set-minigraph-status';
-import { getWriteableConfig } from '@app/core/utils/files/config-file-normalizer';
 import { writeFileSync } from 'fs';
-import { safelySerializeObjectToIni } from '@app/core/utils/files/safe-ini-serializer';
-import { PUBSUB_CHANNEL, pubsub } from '@app/core/pubsub';
+import { access } from 'fs/promises';
+
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { isEqual } from 'lodash-es';
-import { setupRemoteAccessThunk } from '@app/store/actions/setup-remote-access';
+import merge from 'lodash/merge';
+
+import type { Owner } from '@app/graphql/generated/api/types';
+import { logger } from '@app/core/log';
+import { pubsub, PUBSUB_CHANNEL } from '@app/core/pubsub';
+import { getWriteableConfig } from '@app/core/utils/files/config-file-normalizer';
+import { safelySerializeObjectToIni } from '@app/core/utils/files/safe-ini-serializer';
+import { parseConfig } from '@app/core/utils/misc/parse-config';
 import { NODE_ENV } from '@app/environment';
+import { DynamicRemoteAccessType, MinigraphStatus } from '@app/graphql/generated/api/types';
 import { GraphQLClient } from '@app/mothership/graphql-client';
 import { stopPingTimeoutJobs } from '@app/mothership/jobs/ping-timeout-jobs';
+import { type RootState } from '@app/store';
+import { setGraphqlConnectionStatus } from '@app/store/actions/set-minigraph-status';
+import { setupRemoteAccessThunk } from '@app/store/actions/setup-remote-access';
+import { FileLoadStatus } from '@app/store/types';
+import { type RecursivePartial } from '@app/types';
+import { type MyServersConfig, type MyServersConfigMemory } from '@app/types/my-servers-config';
 
 export type SliceState = {
     status: FileLoadStatus;
@@ -51,7 +48,7 @@ export const initialState: SliceState = {
         refreshtoken: '',
         allowedOrigins: '',
         dynamicRemoteAccessType: DynamicRemoteAccessType.DISABLED,
-        ssoSubIds: ''
+        ssoSubIds: '',
     },
     local: {},
     api: {
@@ -71,8 +68,8 @@ export const initialState: SliceState = {
 } as const;
 
 export const loginUser = createAsyncThunk<
-    Pick<MyServersConfig['remote'], 'email' | 'avatar' | 'username' | 'apikey'| 'localApiKey'>,
-    Pick<MyServersConfig['remote'], 'email' | 'avatar' | 'username' | 'apikey'| 'localApiKey'>,
+    Pick<MyServersConfig['remote'], 'email' | 'avatar' | 'username' | 'apikey' | 'localApiKey'>,
+    Pick<MyServersConfig['remote'], 'email' | 'avatar' | 'username' | 'apikey' | 'localApiKey'>,
     { state: RootState }
 >('config/login-user', async (userInfo) => {
     logger.info('Logging in user: %s', userInfo.username);
@@ -84,29 +81,28 @@ export const loginUser = createAsyncThunk<
     return userInfo;
 });
 
-export const logoutUser = createAsyncThunk<
-    void,
-    { reason?: string },
-    { state: RootState }
->('config/logout-user', async ({ reason }) => {
-    logger.info('Logging out user: %s', reason ?? 'No reason provided');
-    const { pubsub } = await import('@app/core/pubsub');
+export const logoutUser = createAsyncThunk<void, { reason?: string }, { state: RootState }>(
+    'config/logout-user',
+    async ({ reason }) => {
+        logger.info('Logging out user: %s', reason ?? 'No reason provided');
+        const { pubsub } = await import('@app/core/pubsub');
 
-    // Publish to servers endpoint
-    await pubsub.publish(PUBSUB_CHANNEL.SERVERS, {
-        servers: [],
-    });
+        // Publish to servers endpoint
+        await pubsub.publish(PUBSUB_CHANNEL.SERVERS, {
+            servers: [],
+        });
 
-    const owner: Owner = {
-        username: 'root',
-        url: '',
-        avatar: '',
-    };
-    // Publish to owner endpoint
-    await pubsub.publish(PUBSUB_CHANNEL.OWNER, { owner });
-    stopPingTimeoutJobs();
-    await GraphQLClient.clearInstance();
-});
+        const owner: Owner = {
+            username: 'root',
+            url: '',
+            avatar: '',
+        };
+        // Publish to owner endpoint
+        await pubsub.publish(PUBSUB_CHANNEL.OWNER, { owner });
+        stopPingTimeoutJobs();
+        await GraphQLClient.clearInstance();
+    }
+);
 
 /**
  * Load the myservers.cfg into the store. Returns null if the state after loading doesn't change
@@ -128,32 +124,6 @@ type LoadFailureConfigEqual = {
 };
 type ConfigRejectedValues = LoadFailureConfigEqual | LoadFailureWithConfig;
 
-const generateApiKeysIfNotExistent = (
-    file: RecursivePartial<MyServersConfig>
-): MyServersConfig => {
-    const newConfigFile = merge(file, {
-        upc: {
-            apikey:
-                file.upc?.apikey?.trim()?.length === 64
-                    ? file.upc?.apikey
-                    : `unupc_${randomBytes(58).toString('hex')}`.substring(
-                          0,
-                          64
-                      ),
-        },
-        notifier: {
-            apikey:
-                file.notifier?.apikey?.trim().length === 64
-                    ? file.notifier?.apikey
-                    : `unnotify_${randomBytes(58).toString('hex')}`.substring(
-                          0,
-                          64
-                      ),
-        },
-    }) as MyServersConfig;
-    return newConfigFile;
-};
-
 export const loadConfigFile = createAsyncThunk<
     MyServersConfig,
     string | undefined,
@@ -161,71 +131,51 @@ export const loadConfigFile = createAsyncThunk<
         state: RootState;
         rejectValue: ConfigRejectedValues;
     }
->(
-    'config/load-config-file',
-    async (filePath, { getState, rejectWithValue }) => {
-        try {
-            const { paths, config } = getState();
+>('config/load-config-file', async (filePath, { getState, rejectWithValue }) => {
+    try {
+        const { paths, config } = getState();
 
-            const path = filePath ?? paths['myservers-config'];
+        const path = filePath ?? paths['myservers-config'];
 
-            const fileExists = await access(path, F_OK)
-                .then(() => true)
-                .catch(() => false);
-            if (!fileExists) {
-                throw new Error('Config File Missing');
-            }
+        const fileExists = await access(path, F_OK)
+            .then(() => true)
+            .catch(() => false);
+        if (!fileExists) {
+            throw new Error('Config File Missing');
+        }
 
-            const file = fileExists
-                ? parseConfig<RecursivePartial<MyServersConfig>>({
-                      filePath: path,
-                      type: 'ini',
-                  })
-                : {};
+        const newConfigFile = getWriteableConfig(
+            parseConfig<MyServersConfig>({ filePath: path, type: 'ini' }),
+            'flash'
+        );
 
-            const newConfigFile = generateApiKeysIfNotExistent(file);
-
-            const isNewlyLoadedConfigEqual = isEqual(
-                getWriteableConfig(newConfigFile as SliceState, 'flash'),
-                getWriteableConfig(config, 'flash')
-            );
-            if (isNewlyLoadedConfigEqual) {
-                logger.warn(
-                    'Not loading config because it is the same as before'
-                );
-                return rejectWithValue({
-                    type: CONFIG_LOAD_ERROR.CONFIG_EQUAL,
-                });
-            }
-            return newConfigFile;
-        } catch (error: unknown) {
-            logger.warn('Config file is corrupted with error: %o - recreating config', error);
-            const config = getWriteableConfig(initialState, 'flash');
-            const newConfig = generateApiKeysIfNotExistent(config);
-            newConfig.remote.wanaccess = 'no';
-            const serializedConfig = safelySerializeObjectToIni(newConfig);
-            writeFileSync(
-                getState().paths['myservers-config'],
-                serializedConfig
-            );
+        const isNewlyLoadedConfigEqual = isEqual(newConfigFile, getWriteableConfig(config, 'flash'));
+        if (isNewlyLoadedConfigEqual) {
+            logger.warn('Not loading config because it is the same as before');
             return rejectWithValue({
-                type: CONFIG_LOAD_ERROR.CONFIG_CORRUPTED,
-                error:
-                    error instanceof Error ? error : new Error('Unknown Error'),
-                config: newConfig,
+                type: CONFIG_LOAD_ERROR.CONFIG_EQUAL,
             });
         }
+        return newConfigFile;
+    } catch (error: unknown) {
+        logger.warn('Config file is corrupted with error: %o - recreating config', error);
+        const newConfig = getWriteableConfig(initialState, 'flash');
+        newConfig.remote.wanaccess = 'no';
+        const serializedConfig = safelySerializeObjectToIni(newConfig);
+        writeFileSync(getState().paths['myservers-config'], serializedConfig);
+        return rejectWithValue({
+            type: CONFIG_LOAD_ERROR.CONFIG_CORRUPTED,
+            error: error instanceof Error ? error : new Error('Unknown Error'),
+            config: newConfig,
+        });
     }
-);
+});
 
 export const config = createSlice({
     name: 'config',
     initialState,
     reducers: {
-        updateUserConfig(
-            state,
-            action: PayloadAction<RecursivePartial<MyServersConfig>>
-        ) {
+        updateUserConfig(state, action: PayloadAction<RecursivePartial<MyServersConfig>>) {
             return merge(state, action.payload);
         },
         updateAccessTokens(
@@ -287,10 +237,7 @@ export const config = createSlice({
                     state.status = FileLoadStatus.LOADED;
                     break;
                 case CONFIG_LOAD_ERROR.CONFIG_CORRUPTED:
-                    logger.debug(
-                        'Config File Load Failed - %o',
-                        action.payload.error
-                    );
+                    logger.debug('Config File Load Failed - %o', action.payload.error);
                     merge(state, action.payload.config);
                     state.status = FileLoadStatus.LOADED;
                     break;
@@ -333,8 +280,7 @@ export const config = createSlice({
 
         builder.addCase(setupRemoteAccessThunk.fulfilled, (state, action) => {
             state.remote.wanaccess = action.payload.wanaccess;
-            state.remote.dynamicRemoteAccessType =
-                action.payload.dynamicRemoteAccessType;
+            state.remote.dynamicRemoteAccessType = action.payload.dynamicRemoteAccessType;
             state.remote.wanport = action.payload.wanport;
             state.remote.upnpEnabled = action.payload.upnpEnabled;
         });
