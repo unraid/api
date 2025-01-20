29ca5829ffdc63df2a21c71d380c14647bcb1e03
Eli Bosley <ekbosley@gmail.com>
2025-01-19 19:07:21 -0500

    feat: only write config when a specific config update action occurs

--- api/src/store/listeners/config-listener.ts
+++ api/src/store/listeners/config-listener.ts
@@ -1,66 +1,22 @@
-import { startAppListening } from '@app/store/listeners/listener-middleware';
-import { isEqual } from 'lodash-es';
-import { logger } from '@app/core/log';
-import {
-    type ConfigType,
-    getWriteableConfig,
-} from '@app/core/utils/files/config-file-normalizer';
-import {
-    loadConfigFile,
-    loginUser,
-    logoutUser,
-} from '@app/store/modules/config';
-import { FileLoadStatus } from '@app/store/types';
-import { safelySerializeObjectToIni } from '@app/core/utils/files/safe-ini-serializer';
-import { isFulfilled } from '@reduxjs/toolkit';
-import { environment } from '@app/environment';
 import { writeFileSync } from 'fs';
 
-const actionIsLoginOrLogout = isFulfilled(logoutUser, loginUser);
+
+import type { ConfigType } from '@app/core/utils/files/config-file-normalizer';
+import { logger } from '@app/core/log';
+import { getWriteableConfig } from '@app/core/utils/files/config-file-normalizer';
+import { safelySerializeObjectToIni } from '@app/core/utils/files/safe-ini-serializer';
+import { startAppListening } from '@app/store/listeners/listener-middleware';
+import { configUpdateActionsFlash, configUpdateActionsMemory } from '@app/store/modules/config';
 
 export const enableConfigFileListener = (mode: ConfigType) => () =>
     startAppListening({
-        predicate(action, currentState, previousState) {
-            if (!environment.IS_MAIN_PROCESS) {
-                return false;
-            }
-
-            if (currentState.config.status === FileLoadStatus.LOADED) {
-                const oldFlashConfig = previousState?.config.api.version
-                    ? getWriteableConfig(previousState.config, mode)
-                    : null;
-                const newFlashConfig = getWriteableConfig(
-                    currentState.config,
-                    mode
-                );
-
-                if (
-                    !isEqual(oldFlashConfig, newFlashConfig) &&
-                    action.type !== loadConfigFile.fulfilled.type &&
-                    action.type !== loadConfigFile.rejected.type
-                ) {
-                    return true;
-                }
-
-                if (actionIsLoginOrLogout(action) && mode === 'memory') {
-                    logger.trace(
-                        'Logout / Login Action Encountered, writing memory config'
-                    );
-                    return true;
-                }
-            }
-
-            return false;
-        },
+        matcher: mode === 'flash' ? configUpdateActionsFlash : configUpdateActionsMemory,
         async effect(_, { getState }) {
             const { paths, config } = getState();
             const pathToWrite =
-                mode === 'flash'
-                    ? paths['myservers-config']
-                    : paths['myservers-config-states'];
+                mode === 'flash' ? paths['myservers-config'] : paths['myservers-config-states'];
             const writeableConfig = getWriteableConfig(config, mode);
-            const serializedConfig =
-                safelySerializeObjectToIni(writeableConfig);
+            const serializedConfig = safelySerializeObjectToIni(writeableConfig);
             logger.debug('Writing updated config to %s', pathToWrite);
             writeFileSync(pathToWrite, serializedConfig);
         },
