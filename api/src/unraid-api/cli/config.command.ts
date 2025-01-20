d2d0f7cd9acb53ea2372245d7ef669c7ca24ee8a
Eli Bosley <ekbosley@gmail.com>
2025-01-19 23:14:03 -0500

    feat: remove unused fields

---
+++ api/src/unraid-api/cli/config.command.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+
+import { Command, CommandRunner } from 'nest-commander';
+
+import { getters } from '@app/store/index';
+import { LogService } from '@app/unraid-api/cli/log.service';
+
+@Injectable()
+@Command({
+    name: 'config',
+    description: 'Display current configuration values',
+})
+export class ConfigCommand extends CommandRunner {
+    constructor(private readonly logger: LogService) {
+        super();
+    }
+
+    async run(): Promise<void> {
+        this.logger.log('\nDisk Configuration:');
+        const diskConfig = await readFile(getters.paths()['myservers-config'], 'utf8');
+        this.logger.log(diskConfig);
+        process.exit(0);
+    }
+}
