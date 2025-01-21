9d59044fedabd2346a704751d24b3319964a70f5
Eli Bosley <ekbosley@gmail.com>
2025-01-21 15:02:43 -0500

    feat: add user with cli

--- api/src/unraid-api/cli/cli.module.ts
+++ api/src/unraid-api/cli/cli.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
 
+import { InquirerService } from 'nest-commander';
+
 import { ConfigCommand } from '@app/unraid-api/cli/config.command';
 import { KeyCommand } from '@app/unraid-api/cli/key.command';
 import { LogService } from '@app/unraid-api/cli/log.service';
 import { LogsCommand } from '@app/unraid-api/cli/logs.command';
 import { ReportCommand } from '@app/unraid-api/cli/report.command';
 import { RestartCommand } from '@app/unraid-api/cli/restart.command';
-import { SSOCommand } from '@app/unraid-api/cli/sso.command';
+import { AddSSOUserCommand } from '@app/unraid-api/cli/sso/add-sso-user.command';
+import { AddSSOUserQuestionSet } from '@app/unraid-api/cli/sso/add-sso-user.questions';
+import { SSOCommand } from '@app/unraid-api/cli/sso/sso.command';
+import { ValidateTokenCommand } from '@app/unraid-api/cli/sso/validate-token.command';
 import { StartCommand } from '@app/unraid-api/cli/start.command';
 import { StatusCommand } from '@app/unraid-api/cli/status.command';
 import { StopCommand } from '@app/unraid-api/cli/stop.command';
 import { SwitchEnvCommand } from '@app/unraid-api/cli/switch-env.command';
 import { VersionCommand } from '@app/unraid-api/cli/version.command';
-import { ValidateTokenCommand } from '@app/unraid-api/cli/validate-token.command';
 
 @Module({
     providers: [
+        AddSSOUserCommand,
+        AddSSOUserQuestionSet,
         LogService,
         StartCommand,
         StopCommand,
