69bf12f47cab39f83899f1593facd366f6264919
Eli Bosley <ekbosley@gmail.com>
2025-01-23 13:00:32 -0500

    feat: remove sso user command

--- api/src/unraid-api/cli/sso/sso.command.ts
+++ api/src/unraid-api/cli/sso/sso.command.ts
@@ -5,12 +5,13 @@ import { Command, CommandRunner } from 'nest-commander';
 import { LogService } from '@app/unraid-api/cli/log.service';
 import { ValidateTokenCommand } from '@app/unraid-api/cli/sso/validate-token.command';
 import { AddSSOUserCommand } from '@app/unraid-api/cli/sso/add-sso-user.command';
+import { RemoveSSOUserCommand } from '@app/unraid-api/cli/sso/remove-sso-user.command';
 
 @Injectable()
 @Command({
     name: 'sso',
     description: 'Main Command to Configure / Validate SSO Tokens',
-    subCommands: [ValidateTokenCommand, AddSSOUserCommand],
+    subCommands: [ValidateTokenCommand, AddSSOUserCommand, RemoveSSOUserCommand],
 })
 export class SSOCommand extends CommandRunner {
     constructor(private readonly logger: LogService) {
