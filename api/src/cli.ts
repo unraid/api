ed34c99daf3b21d6d252f95698a1533e6e0a8e93
Eli Bosley <ekbosley@gmail.com>
2025-01-27 11:58:24 -0500

    fix: shell path to unraid-api

--- api/src/cli.ts
+++ api/src/cli.ts
@@ -9,7 +9,7 @@ import { cliLogger, internalLogger } from '@app/core/log';
 import { CliModule } from '@app/unraid-api/cli/cli.module';
 
 try {
-    const shellToUse = execSync('which bash').toString().trim();
+    const shellToUse = execSync('which unraid-api').toString().trim();
     await CommandFactory.run(CliModule, {
         cliName: 'unraid-api',
         logger: false, // new LogService(), - enable this to see nest initialization issues
