429b4075450d2f601b18eec6fcc1d1d759e1de09
Eli Bosley <ekbosley@gmail.com>
2025-01-27 11:25:13 -0500

    fix: pull token from query not params

--- api/src/unraid-api/auth/cookie.strategy.ts
+++ api/src/unraid-api/auth/cookie.strategy.ts
@@ -20,7 +20,7 @@ export class UserCookieStrategy extends PassportStrategy(Strategy, strategyName)
     public validate = async (req: CustomRequest): Promise<any> => {
         return (
             this.authService.validateCsrfToken(
-                req.headers['x-csrf-token'] || (req.params as { csrf_token?: string })?.csrf_token
+                req.headers['x-csrf-token'] || (req.query as { csrf_token?: string })?.csrf_token
             ) && this.authService.validateCookiesCasbin(req.cookies)
         );
     };
