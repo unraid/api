c66079e9a8e0ef47e5054118d0581bec708ac604
Eli Bosley <ekbosley@gmail.com>
2025-01-28 12:59:35 -0500

    fix: code review feedback

--- api/src/core/utils/vms/parse-domain.ts
+++ api/src/core/utils/vms/parse-domain.ts
@@ -10,50 +10,51 @@ export type DomainLookupType = 'id' | 'uuid' | 'name';
  * @private
  */
 export const parseDomain = async (type: DomainLookupType, id: string): Promise<Domain> => {
-	const types = {
-		id: 'lookupDomainByIdAsync',
-		uuid: 'lookupDomainByUUIDAsync',
-		name: 'lookupDomainByNameAsync',
-	};
+    const types = {
+        id: 'lookupDomainByIdAsync',
+        uuid: 'lookupDomainByUUIDAsync',
+        name: 'lookupDomainByNameAsync',
+    };
 
-	if (!type || !Object.keys(types).includes(type)) {
-		throw new Error(`Type must be one of [${Object.keys(types).join(', ')}], ${type} given.`);
-	}
+    if (!type || !Object.keys(types).includes(type)) {
+        throw new Error(`Type must be one of [${Object.keys(types).join(', ')}], ${type} given.`);
+    }
 
-	const { UnraidHypervisor } = await import('@app/core/utils/vms/get-hypervisor');
-	const client = await UnraidHypervisor.getInstance().getHypervisor();
-	const method = types[type];
-	const domain = await client[method](id);
-	const info = await domain.getInfoAsync();
+    const { UnraidHypervisor } = await import('@app/core/utils/vms/get-hypervisor');
+    const client = await UnraidHypervisor.getInstance().getHypervisor();
+    const method = types[type];
+    const domain = await client[method](id);
+    const info = await domain.getInfoAsync();
 
-	const [uuid, osType, autostart, maxMemory, schedulerType, schedulerParameters, securityLabel, name] = await Promise.all([
-		domain.getUUIDAsync(),
-		domain.getOSTypeAsync(),
-		domain.getAutostartAsync(),
-		domain.getMaxMemoryAsync(),
-		domain.getSchedulerTypeAsync(),
-		domain.getSchedulerParametersAsync(),
-		domain.getSecurityLabelAsync(),
-		domain.getNameAsync(),
-	]);
+    const [uuid, osType, autostart, maxMemory, schedulerType, schedulerParameters, securityLabel, name] =
+        await Promise.all([
+            domain.getUUIDAsync(),
+            domain.getOSTypeAsync(),
+            domain.getAutostartAsync(),
+            domain.getMaxMemoryAsync(),
+            domain.getSchedulerTypeAsync(),
+            domain.getSchedulerParametersAsync(),
+            domain.getSecurityLabelAsync(),
+            domain.getNameAsync(),
+        ]);
 
-	const results = {
-		uuid,
-		osType,
-		autostart,
-		maxMemory,
-		schedulerType,
-		schedulerParameters,
-		securityLabel,
-		name,
-		...info,
-		state: info.state.replace(' ', '_'),
-	};
+    const results = {
+        uuid,
+        osType,
+        autostart,
+        maxMemory,
+        schedulerType,
+        schedulerParameters,
+        securityLabel,
+        name,
+        ...info,
+        state: info.state.replace(' ', '_'),
+    };
 
-	if (info.state === 'running') {
-		results.vcpus = await domain.getVcpusAsync();
-		results.memoryStats = await domain.getMemoryStatsAsync();
-	}
+    if (info.state === 'running') {
+        results.vcpus = await domain.getVcpusAsync();
+        results.memoryStats = await domain.getMemoryStatsAsync();
+    }
 
-	return results;
+    return results;
 };
