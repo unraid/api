3452874d13941f291dc08427779cfd9fbc40a612
Pujit Mehrotra <pujit@lime-technology.com>
2025-01-29 11:24:09 -0500

    refactor: debug based on env in registration.ts

--- unraid-ui/src/register.ts
+++ unraid-ui/src/register.ts
@@ -3,7 +3,7 @@ import './styles/index.css';
 import * as Components from '@/components';
 import kebabCase from 'kebab-case';
 
-const debugImports = true;
+const debugImports = process.env.NODE_ENV !== 'production';
 
 type RegisterParams = {
   namePrefix?: string;
@@ -12,18 +12,20 @@ type RegisterParams = {
 
 export function registerAllComponents(params: RegisterParams = {}) {
   const { namePrefix = 'unraid', pathToSharedCss = './src/styles/index.css' } = params;
-  // registerCommonComponents();
   Object.entries(Components).forEach(([name, component]) => {
     // add our shared css to each web component
     component.styles ??= [];
     component.styles.unshift(`@import "${pathToSharedCss}"`);
 
+    // translate ui component names from PascalCase to kebab-case
     let elementName = kebabCase(name);
     if (!elementName) {
       console.log('[register components] Could not translate component name to kebab-case:', name);
       elementName = name;
     }
     elementName = namePrefix + elementName;
+
+    // register custom web components
     if (debugImports) {
       console.log(name, elementName, component.styles);
     }
