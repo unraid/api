46cba36993d6869784c7dbb3858d3dea02f51683
Eli Bosley <ekbosley@gmail.com>
2025-01-29 11:39:50 -0500

    feat: add logrotate cron again

--- api/src/unraid-api/cron/cron.module.ts
+++ api/src/unraid-api/cron/cron.module.ts
@@ -1,9 +1,11 @@
-import { WriteFlashFileService } from '@app/unraid-api/cron/write-flash-file.service';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 
+import { WriteFlashFileService } from '@app/unraid-api/cron/write-flash-file.service';
+import { LogRotateService } from '@app/unraid-api/cron/log-rotate.service';
+
 @Module({
     imports: [ScheduleModule.forRoot()],
-    providers: [WriteFlashFileService],
+    providers: [WriteFlashFileService, LogRotateService],
 })
 export class CronModule {}
