c7c3f028ad71d624227fc9cb24d9a7014eb5975f
Eli Bosley <ekbosley@gmail.com>
2025-02-05 15:44:03 -0500

    fix: lint

--- api/src/__test__/graphql/resolvers/query/cloud/check-mothership-authentication.test.ts
+++ api/src/__test__/graphql/resolvers/query/cloud/check-mothership-authentication.test.ts
@@ -5,24 +5,22 @@ import { expect, test } from 'vitest';
 import packageJson from '@app/../package.json';
 import { checkMothershipAuthentication } from '@app/graphql/resolvers/query/cloud/check-mothership-authentication';
 
-test(
-    'It fails to authenticate with mothership with no credentials',
-    async () => {
-        try {
-            await expect(
-                checkMothershipAuthentication('BAD', 'BAD')
-            ).rejects.toThrowErrorMatchingInlineSnapshot(
-                `[Error: Failed to connect to https://mothership.unraid.net/ws with a "426" HTTP error.]`
-            );
-            expect(packageJson.version).not.toBeNull();
-            await expect(
-                checkMothershipAuthentication(packageJson.version, 'BAD_API_KEY')
-            ).rejects.toThrowErrorMatchingInlineSnapshot(`[Error: Invalid credentials]`);
-        } catch (error) {
-            if (error instanceof Error && error.message.includes('Timeout')) {
-                // Test succeeds on timeout
-                return;
-            }
-            throw error;
+test('It fails to authenticate with mothership with no credentials', async () => {
+    try {
+        await expect(
+            checkMothershipAuthentication('BAD', 'BAD')
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+            `[Error: Failed to connect to https://mothership.unraid.net/ws with a "426" HTTP error.]`
+        );
+        expect(packageJson.version).not.toBeNull();
+        await expect(
+            checkMothershipAuthentication(packageJson.version, 'BAD_API_KEY')
+        ).rejects.toThrowErrorMatchingInlineSnapshot(`[Error: Invalid credentials]`);
+    } catch (error) {
+        if (error instanceof Error && error.message.includes('Timeout')) {
+            // Test succeeds on timeout
+            return;
+        }
+        throw error;
     }
 });
