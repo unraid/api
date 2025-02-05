a6717c41f405ccbde09620746a2713e880843227
Eli Bosley <ekbosley@gmail.com>
2025-02-05 14:04:10 -0500

    fix: force kill the API

--- api/src/unraid-api/cli/stop.command.ts
+++ api/src/unraid-api/cli/stop.command.ts
@@ -10,10 +10,16 @@ export class StopCommand extends CommandRunner {
     constructor(private readonly pm2: PM2Service) {
         super();
     }
+
     async run() {
-        const { stderr } = await this.pm2.run({ tag: 'PM2 Stop' }, 'stop', ECOSYSTEM_PATH);
-        if (stderr) {
-            process.exit(1);
-        }
+        // Stop and remove the PM2 process
+        await this.pm2.run({ tag: 'PM2 Stop', stdio: 'inherit' }, 'stop', ECOSYSTEM_PATH);
+        await this.pm2.run({ tag: 'PM2 Delete', stdio: 'inherit' }, 'delete', ECOSYSTEM_PATH);
+
+        // Clean up PM2 state
+        await this.pm2.deleteDump(); // Delete the PM2 dump file
+
+        // Kill the PM2 daemon
+        await this.pm2.run({ tag: 'PM2 Kill', stdio: 'inherit' }, 'kill');
     }
 }
