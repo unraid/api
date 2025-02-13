fcc26ab8485ce281aee824175d99408d9d499f10
Eli Bosley <ekbosley@gmail.com>
2025-02-13 16:55:30 -0500

    feat: temp minification disable

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -74,7 +74,7 @@ export default defineNuxtConfig({
       __VUE_PROD_DEVTOOLS__: false,
     },
     build: {
-      minify: 'terser',
+      minify: false,
       terserOptions: {
         mangle: {
           reserved: terserReservations(charsToReserve),
