222814805c51e1bdd4702670a6ea9ea8f48000c2
Eli Bosley <ekbosley@gmail.com>
2025-02-12 19:52:36 -0500

    fix: monorepo install issue

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,19 +1,4 @@
-import { readFileSync } from 'fs';
-
-import { parse } from 'dotenv';
 import removeConsole from 'vite-plugin-remove-console';
-
-const envConfig = parse(readFileSync('.env'));
-console.log('\n');
-console.log('==============================');
-console.log('========= ENV VALUES =========');
-console.log('==============================');
-for (const k in envConfig) {
-  process.env[k] = envConfig[k];
-  console.log(`[${k}]`, process.env[k]);
-}
-console.log('==============================');
-console.log('\n');
 
 /**
  * Used to avoid redeclaring variables in the webgui codebase.
