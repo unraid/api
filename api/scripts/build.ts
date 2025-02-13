0e3823e6b9fe0420fbf3d546e19201b630f666a1
Eli Bosley <ekbosley@gmail.com>
2025-02-12 23:13:09 -0500

    feat: frozen lockfile

--- api/scripts/build.ts
+++ api/scripts/build.ts
@@ -40,7 +40,7 @@ try {
     
     console.log('Installing production dependencies...');
     $.verbose = true;
-    await $`pnpm install --prod`;
+    await $`pnpm install --prod --prune --frozen-lockfile`;
 
     // chmod the cli
     await $`chmod +x ./dist/cli.js`;
