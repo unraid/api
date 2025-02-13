dc665271223edf3f1e48e560c2d7fa2c33fe79f0
Eli Bosley <ekbosley@gmail.com>
2025-02-12 23:55:03 -0500

    fix: ignore workspace root check

--- api/scripts/build.ts
+++ api/scripts/build.ts
@@ -40,7 +40,7 @@ try {
     
     console.log('Installing production dependencies...');
     $.verbose = true;
-    await $`pnpm install --prod --frozen-lockfile`;
+    await $`pnpm install --prod --frozen-lockfile --ignore-workspace-root-check`;
 
     // chmod the cli
     await $`chmod +x ./dist/cli.js`;
