5a3e8df003a5005acc39442160e263f86793ce88
Eli Bosley <ekbosley@gmail.com>
2025-02-20 10:13:21 -0500

    fix: storybook resolution issue (#1153)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Chores**
    - Enhanced the Storybook configuration by integrating improvements for
    build efficiency.
    - Updated build settings to optimize dependency handling during
    Storybook execution.
    - Updated several Storybook-related package versions and added new
    dependencies.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- unraid-ui/.storybook/main.ts
+++ unraid-ui/.storybook/main.ts
@@ -1,5 +1,6 @@
-import type { StorybookConfig } from "@storybook/vue3-vite";
 import { dirname, join } from "path";
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -14,26 +15,30 @@ const config: StorybookConfig = {
       docgen: "vue-component-meta",
     },
   },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
   docs: {
     autodocs: "tag",
   },
   async viteFinal(config) {
+    config.root = dirname(require.resolve('@storybook/builder-vite'));
     return {
       ...config,
       resolve: {
         alias: {
-          "@": join(dirname(new URL(import.meta.url).pathname), "../src"),
-          "@/components": join(dirname(new URL(import.meta.url).pathname), "../src/components"),
-          "@/lib": join(dirname(new URL(import.meta.url).pathname), "../src/lib"),
+          '@': join(dirname(new URL(import.meta.url).pathname), '../src'),
+          '@/components': join(dirname(new URL(import.meta.url).pathname), '../src/components'),
+          '@/lib': join(dirname(new URL(import.meta.url).pathname), '../src/lib'),
         },
       },
       css: {
         postcss: {
           plugins: [
-            (await import("tailwindcss")).default({
-              config: "./tailwind.config.ts",
+            (await import('tailwindcss')).default({
+              config: './tailwind.config.ts',
             }),
-            (await import("autoprefixer")).default,
+            (await import('autoprefixer')).default,
           ],
         },
       },
