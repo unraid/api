893b28c7dcda4decc94d3f0311e461b42fdc9875
mdatelle <mike@datelle.net>
2025-02-21 13:50:36 -0500

    fix: add width full to unstyled transition wrapper div

--- web/components/Modal.vue
+++ web/components/Modal.vue
@@ -107,6 +107,7 @@ const computedVerticalCenter = computed<string>(() => {
           leave="duration-200 ease-in"
           leave-from="opacity-100 scale-100"
           leave-to="opacity-0 scale-95"
+          class="w-full"
         >
           <div
             :class="[
@@ -116,7 +117,7 @@ const computedVerticalCenter = computed<string>(() => {
               success ? 'shadow-green-600/30 border-green-600/10' : '',
               !error && !success && !disableShadow ? 'shadow-orange/10 border-white/10' : '',
             ]"
-            class="text-16px text-foreground bg-background text-left relative z-10 mx-auto flex flex-col justify-around border-2 border-solid transform overflow-hidden rounded-lg transition-all sm:w-full"
+            class="text-16px text-foreground bg-background text-left relative z-10 mx-auto flex flex-col justify-around border-2 border-solid transform overflow-hidden rounded-lg transition-all"
           >
             <div v-if="showCloseX" class="absolute z-20 right-0 top-0 pt-4px pr-4px hidden sm:block">
               <button
