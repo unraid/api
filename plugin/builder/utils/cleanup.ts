cdaebf54117ffdc116170ef92558ba6cbf4535aa
Eli Bosley <ekbosley@gmail.com>
2025-02-27 15:43:36 -0500

    chore: create deploy directory before running build

--- plugin/builder/utils/cleanup.ts
+++ plugin/builder/utils/cleanup.ts
@@ -1,9 +1,18 @@
 import { execSync } from "node:child_process";
+import { deployDir } from "./paths";
+import { mkdir } from "node:fs/promises";
+import { startingDir } from "./consts";
+import { join } from "node:path";
 
 export const cleanupTxzFiles = async () => {
-  await execSync(`rm -rf deploy/*.txz`);
+  await mkdir(deployDir, { recursive: true });
+  const txzFiles = join(startingDir, deployDir, "*.txz");
+  await execSync(`rm -rf ${txzFiles}`);
 };
 
 export const cleanupPluginFiles = async () => {
-  await execSync(`rm -rf deploy/*.plg`);
-}
+  await mkdir(deployDir, { recursive: true });
+  const pluginFiles = join(startingDir, deployDir, "*.plg");
+  await execSync(`rm -rf ${pluginFiles}`);
+};
+
