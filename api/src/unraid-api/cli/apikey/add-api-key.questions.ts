22d298f45a255238163c050490c0901f815a66a3
Pujit Mehrotra <pujit@lime-technology.com>
2025-03-03 14:18:01 -0500

    fix(api): type imports from generated graphql types

--- api/src/unraid-api/cli/apikey/add-api-key.questions.ts
+++ api/src/unraid-api/cli/apikey/add-api-key.questions.ts
@@ -1,6 +1,7 @@
 import { ChoicesFor, Question, QuestionSet, WhenFor } from 'nest-commander';
 
-import { Permission, Role } from '@app/graphql/generated/api/types.js';
+import type { Permission } from '@app/graphql/generated/api/types.js';
+import { Role } from '@app/graphql/generated/api/types.js';
 import { ApiKeyService } from '@app/unraid-api/auth/api-key.service.js';
 import { LogService } from '@app/unraid-api/cli/log.service.js';
 
