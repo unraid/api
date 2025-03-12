423b5de13b20f9470a86996bd8c0fd923a96a48e
Pujit Mehrotra <pujit@lime-technology.com>
2025-03-12 14:42:42 -0400

    refactor brand button outline bg condition

--- unraid-ui/src/components/brand/BrandButton.vue
+++ unraid-ui/src/components/brand/BrandButton.vue
@@ -47,6 +47,9 @@ const classes = computed(() => {
     icon: `${iconSize} fill-current flex-shrink-0`,
   };
 });
+const needsBrandGradientBackground = computed(() => {
+  return ['outline', 'outline-primary'].includes(props.variant ?? '');
+});
 </script>
 
 <template>
@@ -65,8 +68,9 @@ const classes = computed(() => {
       v-if="variant === 'fill'"
       class="absolute -top-[2px] -right-[2px] -bottom-[2px] -left-[2px] -z-10 bg-gradient-to-r from-unraid-red to-orange opacity-100 transition-all rounded-md group-hover:opacity-60 group-focus:opacity-60"
     />
+    <!-- gives outline buttons the brand gradient background -->
     <div
-      v-if="variant?.includes('outline')"
+      v-if="needsBrandGradientBackground"
       class="absolute -top-[2px] -right-[2px] -bottom-[2px] -left-[2px] -z-10 bg-gradient-to-r from-unraid-red to-orange opacity-0 transition-all rounded-md group-hover:opacity-100 group-focus:opacity-100"
     />
 
