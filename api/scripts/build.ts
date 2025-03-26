d4cb481e8cf57fc33f16bda5dfbfb2d676f5aea8
Pujit Mehrotra <pujit@lime-technology.com>
2025-03-26 13:33:11 -0400

    replace sudo with a variable

--- api/scripts/build.ts
+++ api/scripts/build.ts
@@ -43,7 +43,11 @@ try {
     await $`pnpm install --prod --ignore-workspace`;
 
     await $`rm -rf node_modules`; // Don't include node_modules in final package
-    await $`sudo chown -R 0:0 ../.pnpm-store`;
+
+    const sudoCheck = await $`command -v sudo`.nothrow();
+    const SUDO = sudoCheck.exitCode === 0 ? 'sudo' : '';
+    await $`${SUDO} chown -R 0:0 ../.pnpm-store`;
+
     await $`XZ_OPT=-5 tar -cJf ../packed-pnpm-store.txz ../.pnpm-store`;
 
     // chmod the cli
