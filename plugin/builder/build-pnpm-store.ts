487bfe99a91abd2e0eecf5c3db30e10bc473680b
Pujit Mehrotra <pujit@lime-technology.com>
2025-03-26 10:45:18 -0400

    document vendor store bundling

--- plugin/builder/build-pnpm-store.ts
+++ plugin/builder/build-pnpm-store.ts
@@ -17,14 +17,26 @@ function getVersion(): string {
     return packageJson.version;
 }
 
+/**
+ * The name of the pnpm store archive that will be vendored with the plugin.
+ * @returns The name of the pnpm store bundle file
+ */
 export function getPnpmBundleName(): string {
     const version = getVersion();
     return `pnpm-store-for-v${version}.txz`;
 }
 
+/**
+ * Prepare a versioned bundle of the API's pnpm store to vendor dependencies.
+ * 
+ * It expects a generic `packed-pnpm-store.txz` archive to be available in the `startingDir`.
+ * It copies this archive to the `deployDir` directory and adds a version to the filename.
+ * It does not actually create the packed pnpm store archive; that is done inside the API's build script.
+ * 
+ * After this operation, the vendored store will be available inside the `deployDir`.
+ */
 export async function bundlePnpmStore(): Promise<void> {
     const storeArchive = join(startingDir, "packed-pnpm-store.txz");
     const pnpmStoreTarPath = join(deployDir, getPnpmBundleName());
     await copyFile(storeArchive, pnpmStoreTarPath);
 }
-
