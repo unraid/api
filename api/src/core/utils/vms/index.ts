9e92c09ca9fc520e6c81ef48483ad727cc00198a
Eli Bosley <ekbosley@gmail.com>
2025-03-29 08:44:02 -0400

    fix: index file error

--- api/src/core/utils/vms/index.ts
+++ api/src/core/utils/vms/index.ts
@@ -1,8 +1,5 @@
 // Created from 'create-ts-index'
 
 export * from './filter-devices.js';
-export * from './get-hypervisor.js';
 export * from './get-pci-devices.js';
-export * from './parse-domain.js';
-export * from './parse-domains.js';
 export * from './system-network-interfaces.js';
