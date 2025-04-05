50c920b15944b14a15fa6b2b6bb8baa9585a0b02
Pujit Mehrotra <pujit@lime-technology.com>
2025-04-05 07:13:08 -0400

    rm previous plugin interface

--- api/src/unraid-api/plugin/plugin.interface.ts
+++ api/src/unraid-api/plugin/plugin.interface.ts
@@ -1,11 +1,7 @@
-import type { Provider } from '@nestjs/common';
-import { Logger, Type } from '@nestjs/common';
 
 import type { Constructor } from 'type-fest';
-import { CommandRunner } from 'nest-commander';
 import { z } from 'zod';
 
-import { ApiStore } from '@app/store/index.js';
 
 export interface PluginMetadata {
     name: string;
@@ -35,40 +31,9 @@ const resolverTypeMap = z.record(
 const asyncResolver = () => z.function().returns(z.promise(resolverTypeMap));
 
 type NestModule = Constructor<unknown>;
-
 const isClass = (value: unknown): value is NestModule => {
     return typeof value === 'function' && value.toString().startsWith('class');
 };
-
-/**
- * Convert a NestJS module to a provider.
- * @param module - The NestJS module to convert.
- * @returns A provider that can be used in a NestJS module.
- */
-export function nestModuleToProvider(module: NestModule): Provider {
-    return {
-        provide: module.name,
-        useValue: module,
-    };
-}
-
-/** Warning: unstable API. The config mechanism and API may soon change. */
-export const apiPluginSchema = z.object({
-    _type: z.literal('UnraidApiPlugin'),
-    name: z.string(),
-    description: z.string(),
-    commands: z.array(z.custom<Type<CommandRunner>>()),
-    config: z.function().returns(z.array(z.any())).optional(),
-    registerGraphQLResolvers: asyncResolver().optional(),
-    registerGraphQLTypeDefs: asyncString().optional(),
-    registerRESTControllers: asyncArray().optional(),
-    registerRESTRoutes: asyncArray().optional(),
-    registerServices: asyncArray().optional(),
-    registerCronJobs: asyncArray().optional(),
-    // These schema definitions are picked up as nest modules as well.
-    onModuleInit: asyncVoid().optional(),
-    onModuleDestroy: asyncVoid().optional(),
-});
 
 /** format of module exports from a nestjs plugin */
 export const apiNestPluginSchema = z
@@ -106,14 +71,3 @@ export const apiNestPluginSchema = z
     });
 
 export type ApiNestPluginDefinition = z.infer<typeof apiNestPluginSchema>;
-
-/** Warning: unstable API. The config mechanism and API may soon change. */
-export type ApiPluginDefinition = z.infer<typeof apiPluginSchema>;
-
-// todo: the blocker to publishing this type is the 'ApiStore' type.
-// It pulls in a lot of irrelevant types (e.g. graphql types) and triggers js transpilation of everything related to the store.
-// If we can isolate the type, we can publish it to npm and developers can use it as a dev dependency.
-/**
- * Represents a subclass of UnraidAPIPlugin that can be instantiated.
- */
-export type ConstructablePlugin = (options: { store: ApiStore; logger: Logger }) => ApiPluginDefinition;
