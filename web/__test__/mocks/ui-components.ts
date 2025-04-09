c9a10afb16765801a5f2efda0357b878708710ac
mdatelle <mike@datelle.net>
2025-04-09 11:04:11 -0400

    refactor: add download prop and remove external from BranButton mock

--- web/__test__/mocks/ui-components.ts
+++ web/__test__/mocks/ui-components.ts
@@ -15,6 +15,7 @@ const MockBrandButton = {
     'iconRightHoverDisplay',
     'text',
     'title',
+    'download',
   ],
   template: `
     <component :is="props.href ? 'a' : 'button'"
@@ -24,7 +25,6 @@ const MockBrandButton = {
       :target="props.external ? '_blank' : undefined"
       :rel="props.external ? 'noopener noreferrer' : undefined"
       :title="props.title"
-      :external="props.external ? 'true' : undefined"
       :download="props.download"
     >
       <span v-if="props.icon" class="icon">{{ props.icon }}</span>
