370c94f63bf74f733878391bfae229af5ac18893
Eli Bosley <ekbosley@gmail.com>
2025-04-16 16:10:55 -0400

    chore: fix combobox

--- unraid-ui/src/components/form/combobox/ComboboxList.vue
+++ unraid-ui/src/components/form/combobox/ComboboxList.vue
@@ -12,7 +12,7 @@ defineOptions({
 const { teleportTarget } = useTeleport();
 
 const props = withDefaults(defineProps<ComboboxContentProps & { class?: HTMLAttributes['class'] }>(), {
-  forceMount: true,
+  forceMount: false,
   position: 'popper',
   to: undefined,
 });
@@ -28,17 +28,27 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
-  <ComboboxPortal :to="teleportTarget">
+  <ComboboxPortal :to="teleportTarget" :force-mount="forceMount">
     <ComboboxContent
       v-bind="{ ...forwarded, ...$attrs }"
       :class="
         cn(
           'z-50 w-[200px] rounded-md border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+          position === 'popper' &&
+            'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
           props.class
         )
       "
     >
-      <ComboboxViewport>
+      <ComboboxViewport
+        :class="
+          cn(
+            'p-1',
+            position === 'popper' &&
+              'h-[--reka-combobox-trigger-height] w-full min-w-[--reka-combobox-trigger-width]'
+          )
+        "
+      >
         <slot />
       </ComboboxViewport>
     </ComboboxContent>
