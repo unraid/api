7067e9e3dd3966309013b52c90090cc82de4e4fb
Eli Bosley <ekbosley@gmail.com>
2025-05-22 09:40:36 -0400

    fix: proper fallback if missing paths config modules

--- api/src/environment.ts
+++ api/src/environment.ts
@@ -94,4 +94,5 @@ export const MOTHERSHIP_GRAPHQL_LINK = process.env.MOTHERSHIP_GRAPHQL_LINK
       : 'https://mothership.unraid.net/ws';
 
 export const PM2_HOME = process.env.PM2_HOME ?? join(homedir(), '.pm2');
-export const PATHS_CONFIG_MODULES = process.env.PATHS_CONFIG_MODULES!;
+export const PATHS_CONFIG_MODULES =
+    process.env.PATHS_CONFIG_MODULES ?? '/usr/local/unraid-api/config/modules';
