af521588670c743d54fe76367e9dd337bbe6b714
Eli Bosley <ekbosley@gmail.com>
2025-05-27 11:40:03 -0400

    fix: forwarded emits

--- unraid-ui/src/components/common/dialog/DialogClose.vue
+++ unraid-ui/src/components/common/dialog/DialogClose.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { DialogClose, type DialogCloseProps } from 'reka-ui';
+import { DialogClose, useForwardPropsEmits, type DialogCloseProps } from 'reka-ui';
 
 const props = defineProps<DialogCloseProps>();
+const emits = defineEmits([]);
+
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
-  <DialogClose v-bind="props">
+  <DialogClose v-bind="forwarded">
     <slot />
   </DialogClose>
 </template>
