0d118539b479fc560801d75a85a343f1a61684f7
mdatelle <mike@datelle.net>
2025-06-23 13:45:01 -0400

    refactor: add component files

---
+++ unraid-ui/src/components/ui/index.ts
@@ -0,0 +1,9 @@
+// Export all UI primitives
+export { AccordionContent, AccordionItem, AccordionTrigger } from './accordion';
+export { DialogContent, DialogTitle, DialogTrigger } from './dialog';
+export {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from './dropdown';
