1a82a5fe295b68ddbcf8966e0b691ef06359e234
Pujit Mehrotra <pujit@lime-technology.com>
2025-07-14 09:42:38 -0400

    fix: delay nginx reload from file mod to avoid breaking nchan

--- api/src/unraid-api/unraid-file-modifier/file-modification-effect.service.ts
+++ api/src/unraid-api/unraid-file-modifier/file-modification-effect.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
+import { ONE_SECOND_MS } from '@app/consts.js';
 import { NginxService } from '@app/unraid-api/nginx/nginx.service.js';
 import { ModificationEffect } from '@app/unraid-api/unraid-file-modifier/file-modification.js';
 
 @Injectable()
 export class FileModificationEffectService {
+    private readonly logger = new Logger(FileModificationEffectService.name);
     constructor(private readonly nginxService: NginxService) {}
     async runEffect(effect: ModificationEffect): Promise<void> {
         switch (effect) {
             case 'nginx:reload':
+                this.logger.log('Reloading Nginx in 10 seconds...');
+                await new Promise((resolve) => setTimeout(resolve, 10 * ONE_SECOND_MS));
                 await this.nginxService.reload();
                 break;
         }
