b54e2571d162b764c518e30c02095cfc7f18ccfe
Pujit Mehrotra <pujit@lime-technology.com>
2025-07-25 10:09:29 -0400

    use ValidationError instead of plain object mapping properties to errors

--- api/src/unraid-api/graph/resolvers/docker/docker-config.service.ts
+++ api/src/unraid-api/graph/resolvers/docker/docker-config.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { ConfigFilePersister } from '@unraid/shared/services/config-file.js';
+import { ValidationError } from 'class-validator';
 
-import { AppError } from '@app/core/errors/app-error.js';
 import { validateObject } from '@app/unraid-api/graph/resolvers/validation.utils.js';
 import { OrganizerV1 } from '@app/unraid-api/organizer/organizer.dto.js';
 import { validateOrganizerIntegrity } from '@app/unraid-api/organizer/organizer.validation.js';
@@ -34,7 +34,10 @@ export class DockerConfigService extends ConfigFilePersister<OrganizerV1> {
         const organizer = await validateObject(OrganizerV1, config);
         const { isValid, errors } = await validateOrganizerIntegrity(organizer);
         if (!isValid) {
-            throw errors;
+            const error = new ValidationError();
+            error.target = organizer;
+            error.contexts = errors;
+            throw error;
         }
         return organizer;
     }
