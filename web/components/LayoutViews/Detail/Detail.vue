cf4b0241d239f4f291cbf6f39c286ce688116b6f
mdatelle <mike@datelle.net>
2025-07-31 14:28:53 -0400

    refactor: use Drawer component for mobile view navigation

--- web/components/LayoutViews/Detail/Detail.vue
+++ web/components/LayoutViews/Detail/Detail.vue
@@ -51,7 +51,6 @@ const selectedTab = ref(props.defaultTabKey || '0');
 const selectedItems = ref<string[]>([]);
 const expandedGroups = ref<Record<string, boolean>>({});
 const autostartEnabled = ref(true);
-const sidebarOpen = ref(false);
 
 // Initialize expanded state for groups
 const initializeExpandedState = () => {
@@ -127,76 +126,28 @@ const handleManageSelectedAction = (action: string) => {
 const handleManageItemAction = (action: string) => {
   console.log('Manage item action:', action);
 };
-
-const toggleSidebar = () => {
-  sidebarOpen.value = !sidebarOpen.value;
-};
-
-const closeSidebar = () => {
-  sidebarOpen.value = false;
-};
 </script>
 
 <template>
   <div class="flex flex-col lg:flex-row h-full">
-    <!-- Mobile sidebar backdrop -->
-    <div
-      v-if="sidebarOpen"
-      class="fixed inset-0 z-40 bg-black bg-opacity-20 lg:hidden"
-      @click="closeSidebar"
+    <!-- Navigation -->
+    <DetailLeftNavigation
+      :items="items"
+      :selected-id="selectedItemId"
+      :selected-items="selectedItems"
+      :expanded-groups="expandedGroups"
+      :navigation-label="navigationLabel"
+      @update:selected-id="selectedItemId = $event"
+      @update:selected-items="selectedItems = $event"
+      @update:expanded-groups="expandedGroups = $event"
+      @add="handleAddAction"
+      @select-all="selectAllItems"
+      @clear-all="clearAllSelections"
+      @manage-action="handleManageSelectedAction"
     />
 
-    <!-- Mobile navigation button -->
-    <div class="lg:hidden m-4">
-      <UButton color="primary" size="md" class="w-full justify-center" @click="toggleSidebar">
-        {{ navigationLabel }}
-      </UButton>
-    </div>
-
-    <!-- Navigation Sidebar -->
-    <div
-      :class="[
-        'fixed lg:block lg:relative inset-y-0 left-0 z-50 lg:z-auto',
-        'w-72 lg:w-auto flex-shrink-0',
-        'transform lg:transform-none transition-transform duration-300 ease-in-out',
-        sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
-        'bg-white dark:bg-gray-900 lg:bg-transparent',
-      ]"
-    >
-      <div class="h-full overflow-y-auto overflow-x-hidden p-4 lg:p-0">
-        <!-- Close button for mobile -->
-        <div class="lg:hidden flex justify-end mb-4">
-          <UButton
-            icon="i-lucide-x"
-            size="sm"
-            color="neutral"
-            variant="ghost"
-            square
-            @click="closeSidebar"
-          />
-        </div>
-
-        <DetailLeftNavigation
-          :items="items"
-          :selected-id="selectedItemId"
-          :selected-items="selectedItems"
-          :expanded-groups="expandedGroups"
-          @update:selected-id="
-            selectedItemId = $event;
-            closeSidebar();
-          "
-          @update:selected-items="selectedItems = $event"
-          @update:expanded-groups="expandedGroups = $event"
-          @add="handleAddAction"
-          @select-all="selectAllItems"
-          @clear-all="clearAllSelections"
-          @manage-action="handleManageSelectedAction"
-        />
-      </div>
-    </div>
-
     <!-- Main Content -->
-    <div class="flex-1 min-w-0 px-4 lg:px-0">
+    <div class="flex-1 min-w-0">
       <DetailRightContent
         :selected-item="selectedItem"
         :tabs="tabs"
