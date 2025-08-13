d24302e843ca9f4ccf3ea5be35e62274ad1a2676
Zack Spear <hi@zackspear.com>
2025-08-13 12:36:11 -0700

    fix: SettingsGrid column Tailwindclasses replace style block

--- unraid-ui/src/components/layout/SettingsGrid.vue
+++ unraid-ui/src/components/layout/SettingsGrid.vue
@@ -8,24 +8,13 @@ const props = defineProps<{
 
 <template>
   <div
-    :class="cn('settings-grid grid gap-2 items-baseline md:pl-3 md:gap-x-10 md:gap-y-6', props.class)"
+    :class="
+      cn(
+        'grid grid-cols-1 md:grid-cols-[35%_1fr] [&>*:nth-child(odd)]:text-end gap-2 items-baseline md:pl-3 md:gap-x-10 md:gap-y-6',
+        props.class
+      )
+    "
   >
     <slot />
   </div>
 </template>
-
-<style>
-.settings-grid {
-  grid-template-columns: 1fr;
-}
-
-@media (min-width: 768px) {
-  .settings-grid {
-    grid-template-columns: 35% 1fr;
-  }
-
-  .settings-grid > *:nth-child(odd) {
-    text-align: end;
-  }
-}
-</style>
