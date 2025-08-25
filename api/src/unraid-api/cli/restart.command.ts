3cd0d3022969b2c92a82533ba7b5caa62b858e51
Pujit Mehrotra <pujit@lime-technology.com>
2025-08-25 10:21:08 -0400

    fix: support `--log-level`

--- api/src/unraid-api/cli/restart.command.ts
+++ api/src/unraid-api/cli/restart.command.ts
@@ -1,8 +1,14 @@
-import { Command, CommandRunner } from 'nest-commander';
+import { Command, CommandRunner, Option } from 'nest-commander';
 
+import type { LogLevel } from '@app/core/log.js';
+import { levels } from '@app/core/log.js';
 import { ECOSYSTEM_PATH } from '@app/environment.js';
 import { LogService } from '@app/unraid-api/cli/log.service.js';
 import { PM2Service } from '@app/unraid-api/cli/pm2.service.js';
+
+interface RestartCommandOptions {
+    'log-level'?: string;
+}
 
 @Command({ name: 'restart', description: 'Restart the Unraid API' })
 export class RestartCommand extends CommandRunner {
@@ -13,11 +19,11 @@ export class RestartCommand extends CommandRunner {
         super();
     }
 
-    async run(): Promise<void> {
+    async run(_: string[], options: RestartCommandOptions): Promise<void> {
         try {
             this.logger.info('Restarting the Unraid API...');
             const { stderr, stdout } = await this.pm2.run(
-                { tag: 'PM2 Restart', raw: true },
+                { tag: 'PM2 Restart', raw: true, env: { LOG_LEVEL: options['log-level'] } },
                 'restart',
                 ECOSYSTEM_PATH,
                 '--update-env'
@@ -39,5 +45,14 @@ export class RestartCommand extends CommandRunner {
             }
             process.exit(1);
         }
+    }
+
+    @Option({
+        flags: `--log-level <${levels.join('|')}>`,
+        description: 'log level to use',
+        defaultValue: 'info',
+    })
+    parseLogLevel(val: string): LogLevel {
+        return levels.includes(val as LogLevel) ? (val as LogLevel) : 'info';
     }
 }
