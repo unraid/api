b1a1a0c092c071f3c49176cc488f7902a4118afd
Eli Bosley <ekbosley@gmail.com>
2025-08-29 14:52:18 -0400

    fix: config download manager route

---
+++ web/components/ConfigDownload/ConfigDownloadManager.ce.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import ConfigDownloadManager from '~/components/ConfigDownload/ConfigDownloadManager.vue';
+</script>
+<template>
+  <div>
+    <ConfigDownloadManager />
+  </div>
+</template>
