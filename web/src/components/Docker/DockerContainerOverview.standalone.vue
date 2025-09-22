2e938ced5eb1bbef17d13eb3cadd5f343d89dcdc
Pujit Mehrotra <pujit@lime-technology.com>
2025-09-22 15:13:03 -0400

    docker container management prototype

--- web/src/components/Docker/DockerContainerOverview.standalone.vue
+++ web/src/components/Docker/DockerContainerOverview.standalone.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DockerContainerManagement from '@/components/Docker/DockerContainerManagement.vue';
 import DockerContainerOverview from '@/components/Docker/DockerContainerOverview.vue';
 import DockerEdit from '@/components/Docker/Edit.vue';
 import DockerLogs from '@/components/Docker/Logs.vue';
@@ -19,6 +20,7 @@ const details = {
 </script>
 <template>
   <div class="grid gap-8">
+    <DockerContainerManagement />
     <DockerContainerOverview />
     <DockerOverview :item="item" :details="details" />
     <DockerPreview :item="item" />
