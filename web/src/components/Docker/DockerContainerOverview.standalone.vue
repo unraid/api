c1f7294c057be185baa06cd943b0a551d5abddad
Pujit Mehrotra <pujit@lime-technology.com>
2025-09-25 14:51:11 -0400

    click overview row to open details

--- web/src/components/Docker/DockerContainerOverview.standalone.vue
+++ web/src/components/Docker/DockerContainerOverview.standalone.vue
@@ -21,10 +21,10 @@ const details = {
 <template>
   <div class="grid gap-8">
     <DockerContainerManagement />
-    <DockerContainerOverview />
-    <DockerOverview :item="item" :details="details" />
+    <!-- <DockerContainerOverview /> -->
+    <!-- <DockerOverview :item="item" :details="details" />
     <DockerPreview :item="item" />
     <DockerEdit :item="item" />
-    <DockerLogs :item="item" />
+    <DockerLogs :item="item" /> -->
   </div>
 </template>
